8489a5ca0c66c67284858e3756346087eb013862
pfarrell <>
2009-12-19 12:57:13 +0000

    Make it possible to retain surface information through supermeshing for the tetrahedral intersector.
    The ++++, ----, +--- and ++-- cases are done. More cases to do.

--- femtools/Tetrahedron_intersection.F90
+++ femtools/Tetrahedron_intersection.F90
@@ -14,9 +14,7 @@ module tetrahedron_intersection_module
 
   type tet_type
     real, dimension(3, 4) :: V ! vertices of the tet
-#ifdef RETAIN_FACE_COLOURING
-    integer, dimension(4) :: colours = 0 ! surface colours
-#endif
+    integer, dimension(4) :: colours = -1 ! surface colours
   end type tet_type
 
   type plane_type
@@ -24,7 +22,7 @@ module tetrahedron_intersection_module
     real :: c
   end type plane_type
 
-  type(tet_type), dimension(BUF_SIZE) :: tet_array, tet_array_tmp
+  type(tet_type), dimension(BUF_SIZE), save :: tet_array, tet_array_tmp
   integer :: tet_cnt = 0, tet_cnt_tmp = 0
   type(mesh_type), save :: intersection_mesh
   logical, save :: mesh_allocated = .false.
@@ -48,17 +46,29 @@ module tetrahedron_intersection_module
     end if
   end subroutine finalise_tet_intersector
 
-  subroutine intersect_tets_dt(tetA, planesB, shape, stat, output)
+  subroutine intersect_tets_dt(tetA, planesB, shape, stat, output, surface_shape, surface_positions, surface_colours)
     type(tet_type), intent(in) :: tetA
     type(plane_type), dimension(:), intent(in) :: planesB
     type(element_type), intent(in) :: shape
     type(vector_field), intent(inout) :: output
+    type(vector_field), intent(out), optional :: surface_positions
+    type(scalar_field), intent(out), optional :: surface_colours
+    type(element_type), intent(in), optional :: surface_shape
     integer :: ele
     integer, intent(out) :: stat
 
-    integer :: i, j
+    integer :: i, j, k, l
     real :: vol
     real, dimension(3) :: vec_tmp
+    integer, dimension(3) :: idx_tmp
+    integer :: surface_eles
+    type(mesh_type) :: surface_mesh, pwc_surface_mesh
+
+    if (present(surface_colours) .or. present(surface_positions) .or. present(surface_shape)) then
+      assert(present(surface_positions))
+      assert(present(surface_colours))
+      assert(present(surface_shape))
+    end if
 
     tet_cnt = 1
     tet_array(1) = tetA
@@ -66,6 +76,7 @@ module tetrahedron_intersection_module
     if (.not. mesh_allocated) then
       call allocate(intersection_mesh, BUF_SIZE * 4, BUF_SIZE, shape, name="IntersectionMesh")
       intersection_mesh%ndglno = (/ (i, i=1,BUF_SIZE*4) /)
+      intersection_mesh%continuity = -1
       mesh_allocated = .true.
     end if
 
@@ -113,6 +124,46 @@ module tetrahedron_intersection_module
     do ele=1,tet_cnt
       call set(output, ele_nodes(output, ele), tet_array(ele)%V)
     end do
+
+    if (present(surface_positions)) then
+      ! OK! Let's loop through all the tets we have and see which faces have positive
+      ! colour. These are the ones we want to record in the mesh
+      surface_eles = 0
+      do ele=1,tet_cnt
+        surface_eles = surface_eles + count(tet_array(ele)%colours > 0)
+      end do
+
+      call allocate(surface_mesh, surface_eles * 3, surface_eles, surface_shape, name="SurfaceMesh")
+      surface_mesh%ndglno = (/ (i, i=1,surface_eles * 3) /)
+      call allocate(surface_positions, 3, surface_mesh, "OutputSurfaceCoordinate")
+      pwc_surface_mesh = piecewise_constant_mesh(surface_mesh, "PWCSurfaceMesh")
+      call allocate(surface_colours, pwc_surface_mesh, "SurfaceColours")
+      call deallocate(surface_mesh)
+      call deallocate(pwc_surface_mesh)
+
+      j = 1
+      do ele=1,tet_cnt
+        do i=1,4
+          if (tet_array(ele)%colours(i) > 0) then
+
+            ! In python, this is
+            ! idx_tmp = [x for x in range(4) if x != i]
+            ! Hopefully that will make it clearer
+            k = 1
+            do l=1,4
+              if (l /= i) then
+                idx_tmp(k) = l
+                k = k + 1
+              end if
+            end do
+            call set(surface_positions, ele_nodes(surface_positions, j), tet_array(ele)%V(:, idx_tmp))
+            call set(surface_colours, j, float(tet_array(ele)%colours(i)))
+            j = j + 1
+          end if
+        end do
+      end do
+    end if
+
   end subroutine intersect_tets_dt
 
   subroutine clip(plane, tet)
@@ -177,11 +228,9 @@ module tetrahedron_intersection_module
            w0 * tet_array_tmp(tet_cnt_tmp)%V(:, pos_idx(i)) + &
            w1 * tet_array_tmp(tet_cnt_tmp)%V(:, neg_idx(1))
       end do
-#ifdef RETAIN_FACE_COLOURING
       ! The colours will have been inherited already; we just need to zero
       ! the one corresponding to the plane cut
-      tet_array_tmp(tet_cnt_tmp)%colours(face_no(pos_idx(1), pos_idx(2), pos_idx(3))) = 0
-#endif
+       tet_array_tmp(tet_cnt_tmp)%colours(face_no(pos_idx(1), pos_idx(2), pos_idx(3))) = 0
     case(2)
       select case(neg_cnt)
       case(2)
@@ -203,23 +252,24 @@ module tetrahedron_intersection_module
         tet_array_tmp(tet_cnt_tmp) = tet
         tet_array_tmp(tet_cnt_tmp)%V(:, pos_idx(1)) = tet_tmp%V(:, 3)
         tet_array_tmp(tet_cnt_tmp)%V(:, pos_idx(2)) = tet_tmp%V(:, 2)
-#ifdef RETAIN_FACE_COLOURING
-        ! Zero any face involving the two 
-        tet_array_tmp(tet_cnt_tmp)%colours(face_no(neg_idx(1), pos_idx(1), pos_idx(2))) = 0
-        tet_array_tmp(tet_cnt_tmp)%colours(face_no(neg_idx(2), pos_idx(1), pos_idx(2))) = 0
-#endif
+        tet_array_tmp(tet_cnt_tmp)%colours(neg_idx(1)) = 0
+        tet_array_tmp(tet_cnt_tmp)%colours(neg_idx(2)) = 0
 
         tet_cnt_tmp = tet_cnt_tmp + 1
         tet_array_tmp(tet_cnt_tmp)%V(:, 1) = tet%V(:, neg_idx(2))
         tet_array_tmp(tet_cnt_tmp)%V(:, 2) = tet_tmp%V(:, 4)
         tet_array_tmp(tet_cnt_tmp)%V(:, 3) = tet_tmp%V(:, 3)
         tet_array_tmp(tet_cnt_tmp)%V(:, 4) = tet_tmp%V(:, 2)
+        tet_array_tmp(tet_cnt_tmp)%colours(3) = tet%colours(pos_idx(2))
+        tet_array_tmp(tet_cnt_tmp)%colours(4) = tet%colours(neg_idx(1))
 
         tet_cnt_tmp = tet_cnt_tmp + 1
         tet_array_tmp(tet_cnt_tmp)%V(:, 1) = tet%V(:, neg_idx(1))
         tet_array_tmp(tet_cnt_tmp)%V(:, 2) = tet_tmp%V(:, 1)
         tet_array_tmp(tet_cnt_tmp)%V(:, 3) = tet_tmp%V(:, 2)
         tet_array_tmp(tet_cnt_tmp)%V(:, 4) = tet_tmp%V(:, 3)
+        tet_array_tmp(tet_cnt_tmp)%colours(3) = tet%colours(pos_idx(1))
+        tet_array_tmp(tet_cnt_tmp)%colours(4) = tet%colours(neg_idx(2))
       case(1)
         ! ++-0
         tet_cnt_tmp = tet_cnt_tmp + 1
@@ -330,7 +380,7 @@ module tetrahedron_intersection_module
     integer, intent(in) :: ele
     type(plane_type), dimension(6) :: plane
     integer, dimension(:), pointer :: faces
-    integer :: i, face, j
+    integer :: i, face
     integer, dimension(4) :: fnodes
     real, dimension(positions%dim, face_ngi(positions, ele)) :: normals
 
@@ -394,14 +444,15 @@ module tetrahedron_intersection_module
     vol = dot_product(vecA, cross) / 6.0
   end function tet_volume
 
-#ifdef RETAIN_FACE_COLOURING
   function face_no(i, j, k) result(face)
     ! Given three local node numbers, what is the face that they share?
     integer, intent(in) :: i, j, k
     integer :: face
 
-    face = i + j + k - 5
+    do face=1,4
+      if (face /= i .and. face /= j .and. face /= k) return
+    end do
+
   end function face_no
-#endif
 
 end module tetrahedron_intersection_module
