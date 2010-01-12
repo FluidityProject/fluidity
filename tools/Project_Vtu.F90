98af3d9c5358af0f9e4a01bafe4ca673f5390d70
maddison <>
2010-01-12 12:44:36 +0000

    Tool for offline Galerkin projection

---
+++ tools/Project_Vtu.F90
@@ -0,0 +1,136 @@
+!    Copyright (C) 2006 Imperial College London and others.
+!    
+!    Please see the AUTHORS file in the main source directory for a full list
+!    of copyright holders.
+!
+!    Prof. C Pain
+!    Applied Modelling and Computation Group
+!    Department of Earth Science and Engineeringp
+!    Imperial College London
+!
+!    C.Pain@Imperial.ac.uk
+!    
+!    This library is free software; you can redistribute it and/or
+!    modify it under the terms of the GNU Lesser General Public
+!    License as published by the Free Software Foundation,
+!    version 2.1 of the License.
+!
+!    This library is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!    Lesser General Public License for more details.
+!
+!    You should have received a copy of the GNU Lesser General Public
+!    License along with this library; if not, write to the Free Software
+!    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+!    USA
+
+#include "fdebug.h"
+
+subroutine project_vtu(input_filename, input_filename_len, donor_basename, donor_basename_len, target_basename, target_basename_len, output_filename, output_filename_len)
+
+  use conservative_interpolation_module
+  use fields
+  use fldebug
+  use global_parameters, only : current_debug_level
+  use intersection_finder_module
+  use linked_lists
+  use solvers
+  use spud
+  use state_module
+  use read_triangle
+  use vtk_interfaces
+  use write_triangle
+  
+  implicit none
+  
+  integer, intent(in) :: input_filename_len
+  integer, intent(in) :: donor_basename_len
+  integer, intent(in) :: target_basename_len
+  integer, intent(in) :: output_filename_len
+  character(len = input_filename_len), intent(in) :: input_filename
+  character(len = donor_basename_len), intent(in) :: donor_basename
+  character(len = target_basename_len), intent(in) :: target_basename
+  character(len = output_filename_len), intent(in) :: output_filename
+  
+  character(len = *), parameter :: fields_path = "/dummy"
+  integer :: i
+  integer, parameter :: quad_degree = 4
+  type(ilist), dimension(:), allocatable :: map_BA
+  type(mesh_type) :: output_mesh
+  type(state_type) :: input_state, output_state
+  type(scalar_field) :: output_s_field
+  type(scalar_field), pointer :: input_s_field
+  type(vector_field) :: donor_positions, output_v_field, target_positions
+  type(vector_field), pointer :: input_v_field
+  type(tensor_field) :: output_t_field
+  type(tensor_field), pointer :: input_t_field
+  
+  ewrite(1, *) "In project_vtu"
+  
+  call set_solver_options(fields_path // "/galerkin_projection/continuous/solver", &
+    & ksptype = "cg", pctype = "sor", atol = epsilon(0.0), rtol = 0.0, max_its = 2000, start_from_zero = .true.)
+      
+  call vtk_read_state(trim(input_filename), input_state, quad_degree = quad_degree)
+  
+  donor_positions = read_triangle_files(trim(donor_basename), quad_degree = quad_degree)
+  target_positions = read_triangle_files(trim(target_basename), quad_degree = quad_degree)
+  output_mesh = make_mesh(target_positions%mesh, ele_shape(donor_positions, 1), continuity = continuity(extract_vector_field(input_state, "Coordinate")))
+  
+  allocate(map_BA(ele_count(target_positions)))
+  map_BA = rtree_intersection_finder(target_positions, donor_positions)
+  
+  call insert(input_state, donor_positions, "Coordinate")
+  
+  call insert(output_state, target_positions, "Coordinate")
+  call insert(output_state, output_mesh, "Mesh")
+  do i = 1, scalar_field_count(input_state)
+    input_s_field => extract_scalar_field(input_state, i)
+    if(input_s_field%name == "vtkGhostLevels") then
+      call remove_scalar_field(input_state, input_s_field%name)
+      cycle
+    end if
+    call allocate(output_s_field, output_mesh, input_s_field%name)
+    output_s_field%option_path = fields_path
+    call insert(output_state, output_s_field, output_s_field%name)
+  end do
+  do i = 1, vector_field_count(input_state)
+    input_v_field => extract_vector_field(input_state, i)
+    if(input_v_field%name == "Coordinate") cycle
+    call allocate(output_v_field, input_v_field%dim, output_mesh, input_v_field%name)
+    output_v_field%option_path = fields_path
+    call insert(output_state, output_v_field, output_v_field%name)
+  end do
+  do i = 1, tensor_field_count(input_state)
+    input_t_field => extract_tensor_field(input_state, i)
+    call allocate(output_t_field, output_mesh, input_t_field%name)
+    output_t_field%option_path = fields_path
+    call insert(output_state, output_t_field, output_t_field%name)
+  end do
+  
+  call deallocate(donor_positions)
+  call deallocate(target_positions)
+  call deallocate(output_mesh)
+  
+  if(current_debug_level >= 2) then
+    ewrite(2, *) "Options tree:"
+    call print_options()
+    ewrite(2, *) "Input state:"
+    call print_state(input_state)
+    ewrite(2, *) "Output state:"
+    call print_state(output_state)
+  end if
+  
+  call interpolation_galerkin(input_state, output_state, map_BA = map_BA)
+  call deallocate(map_BA)
+  deallocate(map_BA)
+  call deallocate(input_state)
+  
+  call vtk_write_state(trim(output_filename), state = (/output_state/))
+  call deallocate(output_state)
+  
+  call print_references(0)
+  
+  ewrite(1, *) "Exiting project_vtu"
+       
+end subroutine project_vtu
---
+++ tools/Project_Vtu_main.cpp
@@ -0,0 +1,131 @@
+/*  Copyright (C) 2006 Imperial College London and others.
+    
+    Please see the AUTHORS file in the main source directory for a full list
+    of copyright holders.
+
+    Prof. C Pain
+    Applied Modelling and Computation Group
+    Department of Earth Science and Engineering
+    Imperial College London
+
+    C.Pain@Imperial.ac.uk
+    
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation,
+    version 2.1 of the License.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+    USA
+*/
+#include "fmangle.h"
+
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif
+
+extern "C" {
+#define project_vtu_fc F77_FUNC(project_vtu, PROJECT_VTU)
+  void project_vtu_fc(const char*, int*, const char*, int*, const char*, int*, const char*, int*);
+  void set_global_debug_level_fc(int* val);
+}
+
+#ifdef _AIX
+#include <unistd.h>
+#else
+#include <getopt.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std; 
+
+void usage(char *binary){
+  cerr<<"Usage: "<<binary<<" [OPTIONS] input_filename donor_basename target_basename output_filename\n"
+      <<"Project an input vtu onto a triangle mesh\n"
+      <<"\t-h\t\tPrints out this message\n"
+      <<"\t-v\t\tVerbose mode\n";
+}
+
+int main(int argc, char **argv){
+ #ifdef HAVE_MPI
+  // This must be called before we process any arguments
+  MPI::Init(argc,argv);
+
+  // Undo some MPI init shenanigans
+  chdir(getenv("PWD"));
+#endif
+ 
+  // Get any command line arguments
+  // reset optarg so we can detect changes
+  optarg = NULL;  
+  char c;
+  map<char, string> args;
+  while((c = getopt(argc, argv, "hv")) != -1){
+    if (c != '?'){
+      if (optarg == NULL){
+        args[c] = "true";
+      }else{
+        args[c] = optarg;
+      };
+    }else{
+      if (isprint(optopt)){
+        cerr << "Unknown option " << optopt << endl;
+      }else{
+        cerr << "Unknown option " << hex << optopt << endl;
+      }
+      usage(argv[0]);
+      exit(-1);
+    }
+  }
+  
+  if (optind != argc - 4){
+    cerr << "Need exactly four non-option arguments" << endl;
+    usage(argv[0]);
+    exit(-1);
+  }
+
+  // Help?
+  if(args.count('h')){
+    usage(argv[0]);
+    exit(-1);
+  }
+
+  // What to do with stdout?
+  int val=3;
+  if(args.count('v')==0)
+    val = 0;
+  set_global_debug_level_fc(&val);
+
+  string input_filename = argv[optind];
+  int input_filename_len = input_filename.length();  
+  
+  string donor_basename = argv[optind + 1];
+  int donor_basename_len = donor_basename.length();
+  
+  string target_basename = argv[optind + 2];
+  int target_basename_len = target_basename.length();
+  
+  string output_filename = argv[optind + 3];
+  int output_filename_len = output_filename.length();  
+
+  project_vtu_fc(input_filename.c_str(), &input_filename_len, donor_basename.c_str(), &donor_basename_len, target_basename.c_str(), &target_basename_len, output_filename.c_str(), &output_filename_len);
+  
+#ifdef HAVE_MPI
+  MPI::Finalize();
+#endif
+  return(0);
+}
