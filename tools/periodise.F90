dfa63203678977b173b7e4eddf7cfbbccf7bcfb6
pfarrell <>
2010-02-02 15:28:04 +0000

    Some more work on periodise

--- tools/periodise.F90
+++ tools/periodise.F90
@@ -9,12 +9,17 @@ program periodise
   use fields
   use reference_counting
   use spud
+  use field_options
+  use write_triangle
   implicit none
 
-  character(len=255) :: filename
+  character(len=4096) :: filename, external_filename
   integer :: status
   type(state_type), dimension(:), pointer :: states
   integer :: ierr
+  character(len=FIELD_NAME_LEN) :: external_name, periodic_name
+  type(mesh_type), pointer :: periodic_mesh, external_mesh
+  type(vector_field) :: periodic_positions, external_positions
 
   call set_debug_level(3)
   call mpi_init(ierr)
@@ -30,9 +35,24 @@ program periodise
 
   call load_options(trim(filename))
   call populate_state(states)
+  call check_valid_input(states, external_filename, external_name, periodic_name)
 
-  call print_references(1)
+  external_mesh => extract_mesh(states(1), trim(external_name))
+  periodic_mesh => extract_mesh(states(1), trim(periodic_name))
+  external_positions = get_coordinate_field(states(1), external_mesh)
+  periodic_positions = get_coordinate_field(states(1), periodic_mesh)
+
+  call postprocess_periodic_mesh(external_mesh, external_positions, periodic_mesh, periodic_positions)
+
+  ! Dump out the periodic mesh to disk:
+  call write_triangle_files(trim(external_filename) // '_periodic', states(1), periodic_mesh)
+
+  ! OK! Now we need to do some setting of options.
+
   call deallocate(states)
+  call deallocate(external_positions)
+  call deallocate(periodic_positions)
+  call print_references(1)
   call python_end
   call mpi_finalize(ierr)
 
@@ -45,5 +65,73 @@ program periodise
     write(0,*) " which has the periodic mesh as the external one, and the previously"
     write(0,*) " external mesh derived from it."
   end subroutine usage
+
+  subroutine check_valid_input(states, external_filename, external_name, periodic_name)
+    type(state_type), dimension(:), intent(in) :: states
+    character(len=FIELD_NAME_LEN), intent(out) :: external_name, periodic_name
+    integer :: j
+    integer :: seen_external_meshes, seen_periodic_meshes
+    character(len=4096), intent(out) :: external_filename
+    type(mesh_type), pointer :: mesh
+
+    seen_external_meshes = 0
+    seen_periodic_meshes = 0
+
+    do j=1,mesh_count(states(1))
+      mesh => extract_mesh(states(1), j)
+
+      if (have_option(trim(mesh%option_path) // '/from_file')) then
+        seen_external_meshes = seen_external_meshes + 1
+        external_name = mesh%name
+        call get_option(trim(mesh%option_path) // '/from_file/file_name', external_filename)
+      end if
+
+      if (have_option(trim(mesh%option_path) // '/from_mesh/periodic_boundary_conditions')) then
+        seen_periodic_meshes = seen_periodic_meshes + 1
+        periodic_name = mesh%name
+      end if
+    end do
+
+    if (seen_external_meshes /= 1 .or. seen_periodic_meshes /= 1) then
+      call usage
+      stop
+    end if
+  end subroutine check_valid_input
+
+  subroutine postprocess_periodic_mesh(external_mesh, external_positions, periodic_mesh, periodic_positions)
+    type(mesh_type), intent(in) :: periodic_mesh, external_mesh
+    type(vector_field), intent(inout) :: periodic_positions
+    type(vector_field), intent(in) :: external_positions
+
+    integer :: periodic_bc, aliased_id, j
+    integer, dimension(2) :: shape_option
+    integer, dimension(:), allocatable :: aliased_boundary_ids
+    integer :: face
+
+    ! Retain the original surface IDs, as we still need them --
+    ! the derivation process will have set the aliased surface elements
+    ! to have surface ID zero
+    periodic_mesh%faces%boundary_ids = external_mesh%faces%boundary_ids
+
+    ! We need to loop through all aliased faces and set the periodic positions to
+    ! the aliased values, so that we can recover them by applying the map later
+
+    do periodic_bc=0,option_count(trim(periodic_mesh%option_path) // '/from_mesh/periodic_boundary_conditions')-1
+      shape_option = option_shape(trim(periodic_mesh%option_path) // '/from_mesh/periodic_boundary_conditions['//int2str(periodic_bc)//']/aliased_boundary_ids') 
+      allocate(aliased_boundary_ids(shape_option(1)))
+      call get_option(trim(periodic_mesh%option_path) // '/from_mesh/periodic_boundary_conditions[' // int2str(periodic_bc) // ']/aliased_boundary_ids', aliased_boundary_ids)
+      do j=1,shape_option(1)
+        aliased_id = aliased_boundary_ids(j)
+        ! Now we can finally loop over faces with this id and set appropriately
+        do face=1,surface_element_count(external_mesh)
+          if (surface_element_id(external_mesh, face) == aliased_id) then
+            call set(periodic_positions, face_global_nodes(periodic_positions, face), face_val(external_positions, face))
+          end if
+        end do
+      end do
+      deallocate(aliased_boundary_ids)
+    end do
+
+  end subroutine postprocess_periodic_mesh
 
 end program periodise
