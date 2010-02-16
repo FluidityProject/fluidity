39f68d0e6b70b61a12c9bedfa49508b20102717d
maddison <>
2010-02-16 19:12:18 +0000

    Some vector decomposition diagnostics

--- diagnostics/Momentum_Diagnostics.F90
+++ diagnostics/Momentum_Diagnostics.F90
@@ -29,13 +29,16 @@
 
 module momentum_diagnostics
 
+  use boundary_conditions
   use coriolis_module, only : two_omega => coriolis
+  use diagnostic_source_fields
   use field_options
   use fields
   use fldebug
   use global_parameters, only : OPTION_PATH_LEN
   use multimaterial_module
   use solvers
+  use sparsity_patterns_meshes
   use spud
   use state_fields_module
   use state_module
@@ -44,7 +47,8 @@ module momentum_diagnostics
   
   private
   
-  public :: calculate_bulk_viscosity, calculate_buoyancy, calculate_coriolis
+  public :: calculate_bulk_viscosity, calculate_buoyancy, calculate_coriolis, &
+    & calculate_scalar_potential, calculate_vector_potential
   
   interface coriolis_force
     module procedure coriolis_force_single, coriolis_force_multiple
@@ -276,5 +280,152 @@ contains
     end select
     
   end function coriolis_force_multiple
+  
+  subroutine calculate_scalar_potential(state, s_field)
+    type(state_type), intent(inout) :: state
+    type(scalar_field), intent(inout) :: s_field
+
+    integer :: i, ref_node, stat
+    type(csr_sparsity), pointer :: sparsity
+    type(csr_matrix) :: matrix
+    type(scalar_field) :: rhs
+    type(vector_field), pointer :: positions, source_field
+
+    positions => extract_vector_field(state, "Coordinate")
+    if(continuity(s_field) /= 0) then
+      FLExit("Can only compute scalar potentials on a continuous mesh")
+    end if
+    source_field => vector_source_field(state, s_field)
+
+    sparsity => get_csr_sparsity_firstorder(state, s_field%mesh, s_field%mesh)
+    ! Could cache this
+    call allocate(matrix, sparsity, name = "LaplacianMatrix")
+    call allocate(rhs, s_field%mesh, "ScalarPotentialRHS")
+
+    call zero(matrix)
+    call zero(rhs)
+    do i = 1, ele_count(rhs)
+      call assemble_scalar_potential_ele(i, positions, source_field, matrix, rhs)
+    end do
+
+    call get_option(trim(complete_field_path(s_field%option_path)) // "/algorithm/reference_node", ref_node, stat = stat)
+    if(stat == SPUD_NO_ERROR) call set_reference_node(matrix, ref_node, rhs = rhs)
+
+    call petsc_solve(s_field, matrix, rhs, option_path = trim(complete_field_path(s_field%option_path)) // "/algorithm")
+
+    call deallocate(matrix)
+    call deallocate(rhs)
+
+  contains
+
+    subroutine assemble_scalar_potential_ele(ele, positions, source_field, matrix, rhs)
+      integer, intent(in) :: ele
+      type(vector_field), intent(in) :: positions
+      type(vector_field), intent(in) :: source_field
+      type(csr_matrix), intent(inout) :: matrix
+      type(scalar_field), intent(inout) :: rhs
+
+      integer, dimension(:), pointer :: nodes
+      real, dimension(ele_ngi(rhs, ele)) :: detwei
+      real, dimension(ele_loc(rhs, ele), ele_ngi(rhs, ele), positions%dim) :: dshape_f
+      real, dimension(ele_loc(rhs, ele), ele_ngi(rhs, ele), positions%dim) :: dshape
+      type(element_type), pointer :: shape, shape_f
+
+      shape => ele_shape(rhs, ele)
+      shape_f => ele_shape(source_field, ele)
+
+      call transform_to_physical(positions, ele, shape, &
+        & dshape = dshape, detwei = detwei)
+      if(shape == shape_f) then
+        dshape_f = dshape
+      else
+        call transform_to_physical(positions, ele, shape_f, &
+          & dshape = dshape_f)
+      end if
+
+      nodes => ele_nodes(rhs, ele)
+
+      call addto(matrix, nodes, nodes, dshape_dot_dshape(dshape, dshape, detwei))
+      call addto(rhs, nodes, shape_rhs(shape, ele_div_at_quad(source_field, ele, dshape_f) * detwei))
+
+    end subroutine assemble_scalar_potential_ele
+
+  end subroutine calculate_scalar_potential
+
+  subroutine calculate_vector_potential(state, v_field)
+    type(state_type), intent(inout) :: state
+    type(vector_field), intent(inout) :: v_field
+    
+    integer :: i
+    type(csr_sparsity), pointer :: sparsity
+    type(csr_matrix) :: matrix
+    type(vector_field) :: rhs
+    type(vector_field), pointer :: positions, source_field
+    
+    positions => extract_vector_field(state, "Coordinate")
+    if(positions%dim /= 3) then
+      FLExit("Can only compute vector potentials in 3D")
+    else if(continuity(v_field) /= 0) then
+      FLExit("Can only compute vector potentials on a continuous mesh")
+    end if
+    source_field => vector_source_field(state, v_field)
+    
+    sparsity => get_csr_sparsity_firstorder(state, v_field%mesh, v_field%mesh)
+    ! Could cache this
+    call allocate(matrix, sparsity, name = "LaplacianMatrix")
+    call allocate(rhs, v_field%dim, v_field%mesh, "VectorPotentialRHS")
+    
+    call zero(matrix)
+    call zero(rhs)
+    do i = 1, ele_count(rhs)
+      call assemble_vector_potential_ele(i, positions, source_field, matrix, rhs)
+    end do
+
+    ! Strong Dirichlet bc of zero on all boundaries
+    do i = 1, surface_element_count(rhs)
+      call set_inactive(matrix, face_global_nodes(rhs, i))
+      call set(rhs, face_global_nodes(rhs, i), spread(spread(0.0, 1, face_loc(rhs, i)), 1, rhs%dim))
+    end do
+
+    call petsc_solve(v_field, matrix, rhs, option_path = trim(complete_field_path(v_field%option_path)) // "/algorithm")
+    
+    call deallocate(matrix)
+    call deallocate(rhs)
+    
+  contains
+
+    subroutine assemble_vector_potential_ele(ele, positions, source_field, matrix, rhs)
+      integer, intent(in) :: ele
+      type(vector_field), intent(in) :: positions
+      type(vector_field), intent(in) :: source_field
+      type(csr_matrix), intent(inout) :: matrix
+      type(vector_field), intent(inout) :: rhs
+
+      integer, dimension(:), pointer :: nodes
+      real, dimension(ele_ngi(rhs, ele)) :: detwei
+      real, dimension(ele_loc(rhs, ele), ele_ngi(rhs, ele), positions%dim) :: dshape_f
+      real, dimension(ele_loc(rhs, ele), ele_ngi(rhs, ele), positions%dim) :: dshape
+      type(element_type), pointer :: shape, shape_f
+
+      shape => ele_shape(rhs, ele)
+      shape_f => ele_shape(source_field, ele)
+
+      call transform_to_physical(positions, ele, shape, &
+        & dshape = dshape, detwei = detwei)
+      if(shape == shape_f) then
+        dshape_f = dshape
+      else
+        call transform_to_physical(positions, ele, shape_f, &
+          & dshape = dshape_f)
+      end if
+
+      nodes => ele_nodes(rhs, ele)
+
+      call addto(matrix, nodes, nodes, dshape_dot_dshape(dshape, dshape, detwei))
+      call addto(rhs, nodes, shape_vector_rhs(shape, ele_curl_at_quad(source_field, ele, dshape_f), detwei))
+
+    end subroutine assemble_vector_potential_ele
+
+  end subroutine calculate_vector_potential
 
 end module momentum_diagnostics
