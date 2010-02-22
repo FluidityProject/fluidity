57d50743571eaa2dd4672610739233d4a93697d5
pfarrell <>
2010-02-22 15:07:32 +0000

    Move postprocess_periodic_mesh from periodise to populate_state. Making a few more things public in populate_state

--- preprocessor/Populate_State.F90
+++ preprocessor/Populate_State.F90
@@ -67,7 +67,9 @@ module populate_state_module
        initialise_prognostic_fields, set_prescribed_field_values, &
        alias_fields, mesh_name, &
        allocate_and_insert_auxilliary_fields, &
-       initialise_field, allocate_metric_limits
+       initialise_field, allocate_metric_limits, &
+       make_mesh_periodic_from_options, make_mesh_unperiodic_from_options, &
+       postprocess_periodic_mesh
 
   interface allocate_field_as_constant
     
@@ -3173,5 +3175,114 @@ contains
       quad_family = FAMILY_COOLS
     end if
   end function get_quad_family
+
+  subroutine postprocess_periodic_mesh(external_mesh, external_positions, periodic_mesh, periodic_positions)
+    type(mesh_type), intent(inout) :: periodic_mesh
+    type(mesh_type), intent(in) :: external_mesh
+    type(vector_field), intent(inout) :: periodic_positions
+    type(vector_field), intent(in) :: external_positions
+
+    type(integer_set):: physical_bc_nodes
+    integer :: periodic_bc, aliased_id, physical_id, j
+    integer, dimension(2) :: shape_option
+    integer, dimension(:), allocatable :: aliased_boundary_ids, physical_boundary_ids
+    integer :: face
+    integer :: ele
+    real, dimension(ele_ngi(periodic_positions, 1)) :: detwei
+    real :: vol
+
+    ! Check for degenerate elements
+    do ele=1,ele_count(periodic_positions)
+      call transform_to_physical(periodic_positions, ele, detwei=detwei)
+      vol = sum(detwei)
+      assert(vol > 0)
+    end do
+
+    ! Retain the original surface IDs, as we still need them --
+    ! the derivation process will have set the aliased surface elements
+    ! to have surface ID zero
+    periodic_mesh%faces%boundary_ids = external_mesh%faces%boundary_ids
+
+
+    ! First we need to create a set of all nodes that are on physical periodic 
+    ! boundaries. If these nodes (in the non-periodic mesh) also appear on 
+    ! aliased boundaries (happens for double periodic), they should not be 
+    ! used to determine the position in the periodic coordinate field
+    
+    call allocate(physical_bc_nodes)
+    do periodic_bc=0,option_count(trim(periodic_mesh%option_path) // '/from_mesh/periodic_boundary_conditions')-1
+      shape_option = option_shape(trim(periodic_mesh%option_path) // '/from_mesh/periodic_boundary_conditions['//int2str(periodic_bc)//']/physical_boundary_ids')
+      allocate(physical_boundary_ids(shape_option(1)))
+      call get_option(trim(periodic_mesh%option_path) // '/from_mesh/periodic_boundary_conditions[' // int2str(periodic_bc) // ']/physical_boundary_ids', physical_boundary_ids)
+      do j=1, shape_option(1)
+        physical_id = physical_boundary_ids(j)
+        ! Now we can finally loop over faces with this id and set appropriately
+        do face=1, surface_element_count(external_mesh)
+          if (surface_element_id(external_mesh, face) == physical_id) then
+            call insert(physical_bc_nodes, face_global_nodes(external_mesh, face))
+          end if
+        end do
+      end do
+      deallocate(physical_boundary_ids)
+    end do
+
+    ! We need to loop through all aliased faces and set the periodic positions to
+    ! the aliased values, so that we can recover them by applying the map later
+    
+    do periodic_bc=0,option_count(trim(periodic_mesh%option_path) // '/from_mesh/periodic_boundary_conditions')-1
+      shape_option = option_shape(trim(periodic_mesh%option_path) // '/from_mesh/periodic_boundary_conditions['//int2str(periodic_bc)//']/aliased_boundary_ids') 
+      allocate(aliased_boundary_ids(shape_option(1)))
+      call get_option(trim(periodic_mesh%option_path) // '/from_mesh/periodic_boundary_conditions[' // int2str(periodic_bc) // ']/aliased_boundary_ids', aliased_boundary_ids)
+      do j=1,shape_option(1)
+        aliased_id = aliased_boundary_ids(j)
+        ! Now we can finally loop over faces with this id and set appropriately
+        do face=1,surface_element_count(external_mesh)
+          call postprocess_periodic_mesh_face(periodic_positions, external_positions, face, physical_bc_nodes)
+        end do
+      end do
+      deallocate(aliased_boundary_ids)
+      
+    end do
+      
+    call deallocate(physical_bc_nodes)
+
+    ! Check for degenerate elements
+    do ele=1,ele_count(periodic_positions)
+      call transform_to_physical(periodic_positions, ele, detwei=detwei)
+      vol = sum(detwei)
+      assert(vol > 0)
+    end do
+    
+  end subroutine postprocess_periodic_mesh
+
+  subroutine postprocess_periodic_mesh_face(periodic_positions, external_positions, face, physical_bc_nodes)
+    ! Write the positions of the nodes on this aliased face
+    ! to the periodic_positions. The physical positions can later be
+    ! recovered by applying the coordinate map. Nodes that are on both
+    ! a physical and an aliased face (happens for double/triple periodic)
+    ! should not be written, as they have another copy, going back
+    ! applying the inverse of the coordinate map associated with the 
+    ! physical face - and its that position we want to write out.
+    type(vector_field), intent(inout):: periodic_positions
+    type(vector_field), intent(in):: external_positions
+    integer, intent(in):: face
+    type(integer_set), intent(in):: physical_bc_nodes
+  
+    integer, dimension(face_loc(external_positions, face)):: face_non_periodic_nodes, face_periodic_nodes
+    integer:: k, np_node, p_node
+    
+    face_non_periodic_nodes=face_global_nodes(external_positions, face)
+    face_periodic_nodes=face_global_nodes(periodic_positions, face)
+    
+    do k=1, size(face_non_periodic_nodes)
+      np_node=face_non_periodic_nodes(k)
+      p_node=face_periodic_nodes(k)
+      if (.not. has_value(physical_bc_nodes, np_node)) then
+        call set(periodic_positions, p_node, node_val(external_positions, np_node))
+      end if
+    end do
+  
+  end subroutine postprocess_periodic_mesh_face
+  
 
 end module populate_state_module
