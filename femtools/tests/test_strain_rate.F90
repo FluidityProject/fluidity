6e3ae184d959af9ea1a13299083bbeb2bd65fd7f
skramer <>
2010-04-19 17:17:15 +0000

    Unittest for Giuseppe to test strain_rate (disabled for now).

---
+++ femtools/tests/test_strain_rate.F90
@@ -0,0 +1,75 @@
+subroutine test_strain_rate
+
+  use fields
+  use field_derivatives
+  use vtk_interfaces
+  use state_module
+  use unittest_tools
+  implicit none
+
+  type(vector_field) :: field
+  type(tensor_field) :: strain_rate_field, solution_field, diff_field
+  type(state_type) :: state
+  type(mesh_type), pointer :: mesh
+  type(vector_field), pointer :: positions
+  logical :: fail
+
+  interface
+    function velocity(pos)
+      real, dimension(:) :: pos
+      real, dimension(3) :: velocity
+    end function
+    function solution(pos)
+      real, dimension(:) :: pos
+      real, dimension(3,3) :: solution
+    end function
+  end interface
+
+  call vtk_read_state("data/cube_unstructured_0.vtu", state)
+  mesh => extract_mesh(state, "Mesh")
+  positions => extract_vector_field(state, "Coordinate")
+  call allocate(field, 3, mesh, "Field")
+  call allocate(strain_rate_field, mesh, "StrainRate")
+  call allocate(solution_field, mesh, "Solution")
+  call allocate(diff_field, mesh, "Difference")
+
+  ! set our input velocity
+  call set_from_function(field, velocity, positions)
+  ! compute the strain rate
+  !call strain_rate(field, positions, strain_rate_field)
+  ! now compute the expected solution
+  call set_from_function(solution_field, solution, positions)
+  
+  call set(diff_field, strain_rate_field)
+  call addto(diff_field, solution_field, scale=-1.0)
+  
+  call vtk_write_fields("data/strain_rate_out", 0, positions, mesh, &
+     vfields=(/ field/), tfields=(/ strain_rate_field, solution_field, diff_field /))
+  
+  fail = maxval( abs( diff_field%val ))< 1e-10
+  call report_test("[strain_rate]", fail, .false., "strain_rate different than expected")
+
+end subroutine test_strain_rate
+
+function velocity(pos)
+  real, dimension(3) :: velocity
+  real, dimension(:) :: pos
+  real :: x,y,z
+  x = pos(1); y = pos(2); z = pos(3)
+
+  velocity(1) = x + 2*y + 3*z
+  velocity(2) = 4*x + 5*y + 6*z
+  velocity(3) = 7*x + 8*y + 9*z
+  
+end function velocity
+
+function solution(pos)
+  real, dimension(3,3) :: solution
+  real, dimension(:) :: pos
+  real :: x,y,z
+  x = pos(1); y = pos(2); z = pos(3)
+
+  solution(1,1)=0
+  !...
+  
+end function solution
