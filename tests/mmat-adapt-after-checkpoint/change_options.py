d00ad83cbfa2e6e58d39d1f581bd05d0f4e3b118
cwilson <>
2010-05-02 14:46:24 +0000

    A hideous hack to get adapt_at_first_timestep turned back on after checkpointing in this test case as it depends on it... in fact, that's what it tests.

--- tests/mmat-adapt-after-checkpoint/change_options.py
+++ tests/mmat-adapt-after-checkpoint/change_options.py
@@ -4,6 +4,14 @@ import sys
 
 file_name = ["mmat-interpolation_2_checkpoint.flml"]
 
+append_options = []
+append_options.append("            <adapt_at_first_timestep>\n")
+append_options.append("              <number_of_adapts>\n")
+append_options.append("                <integer_value rank=\"0\">2</integer_value>\n")
+append_options.append("              </number_of_adapts>\n")
+append_options.append("            </adapt_at_first_timestep>\n")
+
+
 for f in range(len(file_name)):
   try:
     flml_file = file(file_name[f], "r")
@@ -20,6 +28,16 @@ for f in range(len(file_name)):
         i += 1
       if i < len(flml_options):
         flml_options[i] = ""
+      break
+
+  for i in range(len(append_options)):
+    flml_options.append("\n")
+
+  for i in range(len(flml_options)-1,-1,-1):
+    if "</hr_adaptivity>" in flml_options[i]:
+      flml_options[i+len(append_options):len(flml_options)-len(append_options)] = flml_options[i:-len(append_options)]
+      for j in range(len(append_options)):
+        flml_options[i+j] = append_options[j]
       break
   
   try:
