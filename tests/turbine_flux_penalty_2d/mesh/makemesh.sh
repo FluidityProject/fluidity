3d05873b0a99f053297e215f63f6aa468a6287fe
sf1409 <>
2010-05-10 21:19:54 +0000

    fixed path issue with gmsh2triangle

--- tests/turbine_flux_penalty_2d/mesh/makemesh.sh
+++ tests/turbine_flux_penalty_2d/mesh/makemesh.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 gmsh mesh4.geo -2
-gmsh2triangle mesh4.msh -2
+../../../scripts/gmsh2triangle mesh4.msh -2
 scripts/triangle_add_edgeowner.py mesh4
 scripts/triangle_remove_superposed_nodes2.py mesh4_edgow 24
 
 gmsh mesh_connected.geo -2
-gmsh2triangle mesh_connected.msh -2
+../../../scripts/gmsh2triangle mesh_connected.msh -2
 
