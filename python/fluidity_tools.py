b4e44aa84dbfeb4373398b8d49053f4dcdc5310e
dham <>
2010-05-19 14:29:57 +0000

    changes needed for embedded interactive python. Documentation will follow

--- python/fluidity_tools.py
+++ python/fluidity_tools.py
@@ -368,3 +368,25 @@ if __name__ == "__main__":
     import sys
     var = parse_s(sys.argv[1])
     print `var`
+
+def shell():
+  '''
+  shell()
+
+  Return ipython shell. To actually start the shell, invoke the function
+  returned by this function.
+
+  This is particularly useful for debugging embedded
+  python or for crawling over the data when something has gone wrong.
+  '''
+  import sys
+  
+  if not hasattr(sys,"argv"):
+    sys.argv=[]
+
+  from IPython.Shell import IPShellEmbed
+
+  ipshell = IPShellEmbed()
+
+  return ipshell
+  
