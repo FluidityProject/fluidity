31e1d66ea4b3f75ff4e3605456a3d98d419be2e1
dp204 <>
2010-05-24 15:23:58 +0000

    more one-way femdem, start on parallel

--- assemble/Implicit_Solids.F90
+++ assemble/Implicit_Solids.F90
@@ -37,9 +37,10 @@ module implicit_solids
   use tetrahedron_intersection_module
   use unify_meshes_module
   use unittest_tools
-  use global_parameters, only: FIELD_NAME_LEN
+  use global_parameters, only: FIELD_NAME_LEN, current_time
   use spud
   use read_triangle
+  use timeloop_utilities
 
   implicit none
 
@@ -54,7 +55,7 @@ contains
     type(state_type),intent(inout) :: state
     character(len=field_name_len) :: external_mesh_name
     type(vector_field), pointer :: positions
-    type(vector_field) :: external_positions
+    type(vector_field), save :: external_positions
     type(scalar_field), pointer :: solid
     integer :: ele_A, ele_B, ele_C
     type(tet_type) :: tet_A, tet_B
@@ -63,9 +64,10 @@ contains
     integer :: ntests
     integer, dimension(:), pointer :: ele_A_nodes
     type(vector_field) :: intersection
-    real, dimension(:), allocatable :: detwei_v, detwei
     real, dimension(:,:), allocatable :: pos_A
-    real :: vol, max_solid
+    real, dimension(:), allocatable :: detwei
+    real :: vol, ele_A_vol
+    logical, save :: init=.false.
 
     ewrite(3, *) "inside femdem"
 
@@ -74,15 +76,16 @@ contains
 
     positions => extract_vector_field(state, "Coordinate")
 
-    call get_option("/implicit_solids/mesh_name", external_mesh_name)
-    external_positions = &
-         read_triangle_files(trim(external_mesh_name), quad_degree=1)
+    if (.not. init) then
+       call get_option("/implicit_solids/mesh_name", external_mesh_name)
+       external_positions = &
+            read_triangle_serial(trim(external_mesh_name), quad_degree=1)
+       
+       assert(positions%dim >= 2)
+       assert(positions%dim == external_positions%dim)
 
-    assert(positions%dim >= 2)
-    assert(positions%dim == external_positions%dim)
-
-    allocate(detwei_v(face_ngi(external_positions, 1)))
-    allocate(detwei(ele_ngi(external_positions, 1)))
+       init=.true.
+    end if
 
     call rtree_intersection_finder_set_input(positions)
 
@@ -93,6 +96,8 @@ contains
 
        if (positions%dim == 3) then
           tet_B%v = ele_val(external_positions, ele_B)
+       else
+          call intersector_set_dimension(positions%dim)
        end if
 
        do j = 1, nintersections
@@ -110,12 +115,12 @@ contains
 
           else
 
-             call intersector_set_dimension(positions%dim)
              allocate(pos_A(positions%dim, ele_loc(positions, ele_A)))
              pos_A = ele_val(positions, ele_A)
              intersection = intersect_elements(external_positions, &
-                  ele_B, pos_A, ele_shape(positions, ele_A) )
+                  ele_B, pos_A, ele_shape(external_positions, ele_B))
              deallocate(pos_A)
+             stat = 0
 
           end if
 
@@ -126,31 +131,265 @@ contains
              vol = vol + abs(simplex_volume(intersection, ele_C))
           end do
 
+          allocate(detwei(ele_ngi(positions, ele_A)))
+          !allocate(detwei(ele_ngi(external_positions, ele_B)))
+          call transform_to_physical(positions, ele_A, detwei=detwei)
+          !call transform_to_physical(external_positions, ele_B, detwei=detwei)
+          ele_A_vol = sum(detwei)
+          deallocate(detwei)
+
           ele_A_nodes => ele_nodes(positions, ele_A)
           do k = 1, size(ele_A_nodes)
-             call addto(solid, ele_A_nodes(k), vol)
+             call addto(solid, ele_A_nodes(k), vol/ele_A_vol)
           end do
-
+          
           call deallocate(intersection)
 
        end do
 
     end do
 
-    max_solid = max( tiny(0.), maxval(solid) )
     do i = 1, node_count(solid)
-       call set(solid, i, node_val(solid, i)/max_solid)
+       call set(solid, i, min(1., node_val(solid, i)))
     end do
 
     ewrite_minmax(solid)
 
-    call deallocate(external_positions)
+    if(simulation_completed(current_time)) call deallocate(external_positions)
     call finalise_tet_intersector
     call rtree_intersection_finder_reset(ntests)
-    deallocate(detwei, detwei_v)
 
     ewrite(3, *) "leaving femdem"
 
   end subroutine solids
+
+  function read_triangle_serial(filename, quad_degree) result (field)
+    implicit none
+
+    character(len=*), intent(in) :: filename
+    !! The degree of the quadrature.
+    integer, intent(in), optional, target :: quad_degree
+    !! The degree of the quadrature.
+
+    type(vector_field) :: field
+    type(quadrature_type) :: quad
+    type(element_type) :: shape
+
+    integer :: dim, loc
+
+    call identify_triangle_file(filename, dim, loc)
+    quad=make_quadrature(loc, dim, degree=quad_degree)
+    shape=make_element_shape(loc, dim, 1, quad)
+    field=read_triangle_files_serial(filename, shape)
+
+    ! deallocate our references of shape and quadrature:
+    ! NOTE: we're using the specific deallocate interface here
+    !       to make the intel compiler shut up
+    call deallocate_element(shape)
+    call deallocate(quad)
+
+  end function read_triangle_serial
+
+
+  function read_triangle_files_serial(filename, shape) result (field)
+    !!< Filename is the base name of the triangle file without .node or .ele.
+
+    character(len=*), intent(in) :: filename
+    type(element_type), intent(in), target :: shape
+    type(vector_field) :: field
+
+    integer :: node_unit, ele_unit
+    real, allocatable, dimension(:) :: read_buffer
+    integer, allocatable, dimension(:,:) :: edge_buffer
+    integer, allocatable, dimension(:) :: sndglno
+    integer, allocatable, dimension(:) :: boundary_ids, element_owner
+
+    character(len = parallel_filename_len(filename)) :: lfilename
+    integer :: i, j, nodes, dim, node_attributes, boundaries, &
+         ele_attributes, loc, sloc, elements, edges, edge_count
+    integer, allocatable, dimension(:):: node_order
+    logical :: file_exists
+    type(mesh_type) :: mesh
+
+    lfilename = trim(filename)
+
+    node_unit=free_unit()
+
+    ewrite(2, *) "Opening "//trim(lfilename)//".node for reading."
+    ! Open node file
+    open(unit=node_unit, file=trim(lfilename)//".node", err=42, action="read")
+
+    ! Read node file header.
+    read (node_unit, *) nodes, dim, node_attributes, boundaries
+
+    ele_unit=free_unit()
+
+    ewrite(2, *) "Opening "//trim(lfilename)//".ele for reading."
+    ! Open element file
+    open(unit=ele_unit, file=trim(lfilename)//".ele", err=43, action="read")
+
+    ! Read element file header.
+    read (ele_unit, *) elements, loc, ele_attributes
+
+    assert(loc==shape%loc)
+    allocate(node_order(loc))
+    select case(loc)
+    case(3)
+       node_order = (/1,2,3/)
+    case default
+       do j = 1, loc
+          node_order(j) = j
+       end do
+    end select
+
+    call allocate(mesh, nodes, elements, shape, name="CoordinateMesh")
+    call allocate(field, dim, mesh, name="Coordinate")
+
+    ! Drop the local reference to mesh - now field owns the only reference.
+    call deallocate(mesh)
+
+    allocate(read_buffer(dim+node_attributes+boundaries+1))
+
+    if(node_attributes==1) then ! this assumes the node attribute are column numbers
+       allocate(field%mesh%columns(1:nodes))
+    end if
+
+    do i = 1, nodes
+       read(node_unit,*) read_buffer
+       forall (j=1:dim)
+          field%val(j)%ptr(i)=read_buffer(j+1)
+       end forall
+       if (node_attributes==1) then
+          field%mesh%columns(i)=floor(read_buffer(dim+1))
+       end if
+    end do
+
+    deallocate(read_buffer)
+    allocate(read_buffer(loc+ele_attributes+1))
+
+    if(ele_attributes==1) then  ! this assumes that the element attribute is a region id
+       allocate(field%mesh%region_ids(1:elements))
+    end if
+
+    do i = 1, elements
+       read(ele_unit,*) read_buffer
+       field%mesh%ndglno((i-1)*loc+1:i*loc)=floor(read_buffer(node_order+1))
+       if(ele_attributes==1) then
+          field%mesh%region_ids(i)=read_buffer(loc+2)
+       end if
+    end do
+
+    close(node_unit)
+    close(ele_unit)
+
+    ! Open edge file
+    select case (dim)
+    case(2)
+       inquire(file=trim(lfilename)//".edge",exist=file_exists)
+       if(file_exists) then
+          ewrite(2, *) "Opening "//trim(lfilename)//".edge for reading."
+          open(unit=node_unit, file=trim(lfilename)//".edge", err=41, &
+               action="read")
+       end if
+    case(3)
+       inquire(file=trim(lfilename)//".face",exist=file_exists)
+       if(file_exists) then
+          ewrite(2, *) "Opening "//trim(lfilename)//".face for reading."
+          open(unit=node_unit, file=trim(lfilename)//".face", err=41, &
+               action="read")
+       end if
+    end select
+
+    if(file_exists) then
+       ! Read edge file header.
+       read (node_unit, *) edges, boundaries
+    else
+       edges = 0
+       boundaries = 1
+    end if
+
+    if(edges==0) then
+       file_exists = .false.
+       close(node_unit)
+    end if
+
+    select case(shape%numbering%family)
+    case(FAMILY_SIMPLEX)
+       if ((loc/=dim+1).and.(boundaries/=0)) then
+          ewrite(0,*) "Warning: triangle boundary markers not supported for qua", &
+               "dratic space elements."
+          if(file_exists) then
+             file_exists= .false.
+             close(node_unit)
+          end if
+       end if
+       sloc=loc-1
+    case default
+       FLAbort('Illegal element family')
+    end select
+
+    allocate(edge_buffer(sloc+boundaries+1,edges))
+    edge_buffer=0
+    allocate(sndglno(edges*sloc))
+    sndglno=0
+    allocate(boundary_ids(1:edges))
+    boundary_ids=0
+    if (boundaries==2) then
+       allocate(element_owner(1:edges))
+       element_owner=0
+    end if
+    edge_count=0
+
+    if (boundaries==0) then
+       ewrite(0,*) "Warning: triangle edge file has no boundary markers"
+       if(file_exists) then
+          file_exists=.false.
+          close(node_unit)
+       end if
+    else
+       if(file_exists) then
+          read(node_unit, *) edge_buffer
+          do i = 1, edges
+             if (edge_buffer(sloc+2,i)/=0) then
+                ! boundary edge/face
+                edge_count=edge_count+1
+                sndglno((edge_count-1)*sloc+1:edge_count*sloc)= &
+                     edge_buffer(2:sloc+1,i)
+                boundary_ids(edge_count)=edge_buffer(sloc+2,i)
+                if (boundaries==2) then
+                   element_owner(edge_count)=edge_buffer(sloc+3,i)
+                end if
+             end if
+          end do
+
+          file_exists=.false.
+          close(node_unit)
+       end if
+    end if
+
+    if (boundaries<2) then
+       call add_faces(field%mesh, &
+            sndgln=sndglno(1:edge_count*sloc), &
+            boundary_ids=boundary_ids(1:edge_count))
+    else
+       call add_faces(field%mesh, &
+            sndgln=sndglno(1:edge_count*sloc), &
+            boundary_ids=boundary_ids(1:edge_count), &
+            element_owner=element_owner)
+    end if
+
+    deallocate(edge_buffer)
+    deallocate(sndglno)
+    deallocate(boundary_ids)
+
+41  continue ! We jump to here if there was no edge file.
+
+    return
+
+42  FLAbort("Unable to open "//trim(lfilename)//".node")
+
+43  FLAbort("Unable to open "//trim(lfilename)//".ele")
+
+  end function read_triangle_files_serial
 
 end module implicit_solids
