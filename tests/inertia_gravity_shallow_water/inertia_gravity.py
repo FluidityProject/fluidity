07a47f7f7471cde6584732fd5137fdce0158bf40
dham <>
2010-07-05 11:42:58 +0000

    scripts for plotting convergence results

--- tests/inertia_gravity_shallow_water/inertia_gravity.py
+++ tests/inertia_gravity_shallow_water/inertia_gravity.py
@@ -125,6 +125,27 @@ def convergence_constant_dt(dx):
             
     return error
 
+def convergence_helmholtz(dx):
+
+    error = numpy.zeros(dx.size)
+
+    binary="../../bin/shallow_water"
+
+    for x in enumerate(dx):
+        generate_meshfile("square",x[1])
+
+        last_error=1.e100
+        print "Simulation with dx="+`x[1]`
+        
+        os.system(binary+" helmholtz.swml")
+            
+        s=stat_parser("inertia_gravity.stat")
+        this_error=s["Fluid"]['LayerThicknessError']['l2norm'][-1]
+        
+        error[x[0]]=this_error
+            
+    return error
+
 
 g=1
 H=1
@@ -171,3 +192,60 @@ def height_solution(k):
             (1j*numpy.dot(k,k)/omega)*phi*
             numpy.exp(1j*(numpy.dot(k,x)-omega*t))))
     return sol
+
+def helmholtz_rhs(k):
+    '''Return inertia gravity wave solution to the shallow water
+    equations for a given wave number vector k. The result is a
+    function of x and t.'''
+
+    omega=dispersion(k)
+
+    def sol(x,t):
+        return H*(numpy.real(
+            (1j*numpy.dot(k,k)/omega)*phi*
+            numpy.exp(1j*(numpy.dot(k,x)-omega*t))))
+    return sol
+
+def helmholtz_solution(k):
+
+    h=helmholtz_rhs(k)
+    
+    def sol(X,t):
+        return h(X,t)/(1.+numpy.dot(k,k))
+
+    return sol
+
+def plot_results(dx, error_p1p2, error_p1p2_poor):
+    '''plot_results(error)
+
+    Produce a plot of the actual errors provided in the argument
+    "error". 
+    '''
+    from pylab import \
+    plot,figure,quiver,frange,subplot,xticks,yticks,axis,xlabel,ylabel, \
+    subplots_adjust,loglog,legend
+
+    figure()
+
+    loglog(dx,error_p1p2_poor,'-xr')
+    loglog(dx,0.05*dx**2,'--r')
+    loglog(dx,error_p1p2,'-xk')
+    loglog(dx,0.01*dx**3,'--k')
+    
+    
+    yticks(yticks()[0], map(lambda x: "%3.1e"%x, yticks()[0]))
+    xticks(xticks()[0], map(lambda x: "%3.1e"%x, xticks()[0]))
+
+    xlabel("dx")
+
+    legend(("error (interpolated ICs)",
+            "O(dx^2)",
+            "error (projected ICs)",
+            "O(dx^3)"),loc="lower right")
+
+def plot_error_from_file():
+    import pickle
+
+    a=pickle.load(file("error","r"))
+
+    plot_results(a[4],a[0],a[2])
