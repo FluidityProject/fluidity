3b7a239614acbbbb8b0acbc5f46f4c143463680a
maddison <>
2010-07-09 10:10:31 +0000

    Simple tool to probe simplex vtu at a coordinate. Has much better behaviour at domain boundaries than vtktools.vtu.ProbeData.

---
+++ tools/Probe_Vtu.F90
@@ -0,0 +1,121 @@
+!    Copyright (C) 2006 Imperial College London and others.
+!    
+!    Please see the AUTHORS file in the main source directory for a full list
+!    of copyright holders.
+!
+!    Prof. C Pain
+!    Applied Modelling and Computation Group
+!    Department of Earth Science and Engineeringp
+!    Imperial College London
+!
+!    C.Pain@Imperial.ac.uk
+!    
+!    This library is free software; you can redistribute it and/or
+!    modify it under the terms of the GNU Lesser General Public
+!    License as published by the Free Software Foundation,
+!    version 2.1 of the License.
+!
+!    This library is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!    Lesser General Public License for more details.
+!
+!    You should have received a copy of the GNU Lesser General Public
+!    License along with this library; if not, write to the Free Software
+!    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+!    USA
+
+#include "fdebug.h"
+
+subroutine probe_vtu(vtu_filename, vtu_filename_len, fieldname, fieldname_len, x, y, z, dim)
+
+  use fields
+  use fldebug
+  use futils
+  use pickers
+  use reference_counting
+  use state_module
+  use vtk_interfaces
+  
+  implicit none
+  
+  integer, intent(in) :: vtu_filename_len
+  integer, intent(in) :: fieldname_len
+  
+  character(len = vtu_filename_len), intent(in) :: vtu_filename
+  character(len = fieldname_len), intent(in) :: fieldname
+  real, intent(in) :: x
+  real, intent(in) :: y
+  real, intent(in) :: z
+  integer, intent(in) :: dim
+  
+  character(len = 1 + 1 + real_format_len(padding = 1) + 1) :: format
+  integer :: ele, stat
+  logical :: allocated
+  real :: s_val
+  real, dimension(dim) :: coord, v_val
+  real, dimension(dim, dim) :: t_val
+  real, dimension(:), allocatable :: local_coord
+  type(scalar_field), pointer :: s_field
+  type(vector_field), pointer :: positions, v_field
+  type(state_type) :: state
+  type(tensor_field), pointer :: t_field
+  
+  ewrite(1, *) "In probe_vtu"
+  
+  call vtk_read_state(vtu_filename, state = state)
+  
+  positions => extract_vector_field(state, "Coordinate")
+  if(positions%dim /= dim) then
+    FLExit("Expected " // int2str(dim) // " dimensional probe coord")
+  else if(ele_numbering_family(ele_shape(positions, 1)) /= FAMILY_SIMPLEX) then
+    FLExit("Mesh in vtu " // vtu_filename // " is not composed of linear simplices")
+  end if
+  
+  if(dim > 0) coord(1) = x
+  if(dim > 1) coord(2) = y
+  if(dim > 2) coord(3) = z
+  ewrite(2, *) x, y, z
+  ewrite(2, *) "Probe coord: ", coord
+  allocate(local_coord(dim + 1))
+  call picker_inquire(positions, coord, ele, local_coord = local_coord)
+  if(ele < 0) then
+    FLExit("Probe point not contained in vtu " // vtu_filename)
+  end if
+  
+  s_field => extract_scalar_field(state, fieldname, allocated = allocated, stat = stat)
+  if(stat == 0) then
+    format = "(" // real_format() // ")"
+    s_val = eval_field(ele, s_field, local_coord)
+    ewrite(2, *) fieldname // " value:"
+    print format, s_val
+    if(allocated) deallocate(s_field)
+  else
+    v_field => extract_vector_field(state, fieldname, stat = stat)
+    if(stat == 0) then
+      format = "(" // int2str(dim) // real_format(padding = 1) // ")"
+      v_val = eval_field(ele, v_field, local_coord)
+      ewrite(2, *) fieldname // " value:"
+      print format, v_val
+    else
+      t_field => extract_tensor_field(state, fieldname, stat = stat)
+      if(stat == 0) then
+        format = "(" // int2str(dim ** 2) // real_format(padding = 1) // ")"
+        t_val = eval_field(ele, t_field, local_coord)
+        ewrite(2, *) fieldname // " value:"
+        print format, t_val
+      else
+        FLExit("Field " // fieldname // " not found in vtu " // vtu_filename)
+      end if
+    end if
+  end if
+  
+  deallocate(local_coord)
+  
+  call deallocate(state)
+  
+  call print_references(0)
+  
+  ewrite(1, *) "Exiting probe_vtu"
+
+end subroutine probe_vtu
---
+++ tools/Probe_Vtu_main.cpp
@@ -0,0 +1,146 @@
+/*  Copyright (C) 2006 Imperial College London and others.
+    
+    Please see the AUTHORS file in the main source directory for a full list
+    of copyright holders.
+
+    Prof. C Pain
+    Applied Modelling and Computation Group
+    Department of Earth Science and Engineering
+    Imperial College London
+
+    C.Pain@Imperial.ac.uk
+    
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation,
+    version 2.1 of the License.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+    USA
+*/
+#include "fmangle.h"
+#include "Usage.h"
+#include "Precision.h"
+
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif
+
+extern "C" {
+#define probe_vtu_fc F77_FUNC(probe_vtu, PROBE_VTU)
+  void probe_vtu_fc(const char*, int*, const char*, int*, flfloat_t*, flfloat_t*, flfloat_t*, int*);
+}
+
+#ifdef _AIX
+#include <unistd.h>
+#else
+#include <getopt.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std; 
+
+void project_vtu_usage(char *binary){
+  cerr<<"Usage: "<<binary<<" [OPTIONS] VTU FIELDNAME X Y Z\n"
+      <<"Probe a vtu at the specified coordinate\n"
+      <<"\t-h\t\tPrints out this message\n"
+      <<"\t-v\t\tVerbose mode\n";
+}
+
+int main(int argc, char **argv){
+ #ifdef HAVE_MPI
+  // This must be called before we process any arguments
+  MPI::Init(argc,argv);
+
+  // Undo some MPI init shenanigans
+  chdir(getenv("PWD"));
+#endif
+  
+  // Initialise PETSc (this also parses PETSc command line arguments)
+  PetscInit(argc, argv);
+ 
+  // Get any command line arguments
+  // reset optarg so we can detect changes
+  optarg = NULL;  
+  char c;
+  map<char, string> args;
+  while((c = getopt(argc, argv, "hv")) != -1){
+    if (c != '?'){
+      if (optarg == NULL){
+        args[c] = "true";
+      }else{
+        args[c] = optarg;
+      };
+    }else{
+      if (isprint(optopt)){
+        cerr << "Unknown option " << optopt << endl;
+      }else{
+        cerr << "Unknown option " << hex << optopt << endl;
+      }
+      project_vtu_usage(argv[0]);
+      exit(-1);
+    }
+  }
+
+  // Help?
+  if(args.count('h')){
+    project_vtu_usage(argv[0]);
+    exit(-1);
+  }
+  
+  if (optind >= argc - 2 or optind < argc - 5){
+    cerr << "Need between three and five non-option arguments" << endl;
+    project_vtu_usage(argv[0]);
+    exit(-1);
+  }
+
+  // What to do with stdout?
+  int val=3;
+  if(args.count('v')==0)
+    val = 0;
+  set_global_debug_level_fc(&val);
+
+  string vtu_filename = argv[optind];
+  int vtu_filename_len = vtu_filename.length();  
+  
+  string fieldname = argv[optind + 1];
+  int fieldname_len = fieldname.length();
+  
+  int dim = 1;
+  flfloat_t x, y = 0.0, z = 0.0;
+  
+  x = atof(argv[optind + 2]);  
+  if(optind < argc - 3){
+    y = atof(argv[optind + 3]);
+    dim++;
+  }  
+  if(optind < argc - 4){
+    z = atof(argv[optind + 4]);
+    dim++;
+  }
+  
+  probe_vtu_fc(vtu_filename.c_str(), &vtu_filename_len, fieldname.c_str(), &fieldname_len, &x, &y, &z, &dim);
+    
+#ifdef HAVE_PETSC
+  PetscFinalize();
+#endif
+
+#ifdef HAVE_MPI
+  MPI::Finalize();
+#endif
+  return(0);
+}
