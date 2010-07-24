3ec3527bc87f1999d9d5e393728ceff496bb8f24
maddison <>
2010-07-24 11:25:20 +0000

    Another python 2.3 fix

--- python/fluidity/diagnostics/optimise.py
+++ python/fluidity/diagnostics/optimise.py
@@ -14,7 +14,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-import ctypes
 import unittest
 
 import fluidity.diagnostics.debug as debug
