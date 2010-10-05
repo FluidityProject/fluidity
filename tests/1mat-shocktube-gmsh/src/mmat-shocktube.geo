01c9e5a594acbabb4d4cfae5f7546b97b4827f78
cwilson <>
2010-10-05 21:31:30 +0000

    These are short tests so may as well have gmsh versions rather than just changing the tests so that gmsh2triangle is still tested, especially as two of them are some of the only hex mesh tests.

---
+++ tests/1mat-shocktube-gmsh/src/mmat-shocktube.geo
@@ -0,0 +1,17 @@
+Point (1) = {0, -1.0, 0, 0.025};
+Extrude {0.2,0,0} {
+  Point{1}; Layers{1}; Recombine;
+}
+Extrude {0,2.0,0} {
+  Line{1}; Layers{40}; Recombine;
+}
+Extrude {0,0,0.2} {
+  Surface{5}; Layers{1}; Recombine;
+}
+Physical Surface(29) = {14};
+Physical Surface(30) = {18};
+Physical Surface(31) = {22};
+Physical Surface(32) = {26};
+Physical Surface(33) = {27};
+Physical Surface(34) = {5};
+Physical Volume(35) = {1};
---
+++ tests/1mat-shocktube-gmsh/transect.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import vtktools
+from numpy import concatenate, arange, newaxis, ones, array
+from pylab import plot, show, figure, axis
+import glob
+import operator
+
+vtus = glob.glob("1material_shocktube_*[0-9].vtu")
+
+colxz=ones((39,1))*0.005
+coly=arange(-0.95,0.99,0.05)[:,newaxis]
+coordinates=concatenate((colxz,coly,colxz),1)
+
+nums = []
+for i in range(len(vtus)):
+  nums.append([int(vtus[i].split(".vtu")[0].split("_")[-1]), i])
+nums.sort(key=operator.itemgetter(0))
+
+for i in range(len(vtus)):
+  vtufile=vtktools.vtu(vtus[nums[i][-1]])
+  newprobedpressure=vtktools.vtu.ProbeData(vtufile,coordinates,'Pressure')
+  #newprobedvelocity=vtktools.vtu.ProbeData(vtufile,coordinates,'Velocity')
+  newprobeddensity=vtktools.vtu.ProbeData(vtufile,coordinates,'Density')
+  newprobedmatnrg=vtktools.vtu.ProbeData(vtufile,coordinates,'InternalEnergy')
+  newnormmatnrg=(newprobedmatnrg-min(newprobedmatnrg))/(max(newprobedmatnrg)-min(newprobedmatnrg))
+  #plot(coordinates[:,1],newnormmatnrg,coordinates[:,1],newprobeddensity,coordinates[:,1],newprobedpressure,coordinates[:,1],newprobedvelocity[:,1])
+  figure(i)
+  plot(coordinates[:,1],newnormmatnrg,coordinates[:,1],newprobeddensity,coordinates[:,1],newprobedpressure)
+  axis([-1, 1, 0, 1])
+
+show()
