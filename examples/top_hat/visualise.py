2eb3e4844ad80091573b9fdf99e7933661659792
skramer <>
2010-10-25 15:41:17 +0000

    Updating 1d advection example.

---
+++ examples/top_hat/visualise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import pylab
+try:
+  import vtktools
+except ImportError:
+  sys.stderr.write("""You need to add the path to <FLUIDITY_SOURCE_LOCATION>/python/
+to your PYTHONPATH environment variable, e.g.:
+  export PYTHONPATH=$PYTHONPATH:$HOME/svn/fluidity/python/\n""")
+  sys.exit(1)
+  
+# open one of the vtus output by fluidity
+vt=vtktools.vtu("top_hat_cg_100.vtu")
+# returns an array containing the 3-d locations of the nodes in the mesh
+# of these only the first coordinate is used in 1D, i.e xyz[:,0]
+xyz=vt.GetLocations()
+x=xyz[:,0]
+# get the values of the "Tracer" field at the nodes
+T=vt.GetScalarField("Tracer")
+
+# plot Tracer T against x, black lines, grey round markers
+pylab.plot(x,T, 'black', markerfacecolor='grey', marker='o')
+# set axes and axes' labels
+pylab.axis([0.0,3.0,-0.1,1.1])
+pylab.xlabel('').set_size('xx-large')
+pylab.xlabel('$x$')
+pylab.ylabel('Tracer')
+# show the figure
+pylab.show()
