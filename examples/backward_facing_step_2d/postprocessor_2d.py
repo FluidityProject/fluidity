0ec85040add15a9a2243760eeceaf549ce80b4fd
jbull <>
2010-10-27 12:35:04 +0000

    Making postprocessor scripts consistent and neatening the structure and plots. Adding a .xml to each example, which will test finish time and variation of reattachment length. At present they're medium, but it's possible that the 3D run will go over the time limit.

--- examples/backward_facing_step_2d/postprocessor_2d.py
+++ examples/backward_facing_step_2d/postprocessor_2d.py
@@ -26,14 +26,13 @@ def get_filelist():
 ###################################################################
 
 # Reattachment length:
-def reatt_length(filelist, exclude_initial_results=130):
+def reatt_length(filelist, exclude_initial_results):
 
-  print "Running reattachment length script on selected vtu files...\n"
-  nums=[]
-  result=0.
-  filecount=0
+  print "Calculating reattachment point locations using change of x-velocity sign\n"
+  print "Processing pvtu files...\n"
+  nums=[]; results=[]
 
-  # check for no files
+  ##### check for no files
   if (len(filelist) == 0):
     print "No files!"
     sys.exit(1)
@@ -44,13 +43,13 @@ def reatt_length(filelist, exclude_initial_results=130):
       print "No such file: %s" % file
       sys.exit(1)
     num = int(file.split(".vtu")[0].split('_')[-1])
+    ##### Exclude data from simulation spin-up time
     if num > exclude_initial_results:
       nums.append(num)
     nums.sort()
 
   for num in nums:
     ##### counter for averaging
-    filecount = filecount + 1
     file = "backward_facing_step_2d_"+str(num)+".vtu"
 
     print file
@@ -61,41 +60,39 @@ def reatt_length(filelist, exclude_initial_results=130):
     data = reader.GetOutput()
     datafile = vtktools.vtu(file)
 
-    ##### specify points near bottom surface
-    pts=[]; no_pts = 100; x_0 = 5.0; offset = 0.1
-    pts.append((5.0, offset, 0.0))
+    ##### points near bottom surface, 0 < x < 20
+    pts=[]; no_pts = 82; offset = 0.1
+    x = 0.0
     for i in range(1, no_pts):
-      x = x_0 + i/5.0
       pts.append((x, offset, 0.0))
+      x += 0.25
+
+    pts = numpy.array(pts)
 
     ##### Get x-velocity on bottom boundary
-    uvw = datafile.ProbeData(vtktools.arr(pts), "Velocity")
-    u = []; surf = []
-    for i in range(len(uvw)):
-      u.append(uvw[i][0]); surf.append(pts[i][0])
+    uvw = datafile.ProbeData(pts, "Velocity")
+    u = []
+    u = uvw[:,0]
 
     ##### Find all potential reattachment points:
-    #print "calculating reattachment point locations using change of x-velocity sign"
     points = []
+
     for i in range(len(u)-1):
-      if((u[i] / u[i+1]) < 0.):
+      ##### Hack to ignore division by zero entries in u.
+      ##### All u should be nonzero away from boundary!
+      if((u[i] / u[i+1]) < 0. and not isinf(u[i] / u[i+1])):
         ##### interpolate between nodes
-        p = surf[i] + (surf[i+1]-surf[i]) * (0.0-u[i]) / (u[i+1]-u[i]) - 5.0
-        #print "potential reattachment point at x = ", p
-        points.append(p)
+        p = pts[i] + (pts[i+1]-pts[i]) * (0.0-u[i]) / (u[i+1]-u[i])
+        ##### Ignore spurious corner points
+        if(p>0.1):
+          points.append(p)
 
-    ##### Remove corner point, if present:
-    if(len(points) > 1):
-      if(points[0] < 0.1):
-        print "ignoring point at step corner"
-        points.remove(points[0])
+    ##### Get time for plot:
+    t = min(datafile.GetScalarField("Time"))
+    ##### Append actual reattachment point and time:
+    results.append([points[0],t])
 
-    ##### This is the actual reattachment point:
-    result = result + points[0]
-
-  ##### Average over selected files:
-  result = result / filecount
-  return result
+  return results
 
 #########################################################################
 
@@ -145,7 +142,7 @@ def meanvelo(filelist,x,y):
         profiles[filecount,:,:] = u
 
         ##### reset time to something big to prevent infinite loop
-        t = 100.
+        t = 1000.
         filecount += 1
 
   print "\n...Finished writing data files.\n"
@@ -153,74 +150,94 @@ def meanvelo(filelist,x,y):
 
 #########################################################################
 
-##### Call functions defined above
+def plot_length(reattachment_length):
+  ##### Plot time series of reattachment length using pylab(matplotlib)
+  plot1 = pylab.figure()
+  pylab.title("Time series of reattachment length behind 2D step")
+  pylab.xlabel('Time (s)')
+  pylab.ylabel('Reattachment Length (L/h)')
+  pylab.plot(reattachment_length[:,1], reattachment_length[:,0], marker = 'o', markerfacecolor='white', markersize=6, markeredgecolor='black', linestyle="solid")
+  pylab.savefig("reattachment_length_2D.pdf")
+  return
+
+def plot_meanvelo(profiles):
+  ##### Plot velocity profiles at different points behind step, and at 3 times using pylab(matplotlib)
+  plot1 = pylab.figure(figsize = (16.5, 8.5))
+  pylab.suptitle('Evolution of U-velocity profiles downstream of backward facing step', fontsize=30)
+
+  size = 15
+
+  ax = pylab.subplot(141)
+  ax.plot(profiles[0,0,:],yarray, color='green', linestyle="dashed")
+  ax.plot(profiles[1,0,:],yarray, color='blue', linestyle="dashed")
+  ax.plot(profiles[2,0,:],yarray, color='red', linestyle="dashed")
+  pylab.legend(("5secs", "10secs", "50secs"), loc="lower right")
+  ax.set_title('(a) x/h = 0', fontsize=16)
+  ax.grid("True")
+  for tick in ax.xaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+  for tick in ax.yaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+
+  bx = pylab.subplot(142, sharex=ax, sharey=ax)
+  bx.plot(profiles[0,1,:],yarray, color='green', linestyle="dashed")
+  bx.plot(profiles[1,1,:],yarray, color='blue', linestyle="dashed")
+  bx.plot(profiles[2,1,:],yarray, color='red', linestyle="dashed")
+  pylab.legend(("5secs", "10secs", "50secs"), loc="lower right")
+  bx.set_title('(b) x/h = 2', fontsize=16)
+  bx.grid("True")
+  for tick in bx.xaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+  pylab.setp(bx.get_yticklabels(), visible=False)
+
+  cx = pylab.subplot(143, sharex=ax, sharey=ax)
+  cx.plot(profiles[0,2,:],yarray, color='green', linestyle="dashed")
+  cx.plot(profiles[1,2,:],yarray, color='blue', linestyle="dashed")
+  cx.plot(profiles[2,2,:],yarray, color='red', linestyle="dashed")
+  pylab.legend(("5secs", "10secs", "50secs"), loc="lower right")
+  cx.set_title('(c) x/h = 4', fontsize=16)
+  cx.grid("True")
+  for tick in cx.xaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+  pylab.setp(cx.get_yticklabels(), visible=False)
+
+  dx = pylab.subplot(144, sharex=ax, sharey=ax)
+  dx.plot(profiles[0,3,:],yarray, color='green', linestyle="dashed")
+  dx.plot(profiles[1,3,:],yarray, color='blue', linestyle="dashed")
+  dx.plot(profiles[2,3,:],yarray, color='red', linestyle="dashed")
+  pylab.legend(("5secs", "10secs", "50secs"), loc="lower right")
+  dx.set_title('(d) x/h = 6', fontsize=16)
+  dx.grid("True")
+  for tick in dx.xaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+  pylab.setp(dx.get_yticklabels(), visible=False)
+
+  pylab.axis([-0.2, 1., 0., 2.])
+  bx.set_xlabel('Normalised U-velocity (U/Umax)', fontsize=24)
+  ax.set_ylabel('z/h', fontsize=24)
+
+  pylab.savefig("velo_profiles_2d.pdf")
+  return
+
+#########################################################################
+
+##### Call reattachment_length function defined above
 filelist = get_filelist()
-reattachment_length = reatt_length(filelist)
-print "\naverage reattachment length (in step heights): ", reattachment_length
+
+reattachment_length = numpy.array(reatt_length(filelist, exclude_initial_results=130))
+av_length = sum(reattachment_length[:,0]) / len(reattachment_length[:,0])
+numpy.save("reattachment_length_2D", reattachment_length)
+print "\nTime-averaged reattachment length (in step heights): ", av_length
+plot_length(reattachment_length)
 
 ##### Points to generate profiles:
 xarray = numpy.array([0.0, 2.0, 4.0, 6.0])
 yarray = numpy.array([0.01, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0])
 
+##### Call meanvelo function defined above
 profiles = meanvelo(filelist, xarray, yarray)
 numpy.save("velo_profiles_2d", profiles)
-
-#for t in range(profiles[:,0,0]):
-pylab.figure(num=1, figsize = (16.5, 8.5))
-pylab.suptitle('Evolution of U-velocity profiles downstream of backward facing step', fontsize=30)
-
-size = 15
-
-ax = pylab.subplot(141)
-ax.plot(profiles[0,0,:],yarray, color='green', linestyle="dashed")
-ax.plot(profiles[1,0,:],yarray, color='blue', linestyle="dashed")
-ax.plot(profiles[2,0,:],yarray, color='red', linestyle="dashed")
-pylab.legend(("5secs", "10secs", "50secs"), loc="lower right")
-ax.set_title('(a) x/h = 0', fontsize=16)
-ax.grid("True")
-for tick in ax.xaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-for tick in ax.yaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-
-bx = pylab.subplot(142, sharex=ax, sharey=ax)
-bx.plot(profiles[0,1,:],yarray, color='green', linestyle="dashed")
-bx.plot(profiles[1,1,:],yarray, color='blue', linestyle="dashed")
-bx.plot(profiles[2,1,:],yarray, color='red', linestyle="dashed")
-pylab.legend(("5secs", "10secs", "50secs"), loc="lower right")
-bx.set_title('(b) x/h = 2', fontsize=16)
-bx.grid("True")
-for tick in bx.xaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-pylab.setp(bx.get_yticklabels(), visible=False)
-
-cx = pylab.subplot(143, sharex=ax, sharey=ax)
-cx.plot(profiles[0,2,:],yarray, color='green', linestyle="dashed")
-cx.plot(profiles[1,2,:],yarray, color='blue', linestyle="dashed")
-cx.plot(profiles[2,2,:],yarray, color='red', linestyle="dashed")
-pylab.legend(("5secs", "10secs", "50secs"), loc="lower right")
-cx.set_title('(c) x/h = 4', fontsize=16)
-cx.grid("True")
-for tick in cx.xaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-pylab.setp(cx.get_yticklabels(), visible=False)
-
-dx = pylab.subplot(144, sharex=ax, sharey=ax)
-dx.plot(profiles[0,3,:],yarray, color='green', linestyle="dashed")
-dx.plot(profiles[1,3,:],yarray, color='blue', linestyle="dashed")
-dx.plot(profiles[2,3,:],yarray, color='red', linestyle="dashed")
-pylab.legend(("5secs", "10secs", "50secs"), loc="lower right")
-dx.set_title('(d) x/h = 6', fontsize=16)
-dx.grid("True")
-for tick in dx.xaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-pylab.setp(dx.get_yticklabels(), visible=False)
-
-pylab.axis([-0.2, 1., 0., 2.])
-bx.set_xlabel('Normalised U-velocity (U/Umax)', fontsize=24)
-ax.set_ylabel('z/h', fontsize=24)
-
-pylab.savefig("velo_profiles_2d.pdf")
+plot_meanvelo(profiles)
 pylab.show()
 
 print "\nAll done.\n"
--- examples/backward_facing_step_3d/postprocessor_3d_parallel.py
+++ examples/backward_facing_step_3d/postprocessor_3d_parallel.py
@@ -8,7 +8,6 @@ import vtktools
 import numpy
 import pylab
 from math import isinf
-import Numeric
 
 def get_filelist():
 
@@ -32,10 +31,8 @@ def reatt_length(filelist, yarray, exclude_initial_results):
 
   print "Calculating reattachment point locations using change of x-velocity sign\n"
   print "Processing pvtu files...\n"
-  nums=[]
-  results=[]
+  nums=[]; results=[]
   result=0.
-  filecount=0
 
   ##### check for no files
   if (len(filelist) == 0):
@@ -54,8 +51,6 @@ def reatt_length(filelist, yarray, exclude_initial_results):
     nums.sort()
 
   for num in nums:
-    ##### counter for averaging
-    filecount = filecount + 1
     file = "backward_facing_step_3d_"+str(num)+".pvtu"
 
     print file
@@ -98,12 +93,11 @@ def reatt_length(filelist, yarray, exclude_initial_results):
 
     ##### This is the spanwise-averaged reattachment point:
     avpt = sum(points) / len(points)
-    print "spanwise-averaged reattachment point:", avpt, "\n"
-    results.append(avpt)
+    ##### Get time for plot:
+    t = min(datafile.GetScalarField("Time"))
+    results.append([avpt,t])
 
-  ## And this is the time-averaged reattachment point:
-  result = sum(results) / len(results)
-  return result
+  return results
 
 #########################################################################
 
@@ -171,77 +165,94 @@ def meanvelo(filelist,xarray,yarray,zarray):
 
 #########################################################################
 
+def plot_length(reattachment_length):
+  ##### Plot time series of reattachment length using pylab(matplotlib)
+  plot1 = pylab.figure()
+  pylab.title("Time series of reattachment length behind 3D step")
+  pylab.xlabel('Time (s)')
+  pylab.ylabel('Reattachment Length (L/h)')
+  pylab.plot(reattachment_length[:,1], reattachment_length[:,0], marker = 'o', markerfacecolor='white', markersize=6, markeredgecolor='black', linestyle="solid")
+  pylab.savefig("reattachment_length_3D.pdf")
+  return
+
+def plot_meanvelo(profiles):
+  ##### Plot velocity profiles at different points behind step, and at 3 times using pylab(matplotlib)
+  plot1 = pylab.figure(figsize = (16.5, 8.5))
+  pylab.suptitle('Evolution of U-velocity profiles downstream of backward facing step', fontsize=30)
+
+  size = 15
+  ax = pylab.subplot(141)
+  ax.plot(profiles[0,0,:],zarray, color='green', linestyle="dashed")
+  ax.plot(profiles[1,0,:],zarray, color='blue', linestyle="dashed")
+  ax.plot(profiles[2,0,:],zarray, color='red', linestyle="dashed")
+  pylab.legend(("5secs", "10secs", "50secs"), loc="upper left")
+  ax.set_title('(a) x/h = 4', fontsize=16)
+  ax.grid("True")
+  for tick in ax.xaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+  for tick in ax.yaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+
+  bx = pylab.subplot(142, sharex=ax, sharey=ax)
+  bx.plot(profiles[0,1,:],zarray, color='green', linestyle="dashed")
+  bx.plot(profiles[1,1,:],zarray, color='blue', linestyle="dashed")
+  bx.plot(profiles[2,1,:],zarray, color='red', linestyle="dashed")
+  pylab.legend(("5secs", "10secs", "50secs"), loc="upper left")
+  bx.set_title('(b) x/h = 6', fontsize=16)
+  bx.grid("True")
+  for tick in bx.xaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+  pylab.setp(bx.get_yticklabels(), visible=False)
+
+  cx = pylab.subplot(143, sharex=ax, sharey=ax)
+  cx.plot(profiles[0,2,:],zarray, color='green', linestyle="dashed")
+  cx.plot(profiles[1,2,:],zarray, color='blue', linestyle="dashed")
+  cx.plot(profiles[2,2,:],zarray, color='red', linestyle="dashed")
+  pylab.legend(("5secs", "10secs", "50secs"), loc="upper left")
+  cx.set_title('(c) x/h = 10', fontsize=16)
+  cx.grid("True")
+  for tick in cx.xaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+  pylab.setp(cx.get_yticklabels(), visible=False)
+
+  dx = pylab.subplot(144, sharex=ax, sharey=ax)
+  dx.plot(profiles[0,3,:],zarray, color='green', linestyle="dashed")
+  dx.plot(profiles[1,3,:],zarray, color='blue', linestyle="dashed")
+  dx.plot(profiles[2,3,:],zarray, color='red', linestyle="dashed")
+  pylab.legend(("5secs", "10secs", "50secs"), loc="upper left")
+  dx.set_title('(d) x/h = 19', fontsize=16)
+  dx.grid("True")
+  for tick in dx.xaxis.get_major_ticks():
+    tick.label1.set_fontsize(size)
+  pylab.setp(dx.get_yticklabels(), visible=False)
+
+  pylab.axis([-0.25, 1., 0., 2.])
+  bx.set_xlabel('Normalised U-velocity (U/Umax)', fontsize=24)
+  ax.set_ylabel('z/h', fontsize=24)
+
+  pylab.savefig("velo_profiles_3d_parallel.pdf")
+  return
+
+#########################################################################
+
 ##### Points to generate profiles:
 xarray = numpy.array([4.0, 6.0, 10.0, 19.0])
 yarray = numpy.array([0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 3.9])
 zarray = numpy.array([0.01, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0])
 
-##### Call functions defined above
+##### Call reattachment_length function defined above
 filelist = get_filelist()
-reattachment_length = reatt_length(filelist, yarray, exclude_initial_results=30)
-print "\nTime-averaged reattachment length (in step heights): ", reattachment_length
+reattachment_length = numpy.array(reatt_length(filelist, yarray, exclude_initial_results=40))
+av_length = sum(reattachment_length[:,0]) / len(reattachment_length[:,0])
+numpy.save("reattachment_length_3D", reattachment_length)
+print "\nTime-averaged reattachment length (in step heights): ", av_length
+plot_length(reattachment_length)
 
+##### Call meanvelo function defined above
 profiles = meanvelo(filelist, xarray, yarray, zarray)
 numpy.save("velo_profiles_3d_parallel", profiles)
-
-#########################################################################
-
-##### Plot velocity profiles using pylab(matplotlib)
-pylab.figure(num=1, figsize = (16.5, 8.5))
-pylab.suptitle('Evolution of U-velocity profiles downstream of backward facing step', fontsize=30)
-
-size = 15
-
-ax = pylab.subplot(141)
-ax.plot(profiles[0,0,:],zarray, color='green', linestyle="dashed")
-ax.plot(profiles[1,0,:],zarray, color='blue', linestyle="dashed")
-ax.plot(profiles[2,0,:],zarray, color='red', linestyle="dashed")
-pylab.legend(("5secs", "10secs", "50secs"), loc="upper left")
-ax.set_title('(a) x/h = 4', fontsize=16)
-ax.grid("True")
-for tick in ax.xaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-for tick in ax.yaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-
-bx = pylab.subplot(142, sharex=ax, sharey=ax)
-bx.plot(profiles[0,1,:],zarray, color='green', linestyle="dashed")
-bx.plot(profiles[1,1,:],zarray, color='blue', linestyle="dashed")
-bx.plot(profiles[2,1,:],zarray, color='red', linestyle="dashed")
-pylab.legend(("5secs", "10secs", "50secs"), loc="upper left")
-bx.set_title('(b) x/h = 6', fontsize=16)
-bx.grid("True")
-for tick in bx.xaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-pylab.setp(bx.get_yticklabels(), visible=False)
-
-cx = pylab.subplot(143, sharex=ax, sharey=ax)
-cx.plot(profiles[0,2,:],zarray, color='green', linestyle="dashed")
-cx.plot(profiles[1,2,:],zarray, color='blue', linestyle="dashed")
-cx.plot(profiles[2,2,:],zarray, color='red', linestyle="dashed")
-pylab.legend(("5secs", "10secs", "50secs"), loc="upper left")
-cx.set_title('(c) x/h = 10', fontsize=16)
-cx.grid("True")
-for tick in cx.xaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-pylab.setp(cx.get_yticklabels(), visible=False)
-
-dx = pylab.subplot(144, sharex=ax, sharey=ax)
-dx.plot(profiles[0,3,:],zarray, color='green', linestyle="dashed")
-dx.plot(profiles[1,3,:],zarray, color='blue', linestyle="dashed")
-dx.plot(profiles[2,3,:],zarray, color='red', linestyle="dashed")
-pylab.legend(("5secs", "10secs", "50secs"), loc="upper left")
-dx.set_title('(d) x/h = 19', fontsize=16)
-dx.grid("True")
-for tick in dx.xaxis.get_major_ticks():
-  tick.label1.set_fontsize(size)
-pylab.setp(dx.get_yticklabels(), visible=False)
-
-pylab.axis([-0.25, 1., 0., 2.])
-bx.set_xlabel('Normalised U-velocity (U/Umax)', fontsize=24)
-ax.set_ylabel('z/h', fontsize=24)
-
-pylab.savefig("velo_profiles_3d_parallel.pdf")
+print "Showing plot of velocity profiles.\nTo continue script, close plot window."
+plot_meanvelo(profiles)
 pylab.show()
 
 print "\nAll done.\n"
