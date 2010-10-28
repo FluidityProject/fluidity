45bf93e602c079b53a6d0ab6fe0479066e5a8359
jbull <>
2010-10-28 19:18:05 +0000

    Changing the Makefile.in to build various useful parallelised postprocessing tools on cx1. Modifying time_average_parallel to average multiple field lists at the same time.

--- tools/time_average_parallel.F90
+++ tools/time_average_parallel.F90
@@ -22,7 +22,7 @@ program time_average_parallel
 
   character(len=255) :: initial_file
   character(len=255) :: fieldname
-  integer :: i, rank
+  integer :: i, j, rank
   integer :: ierr
 
   type(vector_field), pointer :: positions
@@ -32,7 +32,7 @@ program time_average_parallel
   type(scalar_field) :: savg
   type(vector_field), pointer :: vfield
   type(vector_field) :: vavg
-  integer :: stat
+  integer :: stat, argc, status
 
   call mpi_init(ierr)
   call python_init
@@ -43,51 +43,70 @@ program time_average_parallel
   write(0,*) "process count: ", nprocs
   write(0,*) "rank: ", rank
 
-  call parse_args(initial_file, fieldname)
-  call vtk_read_state(subdomain_name(initial_file, rank), state)
-  positions => extract_vector_field(state, "Coordinate")
-
-  sfield => extract_scalar_field(state, trim(fieldname) // "1", stat=stat)
-  if (stat == 0) then
-    call allocate(savg, sfield%mesh, trim(fieldname) // "Average")
-    call zero(savg)
-
-    i = 1
-    do 
-      sfield => extract_scalar_field(state, trim(fieldname) // int2str(i), stat=stat)
-      if (stat /= 0) then
-        exit
-      end if
-      call addto(savg, sfield)
-      i = i + 1
-    end do
-
-    call scale(savg, 1.0/(i-1))
-    call insert(state, savg, trim(fieldname) // "Average")
-    call deallocate(savg)
-  else
-    vfield => extract_vector_field(state, trim(fieldname) // "1")
-    call allocate(vavg, vfield%dim, vfield%mesh, trim(fieldname) // "Average")
-    call zero(vavg)
-
-    i = 1
-    do 
-      vfield => extract_vector_field(state, trim(fieldname) // int2str(i), stat=stat)
-      if (stat /= 0) then
-        exit
-      end if
-      call addto(vavg, vfield)
-      i = i + 1
-    end do
-
-    call scale(vavg, 1.0/(i-1))
-    call insert(state, vavg, trim(fieldname) // "Average")
-    call deallocate(vavg)
+  argc = command_argument_count()
+  if (argc == 0) then
+    write(0,*) "Usage: time_average_parallel initial_file FieldName"
+    stop
   end if
 
-  call insert(state, positions%mesh, "Mesh")
-  call vtk_write_state("time_average_parallel", model="Mesh", state=(/state/))
-  call deallocate(state)
+  write(0,*) "argument count: ", argc
+  call get_command_argument(1, value=initial_file, status=status)
+  write(0,*) "initial_file: ", initial_file
+
+  call vtk_read_state(subdomain_name(initial_file, rank), state)
+  positions => extract_vector_field(state, "Coordinate")
+  !call parse_args(initial_file, fieldname)
+
+  ! Modification to parse multiple fields
+  do i = 2, argc
+    call get_command_argument(i, value=fieldname, status=status)
+    write(0,*) "fieldname: ", fieldname
+
+    sfield => extract_scalar_field(state, trim(fieldname) // "1", stat=stat)
+    if (stat == 0) then
+      call allocate(savg, sfield%mesh, trim(fieldname) // "Average")
+      call zero(savg)
+      write(0,*) "field: ", trim(fieldname)
+
+      j = 1
+      do 
+        sfield => extract_scalar_field(state, trim(fieldname) // int2str(i), stat=stat)
+        if (stat /= 0) then
+          exit
+        end if
+        call addto(savg, sfield)
+        j = j + 1
+      end do
+
+      call scale(savg, 1.0/(i-1))
+      call insert(state, savg, trim(fieldname) // "Average")
+      call deallocate(savg)
+    else
+      vfield => extract_vector_field(state, trim(fieldname) // "1")
+      call allocate(vavg, vfield%dim, vfield%mesh, trim(fieldname) // "Average")
+      call zero(vavg)
+      write(0,*) "field, dim: ", trim(fieldname), vfield%dim
+
+      j = 1
+      do 
+        vfield => extract_vector_field(state, trim(fieldname) // int2str(i), stat=stat)
+        if (stat /= 0) then
+          exit
+        end if
+        call addto(vavg, vfield)
+        j = j + 1
+      end do
+
+      call scale(vavg, 1.0/(i-1))
+      call insert(state, vavg, trim(fieldname) // "Average")
+      call deallocate(vavg)
+    end if
+
+    call insert(state, positions%mesh, "Mesh")
+    call vtk_write_state("time_average_parallel", model="Mesh", state=(/state/))
+    call deallocate(state)
+
+  end do
 
   call print_references(-1)
   call mpi_finalize(ierr)
@@ -102,6 +121,7 @@ program time_average_parallel
     name = pvtu_name(1:len_trim(pvtu_name) - len(".pvtu")) // "_" // int2str(subdomain) // ".vtu"
   end function subdomain_name
 
+  ! No longer needed
   subroutine parse_args(initial_file, fieldname)
     character(len=255), intent(out) :: initial_file
     character(len=255), intent(out) :: fieldname
