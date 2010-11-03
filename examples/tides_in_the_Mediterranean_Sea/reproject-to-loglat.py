06423a69127721142f067417a15296d85a0fdd6e
ajm02 <>
2010-11-03 13:13:18 +0000

    adding Med tides example

---
+++ examples/tides_in_the_Mediterranean_Sea/reproject-to-loglat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import vtktools
+import math
+from scipy import sqrt
+import vtk
+import sys
+
+ugrid = u=vtktools.vtu("med_10.pvtu")
+# e.g. x="math.sin(x)"
+x="math.atan2(y,x)*57.2957795"
+y="math.asin(z/math.sqrt(x*x+y*y+z*z))*57.2957795"
+z="math.sqrt(x*x+y*y+z*z)-6.37101e+06"
+
+ugrid.ApplyProjection(x, y, z)
+
+ugrid.Write(filename="tidesnorthsea-flat.vtu")
+
+u=vtktools.vtu("tidesnorthsea-flat.vtu")
+
