87028fe9983074ebcdb39c405bb5e0f8cbfa75b9
tmb1 <>
2010-11-23 15:00:10 +0000

    Cloning David's commit of r15013 later in the same file.

--- femtools/Sparse_Tools.F90
+++ femtools/Sparse_Tools.F90
@@ -3350,8 +3350,12 @@ contains
        ewrite(-1,*) 'Warning, not same sparsity'
        !Code for different sparsity, we assume that in_matrix%sparsity
        !is contained in out_matrix%sparsity
-       assert(size(out_matrix,1)==size(in_matrix,1))       
-       assert(size(out_matrix,2)==size(in_matrix,2))
+#ifdef DDEBUG
+       matrix_same_shape=size(out_matrix,1)==size(in_matrix,1)
+       assert(matrix_same_shape)
+       matrix_same_shape=size(out_matrix,2)==size(in_matrix,2)
+       assert(matrix_same_shape)
+#endif
 
        if (associated(out_matrix%ival)) then
           assert(associated(in_matrix%ival))
