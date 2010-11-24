5c407a54c6dc9827c59dd6580ada2e08bdc9dc0f
tmb1 <>
2010-11-24 15:20:08 +0000

    Buildbot infrastructure; should be entirely orthogonal to anything else in the trunk as called by buildbot. Am including this in the trunk as (a) it's handy for us to have it there for buildbot, and (b) it gives people an idea as to how we handle nasty PBS interactions with buildbot builds.

---
+++ scripts/pbswrapper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os, time, sys
+
+def is_job_running(jobid):
+    outp = os.popen("qstat -an | grep %s" % jobid)
+    if outp.read() == '': return False
+    else: return True
+
+pbsfile = sys.argv[1]
+
+#os.chdir(basedir)
+if not os.system("svn update") == 0:
+  sys.exit(1)
+outp = os.popen("qsub "+pbsfile)
+jobid = outp.read().split(".")[0]
+outp.close()
+
+while is_job_running(jobid):
+    time.sleep(15)
+
+file = open("compile.log")
+data = file.read()
+for line in data.split('\n'):
+    print line + '\n',
+
+if "BUILD COMPLETE" in data:
+  sys.exit(0)
+else:
+  sys.exit(1)
