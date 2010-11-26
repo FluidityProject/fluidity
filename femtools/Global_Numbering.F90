2deae4e31cd65c6bf08998fbabb38851a875a051
dham <>
2010-11-26 13:19:00 +0000

    clean up global_numbering removing much ancient rubbish

--- femtools/Global_Numbering.F90
+++ femtools/Global_Numbering.F90
@@ -50,9 +50,8 @@ module global_numbering
 
   private
   
-  public :: make_global_numbering_DG, make_boundary_numbering,&
-       & make_global_numbering, element_halo_communicate_visibility
-
+  public :: make_global_numbering_DG, make_global_numbering
+   
 contains
   
   subroutine make_global_numbering_DG(new_nonods, new_ndglno, Totele,&
@@ -210,9 +209,6 @@ contains
 
     ! List of vertex numbers in an element.
     integer, dimension(:), pointer :: ele1_vertices, ele2_vertices
-
-!DEBUG
-    character(len=10) :: format
 
     ! Ascertain whether we are calculating halos or not.
     if (present(halos)) then
@@ -572,8 +568,6 @@ contains
     call deallocate(NNList)
     call deallocate(EEList)
 
-    write(format,'(a,i0,a)') '(',element%loc,'i4)'
-
     ASSERT(sum(n)==new_nonods)
 
     owned_nodes=n(0)
@@ -595,12 +589,9 @@ contains
        new_node_owner(n(halo)+1:,0)&
             =new_node_owner(:new_nonods-n(halo),halo)
 
-!!$       call remove_spurious_sends(new_send_targets, new_ndglno,&
-!!$            & element_tot_len, element_halo)
-
        call generate_new_halos(new_halos, new_ndglno, new_node_owner(:,0)&
-            &, new_receive_halo_level, totele,&
-            & element%loc, owned_nodes, new_send_targets) 
+            &, new_receive_halo_level, element%loc, owned_nodes,&
+            & new_send_targets) 
 
        do i = 1, size(old_send_targets,1)
           do j = 1, size(old_send_targets,2)
@@ -629,7 +620,7 @@ contains
       integer, intent(out) :: this_receive_halo_level
       type(integer_set), dimension(:), intent(out) :: this_send_targets
 
-      integer :: halo, i
+      integer :: halo
 
       ! Defaults which may be overwritten in the following if block
       this_receive_halo_level=0                
@@ -682,86 +673,9 @@ contains
 
     end subroutine conduct_halo_voting
 
-!!$    subroutine remove_spurious_sends(send_targets, ndglno, nloc, element_halo)
-!!$      !!< Given the node ownership and a complete element halo, remove any
-!!$      !!< sends which apply only to elements about which the receiving
-!!$      !!< processor is unaware.
-!!$
-!!$      !! Send_targets provides the list of processors to which each
-!!$      !! node is broadcast at each halo level. It is nonods x halos
-!!$      type(ilist), dimension(:,:), intent(inout) :: send_targets
-!!$      !! The element node list and number of nodes per element for the NEW
-!!$      !! mesh.
-!!$      integer, dimension(:), intent(in), target :: ndglno
-!!$      integer, intent(in) :: nloc
-!!$      type(halo_type), intent(in) :: element_halo
-!!$      
-!!$      !! For each node, a list of processors which can see that node.
-!!$      type(ilist), dimension(:), allocatable :: visible_to
-!!$      !! For each processor, a list of halo elements that processor can see.
-!!$      type(integer_vector), dimension(:), allocatable :: known_element_lists
-!!$
-!!$      integer :: e, ele, element_count
-!!$      integer :: n, node_count, proc, h, halo_count
-!!$      integer, dimension(:), pointer :: this_ele
-!!$      type(ilist) :: tmplist
-!!$
-!!$      element_count=size(ndglno)/nloc
-!!$      node_count=size(send_targets,1)
-!!$      halo_count=size(send_targets,2)
-!!$
-!!$      allocate(visible_to(node_count))
-!!$      allocate(known_element_lists(halo_proc_count(element_halo)))
-!!$
-!!$      ! Check that nloc does actually divide size(ndglno)
-!!$      assert(size(ndglno)==element_count*nloc)
-!!$
-!!$      ! Retrieve the list of elements which each processor can see.
-!!$      call element_halo_communicate_visibility(element_halo,&
-!!$           & known_element_lists)
-!!$
-!!$      ! Mark all nodes in elements visible to a processor as themselves
-!!$      ! visible to that processor
-!!$      do proc=1,halo_proc_count(element_halo)
-!!$         do e = 1, size(known_element_lists(proc)%ptr)
-!!$            ele = known_element_lists(proc)%ptr(e)
-!!$            this_ele=>ndglno((ele-1)*nloc+1:ele*nloc)
-!!$
-!!$            do n=1,size(this_ele)
-!!$               
-!!$               call insert_ascending(visible_to(this_ele(n)), proc)
-!!$            
-!!$            end do
-!!$         end do
-!!$      end do
-!!$
-!!$      do proc=1,halo_proc_count(element_halo)
-!!$         deallocate(known_element_lists(proc)%ptr)
-!!$      end do
-!!$
-!!$      do n=1,node_count
-!!$         
-!!$         do h=1,halo_count
-!!$            ! Do nothing for non-send nodes.
-!!$            if (send_targets(n,h)%length==0) cycle
-!!$         
-!!$            
-!!$            tmplist=intersect_ascending(send_targets(n,h),visible_to(n))
-!!$         
-!!$            call flush_list(send_targets(n,h))
-!!$            send_targets(n,h)=tmplist
-!!$         end do
-!!$
-!!$         call flush_list(visible_to(n))
-!!$      end do
-!!$      deallocate(visible_to)
-!!$
-!!$
-!!$    end subroutine remove_spurious_sends
     
     subroutine generate_new_halos(new_halos, new_ndglno, new_node_owner&
-            &, new_receive_halo_level, elements,&
-            & nloc, owned_nodes, send_targets)
+            &, new_receive_halo_level, nloc, owned_nodes, send_targets)
       !!< Given the node ownership and halo level information on the new
       !!< mesh, construct halos for the new mesh.
       !!< Note that these halos are unsorted as sorting the halos requires
@@ -770,7 +684,7 @@ contains
       integer, dimension(:), intent(in), target :: new_ndglno
       integer, dimension(:), intent(in), target :: new_node_owner
       integer, dimension(:), intent(in), target :: new_receive_halo_level
-      integer, intent(in) :: elements, nloc, owned_nodes
+      integer, intent(in) :: nloc, owned_nodes
       ! Send_targets provides the list of processors to which each
       ! node is broadcast at each halo level. It is nonods x halos
       type(integer_set), dimension(:,:), intent(in) :: send_targets
@@ -926,306 +840,5 @@ contains
     end subroutine halo_lists
 
   end subroutine make_global_numbering
-
-  subroutine make_boundary_numbering(boundary_list, boundary_n_lno, &
-       boundary_m_lno, EEList, xnonod&
-       &, xndglno, ele_n, boundary_n, ele_m, boundary_m)
-    ! Generate boundary numberings to facilitate the evaluation of boundary
-    ! integrals. As is usual in fluidity, the suffix n refers to velocity
-    ! elements while the suffix m refers to pressure elements.
-    !
-    ! If f=boundary_list(i,j) then boundary_n_lno((f-1)*boundary%loc+1:f*boundary%loc) is the
-    ! vector of local indices of the nodes on the boundary between i and j. 
-    ! 
-    ! Let f2=boundary_list(j,i). Then:
-    ! xndglno(boundary_list((f-1)*boundary%loc+1:f*boundary%loc)) = 
-    !          xndglno(boundary_list((f2-1)*boundary%loc+1:f2*boundary%loc))
-    type(csr_sparsity), intent(in) :: EEList
-    type(element_type), intent(in) :: ele_n, boundary_n
-    type(element_type), optional, intent(in) :: ele_m, boundary_m
-    integer, intent(in) :: xnonod
-    integer, dimension(size(EEList,1)*ele_n%loc), intent(in), target ::&
-         & xndglno 
-
-    type(csr_matrix), intent(out) :: boundary_list
-    integer, dimension(boundary_n%loc*entries(EEList)), intent(out), optional :: & 
-         boundary_n_lno
-    integer, dimension(:), optional, intent(out) :: & 
-         boundary_m_lno
-
-    integer, dimension(ele_n%numbering%boundaries) :: neigh
-    integer, dimension(ele_n%numbering%vertices) :: vertices
-    integer, dimension(:), pointer :: ele_i, ele_j
-    
-    integer :: boundary_cnt, i, j, m, n, p, rlen
-    logical :: logtest
-    integer, dimension(boundary_n%numbering%vertices) :: boundary_i, boundary_j
-
-    ewrite(2,*) "subroutine make_boundary_numbering"
-
-    !CHECK(size(neigh))
-
-    call allocate(boundary_list, EEList, type=CSR_INTEGER)
-    if (present(boundary_m_lno)) boundary_m_lno=0
-
-    logtest = associated(boundary_list%val)
-    !CHECK(logtest)
-    logtest = associated(boundary_list%ival)
-    !CHECK(logtest)
-
-    if(present(boundary_m_lno)) then
-!       MSG("Checking size of boundary_m_lno")
-!       ASSERT(size(boundary_m_lno)==(boundary_m%loc*entries(EEList)))
-    end if
-
-    ewrite(2,*) "zeroing boundary_list"
-    call zero(boundary_list)
-
-    if (present(boundary_n_lno)) boundary_n_lno=0
-    if (present(boundary_m_lno)) boundary_m_lno=0
-
-    boundary_cnt=0
-
-    vertices=local_vertices(ele_n%numbering)
-       
-    do i=1,size(boundary_list,1)
-       ele_i=>xndglno((i-1)*ele_n%loc+1:i*ele_n%loc)
-       
-       ! Sanity check EEList form.
-       rlen = row_length(boundary_List,i)
-       ASSERT(rlen==size(neigh))
-
-       neigh=row_m(EEList,i)
-
-       neighbourloop: do j=1, size(neigh)
-          ! Exclude boundary boundaries.
-          if (neigh(j)==0) then
-             cycle neighbourloop 
-          end if
-          
-          ! EEList includes the current ele_n, which is obviously wrong.
-          if (neigh(j)==i) cycle neighbourloop
-
-          ! Check to see if the boundary has been done.
-          if (val(boundary_list,i,neigh(j))/=0) cycle neighbourloop 
-
-          ele_j=>xndglno((neigh(j)-1)*ele_n%loc+1:neigh(j)*ele_n%loc)
-
-          p=0
-
-          ! Look for common boundaries.
-          do m=1,size(vertices)
-             do n=1,size(vertices)
-                if (ele_i(vertices(m))==ele_j(vertices(n))) then
-                   p=p+1
-                   boundary_i(p)=m
-                   boundary_j(p)=n
-                end if
-             end do
-          end do
-
-          ! Check that we really have found two boundaries.
-          !ASSERT(p==boundary_n%numbering%vertices)
-
-          ! Put the boundaries we have found in the next two spots.
-          boundary_cnt=boundary_cnt+1
-
-          ! Velocity element boundaries.
-          if(present(boundary_n_lno)) then
-             boundary_n_lno((boundary_cnt-1)*boundary_n%loc+1: &
-                  boundary_cnt*boundary_n%loc)= &
-                  & boundary_local_num(boundary_i, ele_n%numbering)
-          end if
-
-          ! Pressure element boundaries.
-          if(present(boundary_m_lno)) then
-             ASSERT(present(boundary_m))
-             ASSERT(present(boundary_m))
-             ASSERT(present(ele_m))
-             boundary_m_lno((boundary_cnt-1)*boundary_m%loc+1: &
-                  boundary_cnt*boundary_m%loc)= &
-                  boundary_local_num(boundary_i, ele_m%numbering)
-          end if
-
-          call set(boundary_list,i,neigh(j),boundary_cnt)
-
-          boundary_cnt=boundary_cnt+1
-          
-          ! Velocity element boundaries.
-          if(present(boundary_n_lno)) then
-             boundary_n_lno((boundary_cnt-1)*boundary_n%loc+1: &
-                  boundary_cnt*boundary_n%loc)= &
-                  boundary_local_num(boundary_j, ele_n%numbering)
-          end if
-
-          ! Pressure element boundaries.
-          if(present(boundary_m_lno)) then
-             boundary_m_lno((boundary_cnt-1)*boundary_m%loc+1: &
-                  boundary_cnt*boundary_m%loc)= &
-                  boundary_local_num(boundary_j, ele_m%numbering)
-          end if
-
-          call set(boundary_list,neigh(j),i,boundary_cnt)
-       end do neighbourloop
-
-    end do
-
-    logtest = (boundary_cnt==entries(EEList))
-    ASSERT(logtest)
-
-    ewrite(2,*) "END subroutine make_boundary_numbering"
-
-  end subroutine make_boundary_numbering
-
-
-  subroutine element_halo_communicate_visibility(element_halo,&
-       & known_element_lists)
-    !!< Given the element halo, for each processor we know about, send
-    !!< the list of halo elements we know about.
-    type(halo_type), intent(in) :: element_halo
-    type(integer_vector), dimension(halo_proc_count(element_halo)),&
-         & intent(out) :: known_element_lists
-
-#ifdef HAVE_MPI
-    ! For each element, the list of other processors it is sent to.
-    type(ilist), dimension(:), allocatable :: visible_list
-    ! For each processor, the list of extra elements it can see.
-    type(ilist), dimension(:), allocatable :: visible_elements
-    type(inode), pointer :: this_item
-    
-    ! For each processor, the visible_list which is to be sent to it.
-    type(integer_vector), dimension(halo_proc_count(element_halo)) ::&
-         & send_lists
-    integer, dimension(:), allocatable :: requests, receive_list
-    integer, dimension(MPI_STATUS_SIZE) :: status
-    integer :: proc, e, ierr, rank, communicator, count, nprocs, sends,&
-         & receives, pos, p, sendproc, tag
-    
-
-    nprocs=halo_proc_count(element_halo)
-
-    allocate(visible_list(node_count(element_halo)))
-
-    do proc=1, nprocs
-       do e=1, halo_send_count(element_halo, proc)
-
-          call insert_ascending(&
-               visible_list(halo_send(element_halo, proc, e)), &
-               proc)
-       end do
-    end do
-
-    do proc=1, nprocs
-       allocate(send_lists(proc)%ptr(&
-            sum(visible_list(halo_sends(element_halo,proc))%length)))
-    
-       ! Use the first halo_send_count places to indicate how many
-       ! processors (other than the receiver) know about each element.
-       send_lists(proc)%ptr(:halo_send_count(element_halo, proc)) &
-            = visible_list(halo_sends(element_halo,proc))%length - 1 
-       
-       sends = halo_send_count(element_halo, proc)       
-       pos=sends
-       do e=1, sends
-          this_item=>visible_list(halo_send(element_halo, proc, e))&
-               &%firstnode 
-          do while(associated(this_item))
-             ! Eliminate self-cites.
-             if (this_item%value/=proc) then
-                pos=pos+1
-                send_lists(proc)%ptr(pos)=this_item%value
-             end if
-             
-             this_item=>this_item%next
-          end do
-          
-       end do
-       assert(pos==size(send_lists(proc)%ptr))
-    end do
-    
-    call flush_lists(visible_list)
-    deallocate(visible_list)
-
-    ! Set up non-blocking communications
-    communicator = halo_communicator(element_halo)
-    allocate(requests(nprocs))
-    rank = getrank(communicator)  
-    tag = next_mpi_tag()
-
-    do proc=1, nprocs
-
-       call mpi_isend(send_lists(proc)%ptr, &
-            size(send_lists(proc)%ptr), MPI_INTEGER,&
-            proc-1, tag, communicator, requests(proc), ierr)
-       assert(ierr == MPI_SUCCESS)
-
-    end do
-
-    allocate(visible_elements(nprocs))
-
-    ! Wait for incoming data.
-    do proc=1, nprocs
-
-       call mpi_probe(MPI_ANY_SOURCE, tag, communicator, status,&
-            & ierr)
-       assert(ierr == MPI_SUCCESS)
-
-       call mpi_get_count(status, MPI_INTEGER, count, ierr)
-       assert(ierr == MPI_SUCCESS)
-
-       allocate(receive_list(count))
-
-       call mpi_recv(receive_list, count, MPI_INTEGER, status(MPI_SOURCE),&
-            tag, communicator, MPI_STATUS_IGNORE, ierr)
-       assert(ierr == MPI_SUCCESS)
-
-       sendproc=status(MPI_SOURCE)+1
-       ! For each element on the recieves list for this processor, record
-       ! which other elements know about it.
-       receives = halo_receive_count(element_halo, sendproc)
-       pos = receives
-       do e=1, receives
-          ! Recall that the first halo_receive_count entries in the list
-          ! simply say how many extra processors know about this element.
-          do p=1,receive_list(e)
-             pos=pos+1
-             call insert_ascending(visible_elements(receive_list(pos)), &
-                  halo_receive(element_halo, sendproc, e))
-          end do
-       end do
-       
-       assert(pos==size(receive_list))
-       deallocate(receive_list)
-    end do
-    
-    ! Wait for sends to complete
-    call mpi_waitall(size(requests), requests, MPI_STATUSES_IGNORE, ierr)
-
-    ! Now actually sort out the lists. The elements which proc and this
-    ! processor can both see comprise the send and receive elements between
-    ! these two processors, plus the elements which other processors send
-    ! to proc and which we were told about above.
-    do proc=1, nprocs
-       sends=halo_send_count(element_halo,proc)
-       receives=halo_receive_count(element_halo,proc)
-       allocate(known_element_lists(proc)%ptr(&
-            sends+receives+visible_elements(proc)%length))
-
-       known_element_lists(proc)%ptr(:sends) &
-            = halo_sends(element_halo, proc)
-
-       known_element_lists(proc)%ptr(sends+1:sends+receives) &
-            = halo_receives(element_halo, proc)
-       
-       known_element_lists(proc)%ptr(sends+receives+1:) &
-            = list2vector(visible_elements(proc))
-    end do
-
-    call flush_lists(visible_elements)
-#else
-    FLAbort("Communicating halo visibility makes no sense without MPI.")
-#endif
-
-  end subroutine element_halo_communicate_visibility
 
 end module global_numbering
