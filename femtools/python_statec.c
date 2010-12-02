e43bb3a6c017880af568c5fb35854f13f62f91e9
cwilson <>
2010-12-02 18:10:24 +0000

    Ugly temporary commit to try to get to the bottom of the possible PYTHONPATH issues on the long tests.  Will revert almost immediately.

--- femtools/python_statec.c
+++ femtools/python_statec.c
@@ -38,10 +38,8 @@ void init_vars(void){
     PyErr_Clear();
   }
   else{
-    if (get_global_debug_level_() > 1) {
-      printf("fluidity.state_types imported successfully; location: \n");
-      PyRun_SimpleString("import fluidity.state_types; print fluidity.state_types.__file__");
-    }
+    printf("fluidity.state_types imported successfully; location: \n");
+    PyRun_SimpleString("import fluidity.state_types; print fluidity.state_types.__file__");
   }
   PyRun_SimpleString("states = dict()");
 #endif
