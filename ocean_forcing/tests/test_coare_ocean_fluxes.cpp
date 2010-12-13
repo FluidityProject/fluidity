bcce8300934a3a82e5e87471d0b0129c8068cac9
jhill1 <>
2010-12-13 17:31:55 +0000

    Fixing the crappy programming that resulted in a lot of memory being used by these unit tests. Also improving tests to use RMS error and correlation, rather than bias and correlation

--- ocean_forcing/tests/test_coare_ocean_fluxes.cpp
+++ ocean_forcing/tests/test_coare_ocean_fluxes.cpp
@@ -66,7 +66,7 @@ void test_coare_ocean_fluxes_fc() {
     double accumulated_correction = 6.0*60.0*60.0;
 
     int n = 1;
-    // Ocean Station Papa (50, -145)
+    // Ocean Station Papa (50, -145) ==
     // -3.35835e+06,-2.35154e+06,4.88594e+06
     X[0] = -3.35835e+06;
     Y[0] = -2.35154e+06;
@@ -91,26 +91,29 @@ void test_coare_ocean_fluxes_fc() {
     double sumQ_era40 = 0, sumF_era40 = 0, sumTauX_era40 = 0, sumTauY_era40 = 0;
     double sumQ2 = 0, sumF2 = 0, sumTauX2 = 0, sumTauY2 = 0;
     double sumQ_era402 = 0, sumF_era402 = 0, sumTauX_era402 = 0, sumTauY_era402 = 0;
-    int nPoints =  int(25920000-86400/21600);
-    vector<double> Q_flux(nPoints,0), F_flux(nPoints,0), TauX_flux(nPoints,0), TauY_flux(nPoints,0);
-    vector<double> Q_era40(nPoints,0), F_era40(nPoints,0), TauX_era40(nPoints,0), TauY_era40(nPoints,0);
-    nPoints = 0;
+    double xy_Q = 0, xy_F = 0, xy_tauU = 0, xy_tauV = 0;
+    double x_y_2_Q = 0, x_y_2_F = 0, x_y_2_tauU = 0, x_y_2_tauV = 0;
+    double nPoints = 0;
     int formula;
 
+// Want to print graphics? Then compile with -DGRAPH
 #ifdef GRAPHIC
     ofstream test;
     test.open("test_data_coare.csv");
     test.clear();
     test <<"Time,ERA40_Q,Flux_Q,ERA40_Tau_X,Flux_Tau_X,ERA40_Tau_Y,Flux_Tau_Y,ERA40_F,Flux_F" << endl; 
 #endif
-    
-    // Do the fluxes for several days then compare
-    for (int t = 86400 ; t < 25920000; t += 21600) {
+   
+    formula = 1;
+
+    // Do the fluxes for a month, then compare
+    for (int t = 86400 ; t < 2592000; t += 21600) {
     
         FluxesReader_ERAdata.SetTimeSeconds(t);
         FluxesReader_ERAdata.GetScalars(x[0], y[0], values_out);
         FluxesReader_global.SetTimeSeconds(t);
         FluxesReader_global.GetScalars(x[0], y[0], values_in);
+
         // now let's work out the fluxes for our test location
         double Tau_u_out = values_out[4]/(accumulated_correction*ocean_density);
         double Tau_v_out = values_out[5]/(accumulated_correction*ocean_density);
@@ -124,11 +127,18 @@ void test_coare_ocean_fluxes_fc() {
         time = t;
 
         // get fluxes for test location
-        formula = 1;
         get_era40_fluxes_fc(&time, &X[0], &Y[0], &Z[0], &T[0], &Vx[0], &Vy[0], &Vz[0], &S[0], &F_as[0],
                             &Q_as[0], &tau_u[0], &tau_v[0], &Q_solar[0], &n, false, &formula);
 
-        
+        // store some data for the correlations later
+        xy_Q += Q_as[0] * Q_out;
+        xy_F += F_as[0] * F_out;
+        xy_tauU += tau_u[0] * Tau_u_out;
+        xy_tauV += tau_v[0] * Tau_v_out;
+        x_y_2_Q += (Q_as[0] - Q_out) * (Q_as[0] - Q_out);
+        x_y_2_F += (F_as[0] - F_out) * (F_as[0] - F_out);
+        x_y_2_tauU += (tau_u[0] - Tau_u_out) * (tau_u[0] - Tau_u_out);
+        x_y_2_tauV += (tau_v[0] - Tau_v_out) * (tau_v[0] - Tau_v_out);
         sumQ2 += Q_as[0]*Q_as[0];
         sumF2 += F_as[0]*F_as[0];
         sumTauX2 += tau_u[0]*tau_u[0];
@@ -145,16 +155,11 @@ void test_coare_ocean_fluxes_fc() {
         sumF_era40 += F_out;
         sumTauX_era40 += Tau_u_out;
         sumTauY_era40 += Tau_v_out;
-        Q_flux[nPoints] = Q_as[0];
-        F_flux[nPoints] = F_as[0];
-        TauX_flux[nPoints] = tau_u[0];
-        TauY_flux[nPoints] = tau_v[0];
-        Q_era40[nPoints] = Q_out;
-        F_era40[nPoints] = F_out;
-        TauX_era40[nPoints] = Tau_u_out;
-        TauY_era40[nPoints] = Tau_v_out;
 
-/**        cout<<values_out[0]/accumulated_correction<<","<<q_h[0]<<endl;
+/*********************************************        
+        // This table is useful when debugging
+  
+        cout<<values_out[0]/accumulated_correction<<","<<q_h[0]<<endl;
         cout <<"Flux                 |    ERA40 value     |  Our value   "<<endl;
         cout <<"---------------------+--------------------+---------------"<<endl;
         printf("U stress (m2/s2)     | %-18g | %-18g\n",Tau_u_out,tau_u[0]);
@@ -163,17 +168,16 @@ void test_coare_ocean_fluxes_fc() {
         printf("Salinity flux        | %-18g | %-18g\n",F_out,F_as[0]);
         printf("Solar rad. (W/m2)    | %-18g | %-18g\n",values_out[2]/accumulated_correction,values_in[2]/accumulated_correction*(1-0.066));
         cout<<endl<<endl;
-**/
+**********************************************/
+
         nPoints++;
+
+// Want to print graphics? Then compile with -DGRAPHIC
 #ifdef GRAPHIC
-    test <<nPoints<< "," << Q_out << "," << Q_as[0]<< "," << Tau_u_out << "," <<tau_u[0]<<","<<Tau_v_out<<","<<tau_v[0]<<","<<F_out<<","<<F_as[0] << endl; 
+    test <<double(t/(24.*60.*60.))<< "," << Q_out << "," << Q_as[0]<< "," << Tau_u_out << "," <<tau_u[0]<<","<<Tau_v_out<<","<<tau_v[0]<<","<<F_out<<","<<F_as[0] << endl; 
 #endif
-    /*printf("%i,%-5g,%-5g,%-5g,%-5g,%-5g,%-5g,%-5g,%-5g\n", nPoints,
-                                                 Q_out,Q_as[0],
-                                                 Tau_u_out,tau_u[0],
-                                                 Tau_v_out,tau_v[0],
-                                                 F_out,F_as[0]);
-*/
+
+
     }
 
 #ifdef GRAPHIC
@@ -190,71 +194,50 @@ void test_coare_ocean_fluxes_fc() {
     sumTauY_era40 /= nPoints;
 
     // work out some correlation coefficient
-    double xy = 0;
-    for (int i=0; i<nPoints; i++) {
-        xy += Q_flux[i] * Q_era40[i];
-    }
-    double r_q = (nPoints * xy - sumQ*sumQ_era40) /
+    double r_q = (nPoints * xy_Q - sumQ*sumQ_era40) /
                  (sqrt(nPoints*sumQ2-(sumQ*sumQ))*sqrt(nPoints*sumQ_era402-(sumQ_era40*sumQ_era40)));
-
-
-    xy = 0;
-    for (int i=0; i<nPoints; i++) {
-        xy += F_flux[i] * F_era40[i];
-    }
-    double r_f = (nPoints * xy - sumF*sumF_era40) /
+    double r_f = (nPoints * xy_F - sumF*sumF_era40) /
                  (sqrt(nPoints*sumF2-(sumF*sumF))*sqrt(nPoints*sumF_era402-(sumF_era40*sumF_era40)));
-
-    xy = 0;
-    for (int i=0; i<nPoints; i++) {
-        xy += TauX_flux[i] * TauX_era40[i];
-    }
-    double r_tauX = (nPoints * xy - sumTauX*sumTauX_era40) /
+    double r_tauX = (nPoints * xy_tauU - sumTauX*sumTauX_era40) /
                  (sqrt(nPoints*sumTauX2-(sumTauX*sumTauX))*sqrt(nPoints*sumTauX_era402-(sumTauX_era40*sumTauX_era40)));
-
-    xy = 0;
-    for (int i=0; i<nPoints; i++) {
-        xy += TauY_flux[i] * TauY_era40[i];
-    }
-    double r_tauY = (nPoints * xy - sumTauY*sumTauY_era40) /
+    double r_tauY = (nPoints * xy_tauV - sumTauY*sumTauY_era40) /
                  (sqrt(nPoints*sumTauY2-(sumTauY*sumTauY))*sqrt(nPoints*sumTauY_era402-(sumTauY_era40*sumTauY_era40)));
 
+    double rms_q = sqrt(x_y_2_Q / nPoints);
+    double rms_f = sqrt(x_y_2_F / nPoints);
+    double rms_tauU = sqrt(x_y_2_tauU / nPoints);
+    double rms_tauV = sqrt(x_y_2_tauV / nPoints);
 
     // Check that we have roughly the right amount of forcing in total
     fail = true;
     warn = false;
-    error = (sumQ_era40-sumQ)/sumQ;
-    if (abs(error) < .75) fail = false;
-    sprintf(errorMessage, "Error > .75 Is %g",error); 
+    if (rms_q < 1e-4) fail = false;
+    sprintf(errorMessage, "RMS error > 1e-4 and is %g",rms_q); 
     report_test("[test_coare_ocean_forcing: heat flux error to ERA40]",fail,warn,errorMessage);
 
     fail = true;
-    error = (sumF-sumF_era40)/sumF;
-    if (abs(error) < .75) fail = false;
-    error = (sumF-sumF_era40)/sumF;
-    sprintf(errorMessage, "Error > .75 Is %g",error);
+    if (rms_f < 1e-6) fail = false;
+    sprintf(errorMessage, "Error > 1e-6 and is %g",rms_f);
     report_test("[test_coare_ocean_forcing: salinity flux error to ERA40]",fail,warn,errorMessage);
 
     fail = true;
-    error = (sumTauX-sumTauX_era40)/sumTauX;
-    if (abs(error) < .75) fail = false;
-    sprintf(errorMessage, "Error > .75 Is %g",error);
+    if (rms_tauU < 1e-4) fail = false;
+    sprintf(errorMessage, "Error > 1e-4 and is %g",rms_tauU);
     report_test("[test_coare_ocean_forcing: tau x momentum error to ERA40]",fail,warn,errorMessage);
 
     fail = true;
-    error = (sumTauY-sumTauY_era40)/sumTauY;
-    if (abs(error) < .75) fail = false;
-    sprintf(errorMessage, "Error > .75 Is %g",error);
+    if (rms_tauV < 1e-4) fail = false;
+    sprintf(errorMessage, "Error > 1e-4 and is %g",rms_tauV);
     report_test("[test_coare_ocean_forcing: tau y momentum error to ERA40]",fail,warn,errorMessage);
-
-    // check we have the right direciton of forcing
-    if (r_q > .88) fail = false;
-    sprintf(errorMessage, "Correlation coefficient < .88: Is %g",r_q); 
+    
+    // check we have the right direction of forcing
+    if (r_q > .9) fail = false;
+    sprintf(errorMessage, "Correlation coefficient < .9: Is %g",r_q); 
     report_test("[test_coare_ocean_forcing: heat flux correlation]",fail,warn,errorMessage);
 
     fail = true;
-    if (r_f > .88) fail = false;
-    sprintf(errorMessage, "Correlation coefficient < .88: Is %g",r_f); 
+    if (r_f > .9) fail = false;
+    sprintf(errorMessage, "Correlation coefficient < .9: Is %g",r_f); 
     report_test("[test_coare_ocean_forcing: freshwater flux correlation]",fail,warn,errorMessage);
 
     fail = true;
