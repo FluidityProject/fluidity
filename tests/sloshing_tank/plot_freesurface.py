0c6905447d198384d2f3f13cbe816dde97ccf25d
sf1409 <>
2010-12-20 17:31:17 +0000

    A medium free surface test with analytical solution based on:
    A. Prosperetti, Motion of two superimposed viscous fluids
    
    Unfortunately there is a phase shift of the numerical solution that is why the tolerance can not be very high at the moment. The reason for this phase shift might be a missing viscous stress on the free surface
    as described section 4.1 of:
    
    Computation of free surface flows with a projection FEM in a moving mesh framework, Sylvain Rabier, Marc Medale
    
    Ideas how for other fixes are very welcome.
    
    Also this test checks the mass conservation of the free surface algorithm for both p1p1 and p1dgp2. i think i proofed mass conservative for our free surface method, therefore i expected a conservation 
    up to machine precision but i need to stick with 1e-11 for now.

---
+++ tests/sloshing_tank/plot_freesurface.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+import settings
+import ana_sol
+
+import sys
+import math
+import commands
+import matplotlib.pyplot as plt
+import getopt
+
+
+from scipy.special import erf
+from numpy import poly1d
+from matplotlib.pyplot import figure, show
+from numpy import pi, sin, linspace
+from matplotlib.mlab import stineman_interp
+from numpy import exp, cos
+
+from fluidity_tools import stat_parser as stat
+
+# Usage
+def usage():
+        print "plt_freesurface.py --file=detectorfile"
+        print "All the other options are read from settings.py"
+
+
+
+################# Main ###########################
+def main(argv=None):
+
+        a_0 = settings.a0 # initial maximum perturbation
+        g = settings.g # gravity
+        eta= settings.eta # viscosity
+        L= settings.L # wavelength
+        timestep= settings.timestep # timestep 
+        filename=''
+
+        global debug
+        debug=False
+        #debug=True      
+
+        try:                                
+                opts, args = getopt.getopt(sys.argv[1:], "h:", ['file='])
+        except getopt.GetoptError:  
+                usage()                     
+                sys.exit(2)                     
+        for opt, arg in opts:                
+                if opt == '--file':      
+                    filename=arg
+                elif opt == '-h' or opt == '--help':
+                    usage()                     
+                    sys.exit(2) 
+        if filename=='':
+                usage()                     
+                sys.exit(2) 
+
+        print 'Using:\n\ta_0 =', a_0 # initial maximum perturbation
+        print '\tg =', g # gravity
+        print '\teta=', eta # viscosity
+        print '\tL=', L # wavelength
+        print '\ttimestep=', timestep # timestep 
+
+        
+        ####################### Print time plot  ###########################
+        print 'Generating time plot'
+
+        x_time= stat(filename)["ElapsedTime"]["value"]
+        fs_simu= stat(filename)["water"]["FreeSurface"]["left"]
+#        fs_simu= stat(filename)["water"]["FreeSurface"]["middle"]
+        fs_ana = stat(filename)["water"]["FreeSurface_Analytical"]["left"]
+#        fs_ana = stat(filename)["water"]["FreeSurface_Analytical"]["middle"]
+
+        plt.ion() # swith on interactive mode
+        fig = figure()
+        ax = fig.add_subplot(111)
+
+        ax.plot(x_time,fs_simu,'ro')
+        ax.plot(x_time,fs_ana,'-')
+        plt.title('Free Surface timeplot at x=0')
+        plt.xlabel('Time [s]')
+        plt.ylabel('Free surface [m]')
+
+        plt.draw()
+        raw_input("Please press Enter")
+        #plt.cla()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
