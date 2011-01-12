307e0ed9d80000a77f99b05c7d61b3de084cfd54
cjc1 <>
2011-01-12 14:00:52 +0000

    svn confusion

--- tests/inertia_gravity_shallow_water/inertia_gravity.py
+++ tests/inertia_gravity_shallow_water/inertia_gravity.py
@@ -68,7 +68,7 @@ def convergence(dx):
     error = numpy.zeros(dx.size)
 
     binary="../../bin/shallow_water"
-    project = "inertia_gravity_cjc"
+    project = "inertia_gravity"
 
     dt=0.0125
     for x in enumerate(dx):
