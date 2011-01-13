bfb4c609fe218e09eb8544f5f061f8e9db72387e
sf1409 <>
2011-01-13 11:21:07 +0000

    Print the domain volume in each timestep if wetting and drying is used.
    This should really go into the stat file but currently stat file can not save user defined values (besides the standard one). This will hopefully change in the near future.

--- main/Fluids.F90
+++ main/Fluids.F90
@@ -806,6 +806,11 @@ contains
 
        ! Call the modern and significantly less satanic version of study
        call write_diagnostics(state, current_time, dt, timestep)
+       ! Work out the domain volume by integrating the water depth function over the surface if using wetting and drying
+       if (have_option("/mesh_adaptivity/mesh_movement/free_surface/wetting_and_drying")) then
+          ewrite(1, *) "Domain volume (\int_{fs} (\eta.-b)n.n_z)): ", calculate_volume_by_surface_integral(state(1))
+       end if 
+
 
        if(have_option("/timestepping/adaptive_timestep")) call calc_cflnumber_field_based_dt(state, dt)
 
