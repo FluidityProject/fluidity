bbdae8ea07ddfce62845e32999a7f8795de27876
sf1409 <>
2011-01-19 18:49:27 +0000

    use a relative path for fldecomp for user who do not have set up the PATH correctely

--- examples/monai_valley/mesh/makemesh.sh
+++ examples/monai_valley/mesh/makemesh.sh
@@ -2,7 +2,7 @@
 #fldecomp -n 4 -m gmsh MonaiValley_B
 
 gmsh -bin MonaiValley_C.geo -2
-fldecomp -n 4 -m gmsh MonaiValley_C
+../../../bin/fldecomp -n 4 -m gmsh MonaiValley_C
 
 #gmsh -bin MonaiValley_D.geo -2
 #fldecomp -n 4 -m gmsh MonaiValley_D
