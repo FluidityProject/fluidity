a1f58d5deea89412aa7570e75e74a3fa28272f16
jbull <>
2011-01-24 11:06:44 +0000

    Reverting 15327 as it broke stuff. Not sure what or why.

--- diagnostics/Field_Copies.F90
+++ diagnostics/Field_Copies.F90
@@ -53,8 +53,7 @@ module field_copies_diagnostics
   public :: calculate_scalar_galerkin_projection, calculate_vector_galerkin_projection
   public :: calculate_helmholtz_smoothed_scalar, calculate_helmholtz_smoothed_vector
   public :: calculate_lumped_mass_smoothed_scalar, calculate_lumped_mass_smoothed_vector
-  public :: calculate_helmholtz_anisotropic_smoothed_scalar, calculate_helmholtz_anisotropic_smoothed_vector
-
+  
 contains
 
   subroutine calculate_scalar_copy(state, s_field)
@@ -415,7 +414,8 @@ contains
     character(len = OPTION_PATH_LEN) :: path
     integer, dimension(2) :: alpha_shape
     real, dimension(:, :), allocatable :: alpha
-    type(vector_field), pointer :: source_field, positions
+    type(vector_field), pointer :: source_field
+    type(vector_field), pointer :: positions
     
     ewrite(1, *) "In calculate_helmholtz_smoothed_vector"
     
@@ -428,70 +428,16 @@ contains
     call get_option(trim(path) // "/smoothing_length_scale", alpha)
     ewrite(2, *) "alpha = ", alpha
     
+    !ewrite_minmax(source_field%val)
     call smooth_vector(source_field, positions, v_field, alpha, path)
     ewrite_minmax(v_field%val)
     
     deallocate(alpha)
-   
+    !deallocate(source_field)
+    
     ewrite(1, *) "Exiting calculate_helmholtz_smoothed_vector"
     
   end subroutine calculate_helmholtz_smoothed_vector
-
-  subroutine calculate_helmholtz_anisotropic_smoothed_scalar(state, s_field)
-    type(state_type), intent(in) :: state
-    type(scalar_field), intent(inout) :: s_field
-    
-    character(len = OPTION_PATH_LEN) :: path
-    logical :: allocated
-    ! NOT a tensor here. Alpha is a scaling constant for the mesh size tensor.
-    real :: alpha
-    type(scalar_field), pointer :: source_field
-    type(vector_field), pointer :: positions
-
-    ewrite(1, *) "In calculate_helmholtz_anisotropic_smoothed_scalar"
-
-    positions       => extract_vector_field(state, "Coordinate")
-    source_field => scalar_source_field(state, s_field, allocated = allocated)
-
-    path = trim(complete_field_path(s_field%option_path)) // "/algorithm"
-    call get_option(trim(path) // "/smoothing_length_scale", alpha)
-    call anisotropic_smooth_scalar(source_field, positions, s_field, alpha, path)
-
-    ewrite(2, *) "alpha = ", alpha
-    ewrite_minmax(source_field%val)
-    ewrite_minmax(s_field%val)
-
-    if(allocated) deallocate(source_field)
-    
-    ewrite(1, *) "Exiting calculate_helmholtz_anisotropic_smoothed_scalar"
-    
-  end subroutine calculate_helmholtz_anisotropic_smoothed_scalar
-
-  subroutine calculate_helmholtz_anisotropic_smoothed_vector(state, v_field)
-    type(state_type), intent(in) :: state
-    type(vector_field), intent(inout) :: v_field
-    
-    character(len = OPTION_PATH_LEN) :: path
-    ! NOT a tensor here. Alpha is a scaling constant for the mesh size tensor.
-    real :: alpha
-    type(vector_field), pointer :: positions, source_field
-
-    ewrite(1, *) "In calculate_helmholtz_anisotropic_smoothed_vector"
-
-    positions       => extract_vector_field(state, "Coordinate")
-    source_field => vector_source_field(state, v_field)
-
-    path = trim(complete_field_path(v_field%option_path)) // "/algorithm"
-    call get_option(trim(path) // "/smoothing_length_scale", alpha)
-    call anisotropic_smooth_vector(source_field, positions, v_field, alpha, path)
-
-    ewrite(2, *) "alpha = ", alpha
-    ewrite_minmax(source_field%val)
-    ewrite_minmax(v_field%val)
-    
-    ewrite(1, *) "Exiting calculate_helmholtz_anisotropic_smoothed_vector"
-    
-  end subroutine calculate_helmholtz_anisotropic_smoothed_vector
 
   subroutine calculate_lumped_mass_smoothed_scalar(state, s_field)
 
@@ -545,5 +491,18 @@ contains
     ewrite(1, *) "Exiting calculate_lumped_mass_smoothed_vector"
 
   end subroutine calculate_lumped_mass_smoothed_vector
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end module field_copies_diagnostics
