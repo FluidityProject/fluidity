8de4716cb33201fc6626df6d71073391ae2dee78
pef <>
2011-02-18 12:54:13 +0000

    Some more work on the adjoint functional evaluation. No idea if it works or not. But with code this tricky, even getting it to compile is an achievement ...

--- adjoint/Adjoint_Functional_Evaluation.F90
+++ adjoint/Adjoint_Functional_Evaluation.F90
@@ -40,39 +40,53 @@ module adjoint_functional_evaluation
   use embed_python
   use python_state
   use libadjoint_data_callbacks
+  use field_options
 
   public :: libadjoint_functional_derivative
 
   contains
 
-  subroutine libadjoint_functional_derivative(var, ndepends, dependencies, values, name_c, start_time, end_time, output) bind(c)
+  subroutine libadjoint_functional_derivative(var, ndepends, dependencies, values, functional_name_c, start_time, end_time, output) bind(c)
     type(adj_variable), intent(in) :: var
     integer(kind=c_int), intent(in), value :: ndepends
     type(adj_variable), dimension(ndepends), intent(in) :: dependencies
     type(adj_vector), dimension(ndepends), intent(in) :: values
-    character(kind=c_char), dimension(ADJ_NAME_LEN), intent(in) :: name_c
+    character(kind=c_char), dimension(ADJ_NAME_LEN), intent(in) :: functional_name_c
     adj_scalar_f, intent(in), value :: start_time
     adj_scalar_f, intent(in), value :: end_time
     type(adj_vector), intent(out) :: output
 
     character(len=PYTHON_FUNC_LEN) :: code
     integer :: i
-    character(len=ADJ_NAME_LEN) :: name_f
+    integer :: s_idx
+    character(len=ADJ_NAME_LEN) :: functional_name_f, variable_name_f
     character(len = 30) :: buffer
     integer :: timestep, ierr, max_timestep, tmp_timestep, min_timestep, nmaterial_phases
     type(state_type), dimension(:,:), pointer :: states ! material_phases x timesteps
-    character(len=OPTION_PATH_LEN) :: material_phase_name
+    character(len=OPTION_PATH_LEN) :: material_phase_name, field_name, mesh_name
+    character(len=6) :: type_string
 
-    do i=1,size(name_c)
-      name_f(i:i) = name_c(i)
+    logical :: is_scalar, is_vector, is_tensor
+    type(scalar_field) :: sfield
+    type(vector_field) :: vfield
+    type(tensor_field) :: tfield
+    type(mesh_type), pointer :: mesh
+    type(state_type) :: derivative_state
+
+    integer :: dim
+
+    call python_reset
+
+    do i=1,size(functional_name_c)
+      functional_name_f(i:i) = functional_name_c(i)
     end do
     ierr = adj_variable_get_timestep(var, timestep)
 
     ! Fetch the python code the user has helpfully supplied.
-    if (.not. have_option("/adjoint/functional::" // trim(name_f) // "/functional_derivative/algorithm")) then
-      FLAbort("We really should have /adjoint/functional::" // trim(name_f) // "/functional_derivative/algorithm")
+    if (.not. have_option("/adjoint/functional::" // trim(functional_name_f) // "/functional_derivative/algorithm")) then
+      FLAbort("We really should have /adjoint/functional::" // trim(functional_name_f) // "/functional_derivative/algorithm")
     endif
-    call get_option("/adjoint/functional::" // trim(name_f) // "/functional_derivative/algorithm", code)
+    call get_option("/adjoint/functional::" // trim(functional_name_f) // "/functional_derivative/algorithm", code)
 
     ! Convert from the list of adj_values/adj_vectors to actual states
     if (ndepends > 0) then
@@ -95,10 +109,72 @@ module adjoint_functional_evaluation
       end do
       call convert_adj_vectors_to_states(dependencies, values, states)
       call python_add_states_time(states)
+    else
+      ewrite(-1,*) "At a minimum, the functional depends on the meshes that the output is to be allocated on."
+      ewrite(-1,*) "Register them as auxiliary dependencies."
+      FLAbort("You really need at least one dependency.")
     endif
 
+    ! We also need to set up the field to differentiate with respect to.
+    ! First, get the material phase and field names
+    ierr = adj_variable_get_name(var, variable_name_f)
+    s_idx = scan(trim(variable_name_f), ":")
+    material_phase_name = variable_name_f(1:s_idx - 1)
+    field_name = variable_name_f(s_idx + 2:len_trim(variable_name_f))
+
+    ! Now we need to find out if we're differentiating with respect to a scalar, vector, or tensor field.
+    is_scalar = .false.
+    is_vector = .false.
+    is_tensor = .false.
+    derivative_state%name = "DerivativeState"
+    if (have_option("/material_phase::" // trim(material_phase_name) // "/scalar_field::" // trim(field_name))) then
+      is_scalar = .true.
+      type_string = "scalar"
+      ! Allocate a scalar_field on the appropriate mesh and add it to python as 'derivative'.
+      call get_option(trim(complete_field_path("/material_phase::" // trim(material_phase_name) // &
+                           "/scalar_field::" // trim(field_name))) // '/mesh/name', mesh_name)
+      mesh => extract_mesh(states(:, timestep), trim(mesh_name))
+      call allocate(sfield, mesh, trim(variable_name_f))
+      call zero(sfield)
+      call insert(derivative_state, sfield, trim(variable_name_f))
+      output = field_to_adj_vector(sfield)
+      call deallocate(sfield)
+    elseif (have_option("/material_phase::" // trim(material_phase_name) // "/vector_field::" // trim(field_name))) then
+      is_vector = .true.
+      type_string = "vector"
+      call get_option(trim(complete_field_path("/material_phase::" // trim(material_phase_name) // &
+                           "/vector_field::" // trim(field_name))) // '/mesh/name', mesh_name)
+      mesh => extract_mesh(states(:, timestep), trim(mesh_name))
+      call get_option("/geometry/dimension", dim)
+      call allocate(vfield, dim, mesh, trim(variable_name_f))
+      call zero(vfield)
+      call insert(derivative_state, vfield, trim(variable_name_f))
+      output = field_to_adj_vector(vfield)
+      call deallocate(vfield)
+    elseif (have_option("/material_phase::" // trim(material_phase_name) // "/tensor_field::" // trim(field_name))) then
+      is_tensor = .true.
+      type_string = "tensor"
+      call get_option(trim(complete_field_path("/material_phase::" // trim(material_phase_name) // &
+                           "/tensor_field::" // trim(field_name))) // '/mesh/name', mesh_name)
+      mesh => extract_mesh(states(:, timestep), trim(mesh_name))
+      call allocate(tfield, mesh, trim(variable_name_f))
+      call zero(tfield)
+      call insert(derivative_state, tfield, trim(variable_name_f))
+      output = field_to_adj_vector(tfield)
+      call deallocate(tfield)
+    else
+      FLAbort("Unknown variable, couldn't find it as a scalar/vector/tensor field")
+    endif
+
+    ! Now we need to do some more bloody shuffling because of the awkward python_state interfaces --
+    ! We have already set up states to be exactly as we like it, there is no way to insert a scalar field
+    ! without having it associated with a state. *Grumble*
+    call python_run_string("megastates = states; del states")
+    call python_add_state(derivative_state)
+    call python_run_string("derivative = states['DerivativeState']." // trim(type_string) // "_fields[" // trim(variable_name_f) // "]")
+    call python_run_string("states = megastates; del megastates")
+
     ! Also set up some useful variables for the user to use
-    call python_reset
     write(buffer,*) start_time
     call python_run_string("time = " // trim(buffer))
 
@@ -108,10 +184,15 @@ module adjoint_functional_evaluation
     write(buffer, *) timestep
     call python_run_string("n = " // trim(buffer))
 
+    ! OK! We're ready for the user's code:
+    call python_run_string(trim(code))
+
     if (max_timestep >= 0) then
       call deallocate(states)
       deallocate(states)
     endif
+
+    call deallocate(derivative_state)
 
   end subroutine libadjoint_functional_derivative
 
