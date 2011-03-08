fc5a542e5caf1ef56746365fd2c9b16ceb5f3e63
btollit <>
2011-03-08 15:38:58 +0000

    Change the radiation checking of all groups flux convergence to not consider old values less than epsilon
    Lower the tolerances of the test case

--- radiation/Radiation_Check_Flux_Convergence.F90
+++ radiation/Radiation_Check_Flux_Convergence.F90
@@ -28,7 +28,6 @@
 #include "fdebug.h"
 
 module radiation_check_flux_convergence
-
    
    use futils
    use global_parameters, only : OPTION_PATH_LEN
@@ -55,7 +54,7 @@ contains
                                         max_change_flux, &
                                         flux_converged)  
       
-      !!< Check the neutral particle flux convergence
+      !!< Check the neutral particle flux relative convergence for all energy groups 
 
       type(state_type), intent(in) :: state
       character(len=*), intent(in) :: np_radmat_name
@@ -67,12 +66,15 @@ contains
       ! local variable
       integer :: g
       integer :: n
+      logical :: found_non_zero_value
       real :: difference
       type(scalar_field), pointer :: np_flux 
       type(scalar_field), pointer :: np_flux_old
       
-      max_change_flux = 0.0
-      flux_converged = .false.
+      max_change_flux      = 0.0
+      flux_converged       = .false.
+      found_non_zero_value = .false.
+      
       group_loop: do g = 1,number_of_energy_groups
          
          call extract_flux_group_g(state, &
@@ -82,15 +84,19 @@ contains
                                    np_flux_old = np_flux_old)
          
          node_loop: do n = 1,node_count(np_flux)
-
+            
+            ! if the flux old is zero (say for a zero BC) then cycle as no need to find relative error
+            if (abs(node_val(np_flux_old,n)) <= epsilon(0.0)) cycle node_loop
+            
+            ! on first find of non zero value set a flag
+            if (.not. found_non_zero_value) found_non_zero_value = .true.
+            
+            ! the absolute difference
             difference = abs(node_val(np_flux,n) - node_val(np_flux_old,n))
-               
-            find_max_change_flux: if ( node_val(np_flux_old,n) > 0) then
-               
-               max_change_flux = max( max_change_flux, difference/abs(node_val(np_flux_old,n)) )
-                
-            end if find_max_change_flux
-               
+            
+            ! the maximum change over all groups and space                  
+            max_change_flux = max( max_change_flux, difference/abs(node_val(np_flux_old,n)) )
+                               
             check_flux: if (difference < abs(node_val(np_flux_old,n)*flux_tolerance)) then
                
                flux_converged = .true.
@@ -107,6 +113,14 @@ contains
           end do node_loop
                   
       end do group_loop
+      
+      no_non_zero: if (.not. found_non_zero_value) then
+         
+         flux_converged = .true.
+         
+         ewrite(1,*) 'WARNING no non zero old neutral particle flux values found in check converegence'
+      
+      end if no_non_zero
       
       ewrite(1,*) 'max_change_flux,flux_converged: ',max_change_flux,flux_converged  
       
