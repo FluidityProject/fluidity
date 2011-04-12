4f022585b890a255931a659f2de8db22ccfd33d6
jgomes <>
2011-04-12 12:07:13 +0000

    Adding state to the wrapper.

--- legacy_reservoir_prototype/src/Multiphase_Prototype_Wrapper.F90
+++ legacy_reservoir_prototype/src/Multiphase_Prototype_Wrapper.F90
@@ -27,160 +27,196 @@
 
 #include "fdebug.h"
 
-subroutine multiphase_prototype_wrapper(filename, filename_len)
+  subroutine multiphase_prototype_wrapper(filename, filename_len)
 
-  use fldebug
-  use state_module
-  use populate_state_module
-  use diagnostic_variables
-  use diagnostic_fields_wrapper
-  use write_state_module
-  use timeloop_utilities
-  use timers
-  use parallel_tools
-  use global_parameters, only: current_time, dt, timestep, option_path_len, &
-                               simulation_start_time, &
-                               simulation_start_cpu_time, &
-                               simulation_start_wall_time
-  use diagnostic_fields_new, only : &
-    & calculate_diagnostic_variables_new => calculate_diagnostic_variables, &
-    & check_diagnostic_dependencies
-  use spud
-  use mp_prototype
-  implicit none
+    use fldebug
+    use state_module
+    use populate_state_module
+    use diagnostic_variables
+    use diagnostic_fields_wrapper
+    use write_state_module
+    use timeloop_utilities
+    use timers
+    use parallel_tools
+    use global_parameters, only: current_time, dt, timestep, option_path_len, &
+         simulation_start_time, &
+         simulation_start_cpu_time, &
+         simulation_start_wall_time
+    use diagnostic_fields_new, only : &
+         & calculate_diagnostic_variables_new => calculate_diagnostic_variables, &
+         & check_diagnostic_dependencies
+    use field_priority_lists
+    use spud
+    use mp_prototype
+    implicit none
 
-!#ifdef HAVE_PETSC
-!#include "finclude/petsc.h"
-!#endif
+    !#ifdef HAVE_PETSC
+    !#include "finclude/petsc.h"
+    !#endif
 
     ! Interface blocks for the initialisation routines we need to call
     interface
-      subroutine set_global_debug_level(n)
-        integer, intent(in) :: n
-      end subroutine set_global_debug_level
+       subroutine set_global_debug_level(n)
+         integer, intent(in) :: n
+       end subroutine set_global_debug_level
 
-      subroutine mpi_init(ierr)
-        integer, intent(out) :: ierr
-      end subroutine mpi_init
+       subroutine mpi_init(ierr)
+         integer, intent(out) :: ierr
+       end subroutine mpi_init
 
-      subroutine mpi_finalize(ierr)
-        integer, intent(out) :: ierr
-      end subroutine mpi_finalize
+       subroutine mpi_finalize(ierr)
+         integer, intent(out) :: ierr
+       end subroutine mpi_finalize
 
-!      subroutine python_init
-!      end subroutine python_init
+       !      subroutine python_init
+       !      end subroutine python_init
 
-!      subroutine petscinitialize(s, i)
-!        character(len=*), intent(in) :: s
-!        integer, intent(out) :: i
-!      end subroutine petscinitialize
+       !      subroutine petscinitialize(s, i)
+       !        character(len=*), intent(in) :: s
+       !        integer, intent(out) :: i
+       !      end subroutine petscinitialize
     end interface
 
-  type(state_type), dimension(:), pointer :: state
-  
-  integer, intent(in) :: filename_len
-  character(len = filename_len), intent(in) :: filename
+    type(state_type), dimension(:), pointer :: state
 
-  character(len = option_path_len) :: simulation_name
-  real :: finish_time
-  integer :: ierr, i, dump_no = 0
+    integer, intent(in) :: filename_len
+    character(len = filename_len), intent(in) :: filename
+
+    character(len = option_path_len) :: simulation_name
+    real :: finish_time
+    integer :: ierr, i, dump_no = 0
+
+    integer :: ntsol, nonlinear_iterations
 
 #ifdef HAVE_MPI
     call mpi_init(ierr)
     assert(ierr == MPI_SUCCESS)
 #endif
 
-!#ifdef HAVE_PETSC
-!    call PetscInitialize(PETSC_NULL_CHARACTER, ierr)
-!#endif
+    !#ifdef HAVE_PETSC
+    !    call PetscInitialize(PETSC_NULL_CHARACTER, ierr)
+    !#endif
 
-  call set_simulation_start_times()
-  call initialise_walltime
-  timestep = 0
+    call set_simulation_start_times()
+    call initialise_walltime
+    timestep = 0
 
-  call initialise_write_state
-  call populate_state(state)
+#ifdef HAVE_MEMORY_STATS
+    ! this is to make sure the option /io/log_output/memory_diagnostics is read
+    call reset_memory_logs()
+#endif
 
-  ! Check the diagnostic field dependencies for circular dependencies
-  call check_diagnostic_dependencies(state)
+    call initialise_write_state
+    ! Read state from .flml file
+    call populate_state(state)
 
-  ! set the remaining timestepping options, needs to be before any diagnostics are calculated
-  call get_option("/timestepping/timestep", dt)
-!  if(have_option("/timestepping/adaptive_timestep/at_first_timestep")) then
-!    call calc_cflnumber_field_based_dt(state, dt, force_calculation = .true.)
-!    call set_option("/timestepping/timestep", dt)
-!  end if
+    ! Check the diagnostic field dependencies for circular dependencies
+    call check_diagnostic_dependencies(state)
 
-  call get_option("/timestepping/current_time", current_time)
-  call get_option("/timestepping/finish_time", finish_time)
+    ! set the remaining timestepping options, needs to be before any diagnostics are calculated
+    call get_option("/timestepping/timestep", dt)
+    !  if(have_option("/timestepping/adaptive_timestep/at_first_timestep")) then
+    !    call calc_cflnumber_field_based_dt(state, dt, force_calculation = .true.)
+    !    call set_option("/timestepping/timestep", dt)
+    !  end if
 
-  call get_option('/simulation_name',simulation_name)
-  call initialise_diagnostics(trim(simulation_name),state)
+    call get_option("/timestepping/current_time", current_time)
+    call get_option("/timestepping/finish_time", finish_time)
 
-  call calculate_diagnostic_variables(state)
-  call calculate_diagnostic_variables_new(state)
+    call get_option('/simulation_name',simulation_name)
+    call initialise_diagnostics(trim(simulation_name),state)
 
-  if( &
-      ! if this is not a zero timestep simulation (otherwise, there would
-      ! be two identical dump files)
-      & current_time < finish_time &
-      ! unless explicitly disabled
-      & .and. .not. have_option("/io/disable_dump_at_start") &
-      & ) then
-    call write_state(dump_no, state)
-  end if
+    call calculate_diagnostic_variables(state)
+    call calculate_diagnostic_variables_new(state)
 
-  ! ******************************
-  ! *** Start of timestep loop ***
-  ! ******************************
-  timestep_loop: do
-    timestep = timestep + 1
+    ! Calculate the number of scalar fields to solve for and their correct
+    ! solve order taking into account dependencies.
+    call get_ntsol(ntsol)
 
-    ewrite(1, *) "********************"
-    ewrite(1, *) "*** NEW TIMESTEP ***"
-    ewrite(1, *) "********************"
-    ewrite(1, *) "Current simulation time: ", current_time
-    ewrite(1, *) "Timestep number: ", timestep
-    ewrite(1, *) "Timestep size (dt): ", dt
-!    if(.not. allfequals(dt)) then
-!       ewrite(-1, *) "Timestep size (dt): ", dt
-!       FLAbort("The timestep is not global across all processes!")
-!    end if
+    call initialise_field_lists_from_options(state, ntsol)
 
-    if(simulation_completed(current_time, timestep)) exit timestep_loop
+!--------------------------------------------------------------------------------------------------------
+! This should be read by the Copy_Outof_Into_State subrts
+
+    ! For multiphase simulations, we have to call calculate_diagnostic_phase_volume_fraction *before*
+    ! copy_to_stored(state,"Old") is called below. Otherwise, OldPhaseVolumeFraction (in the phase
+    ! containing the diagnostic PhaseVolumeFraction) will be zero and 
+    ! NonlinearPhaseVolumeFraction will be calculated incorrectly at t=0.
+    if(option_count("/material_phase/vector_field::Velocity/prognostic") > 1) then
+      call calculate_diagnostic_phase_volume_fraction(state)
+    end if
+
+   ! set the nonlinear timestepping options, needs to be before the adapt at first timestep
+    call get_option('/timestepping/nonlinear_iterations',nonlinear_iterations,&
+         & default=1)
+    call get_option("/timestepping/nonlinear_iterations/tolerance", &
+         & nonlinear_iteration_tolerance, default=0.0)
+
+
+!--------------------------------------------------------------------------------------------------------
+
 
     if( &
-        ! Do not dump at the start of the simulation (this is handled by write_state call earlier)
-        & current_time > simulation_start_time &
-        ! Do not dump at the end of the simulation (this is handled by later write_state call)
-        & .and. current_time < finish_time &
-        ! Test write_state conditions
-        & .and. do_write_state(current_time, timestep) &
-        & ) then
-      call write_state(dump_no, state)
+                                ! if this is not a zero timestep simulation (otherwise, there would
+                                ! be two identical dump files)
+         & current_time < finish_time &
+                                ! unless explicitly disabled
+         & .and. .not. have_option("/io/disable_dump_at_start") &
+         & ) then
+       call write_state(dump_no, state)
     end if
-        
-    ! Call the multiphase_prototype code  
-    call multiphase_prototype()
-    
-    current_time = current_time + dt
-    
-  end do timestep_loop
-  ! ****************************
-  ! *** END OF TIMESTEP LOOP ***
-  ! ****************************
 
-  ! Dump at end, unless explicitly disabled
-  if(.not. have_option("/io/disable_dump_at_end")) then
-    call write_state(dump_no, state)
-  end if
-  
-  ! Deallocate state
-  do i = 1, size(state)
-    call deallocate(state(i))
-  end do
-  deallocate(state)
-  
+    ! ******************************
+    ! *** Start of timestep loop ***
+    ! ******************************
+    timestep_loop: do
+       timestep = timestep + 1
+
+       ewrite(1, *) "********************"
+       ewrite(1, *) "*** NEW TIMESTEP ***"
+       ewrite(1, *) "********************"
+       ewrite(1, *) "Current simulation time: ", current_time
+       ewrite(1, *) "Timestep number: ", timestep
+       ewrite(1, *) "Timestep size (dt): ", dt
+       !    if(.not. allfequals(dt)) then
+       !       ewrite(-1, *) "Timestep size (dt): ", dt
+       !       FLAbort("The timestep is not global across all processes!")
+       !    end if
+
+       if(simulation_completed(current_time, timestep)) exit timestep_loop
+
+       if( &
+                                ! Do not dump at the start of the simulation (this is handled by write_state call earlier)
+            current_time > simulation_start_time &
+                                ! Do not dump at the end of the simulation (this is handled by later write_state call)
+            .and. current_time < finish_time &
+                                ! Test write_state conditions
+            .and. do_write_state(current_time, timestep) &
+            ) then
+          call write_state(dump_no, state)
+       end if
+
+       ! Call the multiphase_prototype code  
+       call multiphase_prototype()
+
+       current_time = current_time + dt
+
+    end do timestep_loop
+    ! ****************************
+    ! *** END OF TIMESTEP LOOP ***
+    ! ****************************
+
+    ! Dump at end, unless explicitly disabled
+    if(.not. have_option("/io/disable_dump_at_end")) then
+       call write_state(dump_no, state)
+    end if
+
+    ! Deallocate state
+    do i = 1, size(state)
+       call deallocate(state(i))
+    end do
+    deallocate(state)
+
   contains
 
     subroutine set_simulation_start_times()
@@ -196,4 +232,4 @@ subroutine multiphase_prototype_wrapper(filename, filename_len)
 
     end subroutine set_simulation_start_times
 
-end subroutine multiphase_prototype_wrapper
+  end subroutine multiphase_prototype_wrapper
