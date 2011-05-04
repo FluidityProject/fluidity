4f4a8739f9b9a8d7e728180ff2143cdb1632d20d
Simon Funke <simon.funke@gmail.com>
2011-05-04 12:50:14 +0100

    also have a nontrivial mean_layerthickness and gravity mangitude for the shallow_water_adjoint_func_eval testcase. we nopw know that all the libadjoint callbacks are implmented correctly

--- tests/shallow_water_adjoint_func_eval/constants.py
+++ tests/shallow_water_adjoint_func_eval/constants.py
@@ -1,17 +1,17 @@
 from math import sin, cos, pi
 import numpy
 
-d0 = 1.0
+d0 = 5.5
 theta = 0.5
 h = 1.0
 functional = 0.5
 dfunctional = 1
+g = 9.81
 
 def eta_src(x, t):
-  return 2*pi*cos(2*pi*x)
-
+  return 2*pi*d0*cos(2*pi*x)
 def u_src(x, t):
-  return numpy.array([2*pi*cos(2*pi*x)+1,0,0])
+  return numpy.array([2*pi*g*cos(2*pi*x)+1,0,0])
 
 def u_exact(x, t):
   return numpy.array([sin(2*pi*x)+t, 0.0, 0.0])
