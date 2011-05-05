f8101c197f5cb4645a16787035ef019cd08ac3f3
Simon Funke <simon.funke@gmail.com>
2011-05-05 11:31:49 +0100

    new test, not tested yet

---
+++ tests/shallow_water_adjoint_func_eval_2d/constants_func_eval_2d.py
@@ -0,0 +1,21 @@
+from math import sin, cos, pi
+import numpy
+
+d0 = 1.0
+theta = 0.5
+h = 1.0
+g = 9.81
+functional = 2
+dfunctional = 4
+
+def eta_src(X, t):
+  return 2*pi*d0*cos(2*pi*X[0] + t) - cos(2*pi*X[0] + t)
+
+def u_src(X, t):
+  return numpy.array([-g*2*pi*cos(2*pi*X[0] + t) + cos(2*pi*X[0] + t),0,0])
+
+def u_exact(X, t):
+  return numpy.array([sin(2*pi*X[0] + t), 0.0, 0.0])
+
+def eta_exact(X, t):
+  return -sin(2*pi*X[0] + t)
