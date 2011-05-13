f4817e61c8ad13280a65683f37463d220b9d1b7e
Simon Funke <simon.funke@gmail.com>
2011-05-13 14:14:01 +0100

    extend the shallow_water_adjoint_func_eval_2d testcase:
    - exploit the 2 dimension
    - non trivial gravity and d0

--- tests/shallow_water_adjoint_func_eval_2d/constants_func_eval_2d.py
+++ tests/shallow_water_adjoint_func_eval_2d/constants_func_eval_2d.py
@@ -1,17 +1,16 @@
 from math import sin, cos, pi
 import numpy
 
-d0 = 1.0
+d0 = 0.9
 theta = 0.5
-h = 1.0
-g = 1.0
+g = 0.8
 functional = 2
 dfunctional = 4
 
 def eta_src(X, t):
   x = X[0]
   y = X[1]
-  return sin(2*pi*X[0]) 
+  return 2*pi*d0*g*cos(2*(t + y)*pi) + 2*pi*d0*g*cos(2*(t + x)*pi) - 2*pi*cos(2*(t + y)*pi) - 2*pi*cos(2*(t + x)*pi)
 
 def u_src(X, t):
   x = X[0]
@@ -21,20 +20,24 @@ def u_src(X, t):
 def u_exact(X, t):
   x = X[0]
   y = X[1]
-  return numpy.array([sin(2*pi*x) + t, 0.0, 0.0])
+  return numpy.array([g*sin(2*(t + x)*pi),  g*sin(2*(t + y)*pi), 0.0])
 
 def eta_exact(X, t):
   x = X[0]
   y = X[1]
-  return 1.0
+  return -sin(2*(t + y)*pi) - sin(2*(t + x)*pi)
 
 def functional_vector_eta(X,t):
   x = X[0]
   y = X[1]
-  return sin(2*pi*x)  
-  #return 1.0 
+  return sin(2*pi*(x+t))  
 
 def functional_vector_u(X,t):
   x = X[0]
   y = X[1]
-  return [0.0, 0.0, 0.0]
+  if t == 0:
+    return [sin(2*pi*x), sin(2*pi*x), 0.0]
+  else:  
+    # Note: The velocity source term currently has to be zero, because the cartesian <-> local projection methods are not implemented in python.
+    #       Therefore, the dot product of the functional_vector_u will always be zero, so we just choose functional_vector_u = 0 here.
+    return [0.0, 0.0, 0.0]
