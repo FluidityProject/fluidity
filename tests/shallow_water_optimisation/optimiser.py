af7dc4502a8f9642c2ddecb2cf3bfd143e23d7af
Simon Funke <simon.funke@gmail.com>
2011-06-02 10:43:11 +0100

    the optimiser can now call the model program succesfully

--- tests/shallow_water_optimisation/optimiser.py
+++ tests/shallow_water_optimisation/optimiser.py
@@ -3,8 +3,10 @@ import os.path
 import numpy
 import argparse
 import libspud
-import shlex, subprocess
-import scipy.optimize 
+import shlex 
+from subprocess import Popen, PIPE
+import scipy.optimize
+import string
 
 # Initial control
 #if not os.path.isfile("control.npy"):
@@ -13,18 +15,23 @@ import scipy.optimize
 
 def optimisation_loop():
   def J(m):
-    cmd = libspud.get_option('/model_template/option_file')
-    option_template = libspud.get_option('/model_template/command_line')
-    args = shlex.split(cmd)
-    assert(False)
-    subprocess.call()
-    u = solve_poisson(n, h, m, q)
-    d = numpy.dot(Q, u)
-    return get_func(h, m, m_opt, d, dobs, alpha)
+    command_line = libspud.get_option('/model_template/command_line')
+    option_file = libspud.get_option('/model_template/option_file')
+    args = shlex.split(command_line)
+    args.append(option_file)
+    p = Popen(args, stdout=PIPE,stderr=PIPE)
+    out = string.join(p.stdout.readlines() )
+    outerr = string.join(p.stderr.readlines() )
+    if p.wait() != 0:
+      print "Model execution failed: "
+      print outerr
+      print "See .. for more information."
+      exit()
+    print out
+    return 0.0
 
   def Jprime(m):
-    u = solve_poisson(n, h, m, q)
-    return get_Jfuncm(n, h, m, m_opt, q, u, dobs, alpha)
+    return 0.0
 
 
   # Start the optimisation loop
@@ -32,16 +39,13 @@ def optimisation_loop():
   tol = libspud.get_option('/optimisation_options/tolerance')
   print "Using ", algo, " as optimisation algorithm."
   if algo == 'BFGS':
-      res = scipy.optimize.fmin_bfgs(J, numpy.ones(n), Jprime, gtol=tol, full_output=1)
+      res = scipy.optimize.fmin_bfgs(J, numpy.ones(10), Jprime, gtol=tol, full_output=1)
   else:
     print "Unknown optimisation algorithm in option path."
     exit()
   
   print "Functional value J(m): ", res[1]
   print "Control state m: ", res[0]
-  print "Control state error m - m_opt =  ", res[0] - m_opt
-
-
 
 ################# Main program ###################
 def main():
@@ -55,7 +59,11 @@ def main():
   if not libspud.have_option('/optimisation_name/'):
     print "File", args.filename, "is not a valid .oml file."
     exit()
+  if not os.path.isfile(libspud.get_option('/model_template/option_file')):
+      print "Could not find ", libspud.get_option('/model_template/option_file') ," as specified in /model_template/option_file"
+      exit()
   
+
   optimisation_loop() 
 
 if '__main__'==__name__:
