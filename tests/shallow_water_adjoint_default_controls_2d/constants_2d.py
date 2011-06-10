fd9ba5fca7956a4086700487e55022631875afc1
Simon Funke <simon.funke@gmail.com>
2011-06-10 21:11:10 +0100

    a 2d test for the controls

--- tests/shallow_water_adjoint_default_controls_2d/constants_2d.py
+++ tests/shallow_water_adjoint_default_controls_2d/constants_2d.py
@@ -10,16 +10,20 @@ dfunctional = 4
 
 def eta_src(X, t):
   x = X[0]
-  return 2*(t + 1)*d0*pi*h*cos(2*pi*x) + h*cos(2*pi*x)
+  y = X[1]
+  return 2*(t + 1)*pi*d0*h*cos(2*pi*x) - 2*pi*d0*sin(2*pi*y) + h*cos(2*pi*x)
 
 def u_src(X, t):
   x = X[0]
-  return numpy.array([-2*(t + 1)*pi*g*h*sin(2*pi*x) + h*sin(2*pi*x),0,0])
+  y = X[1]
+  return numpy.array([ -2*(t + 1)*pi*g*h*sin(2*pi*x) + h*sin(2*pi*x),0,0])
 
 def u_exact(X, t):
   x = X[0]
-  return numpy.array([(t+1)*h*sin(2*pi*x), 0.0, 0.0])
+  y = X[1]
+  return numpy.array([(t + 1)*h*sin(2*pi*x), cos(2*pi*x) + cos(2*pi*y), 0])
 
 def eta_exact(X, t):
   x = X[0]
-  return (t+1)*h*cos(2*pi*x)
+  y = X[1]
+  return (t + 1)*h*cos(2*pi*x)
--- tests/shallow_water_adjoint_default_controls_2d/source_terms.py
+++ tests/shallow_water_adjoint_default_controls_2d/source_terms.py
@@ -2,27 +2,28 @@
 from sage.all_cmdline import *   # import sage library
 _sage_const_2 = Integer(2); _sage_const_1 = Integer(1); _sage_const_0 = Integer(0)
 t = var('t')
-h = var('h')
+x = var('x')
+y = var('y')
 g = var('g')
-H = var('H')
+h = var('h')
+H = var('d0')
 
-# --------------------------------------------------
-
-u = (t+_sage_const_1 )*h*sin(_sage_const_2 *pi*x)
 eta = (t+_sage_const_1 )*h*cos(_sage_const_2 *pi*x)
+u_x = (t+_sage_const_1 )*h*sin(_sage_const_2 *pi*x)
+u_y = cos(_sage_const_2 *pi*x)+cos(_sage_const_2 *pi*y)
 
-u_src = diff(u, t) + g*diff(eta, x)
-eta_src = diff(eta, t) + diff(H*u, x)
+u_x_src = g*diff(eta, x) + diff(u_x, t)
+u_y_src = g*diff(eta, y) + diff(u_y, t)
+eta_src = diff(eta, t) + diff(H*u_x, x) + diff(H*u_y, y)
 
-print "u: ", u
+print "u_x: ", u_x
+print "u_y: ", u_y
 print "eta: ", eta
 
-print "u_src: ", u_src
+print "u_x src: ", u_x_src
+print "u_y src: ", u_y_src
 print "eta_src: ", eta_src
 
-J = integrate((eta.subs(t=_sage_const_1 ))**_sage_const_2 , x, _sage_const_0 , _sage_const_1 )
+J = integrate(integrate((eta.subs(t=_sage_const_1 ))**_sage_const_2 , x, _sage_const_0 , _sage_const_1 ), y, _sage_const_0 , _sage_const_1 )
 print "J(t=1): ", J.subs(h=_sage_const_1 )
 print "diff(J(t=1), h).subs(h=1): ", diff(J, h).subs(h=_sage_const_1 )
-J = integrate((eta.subs(t=_sage_const_0 ))**_sage_const_2 , x, _sage_const_0 , _sage_const_1 )
-print "J(t=0): ", J.subs(h=_sage_const_1 )
-print "diff(J(t=0), h).subs(h=1): ", diff(J, h).subs(h=_sage_const_1 )
