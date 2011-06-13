49cb74ae26d846d47d06e72226569f94c8aff308
Simon Funke <simon.funke@gmail.com>
2011-06-13 18:03:14 +0100

    some tidying up of the adjoint_controls code

--- adjoint/Adjoint_Controls.F90
+++ adjoint/Adjoint_Controls.F90
@@ -42,13 +42,90 @@ module adjoint_controls
 
     contains 
 
+    ! Retrieves the control details from control number control_no in the optin tree. The outputs are:
+    ! control_name: the name of the control 
+    ! state_id: the state number in which the associated control field exists
+    ! field_name: the name of the associated field
+    ! field_type: the type of the associated field
+    ! active: false if the control is active in this timestep (e.g. InitialConditions for timesteps >0)
+    subroutine get_control_details(states, timestep, control_no, control_name, state_id, field_name, field_type, active)
+      type(state_type), dimension(:), intent(in) :: states
+      integer, intent(in) :: control_no, timestep
+      character(len=OPTION_PATH_LEN), intent(out) :: control_name, field_type, field_name 
+      integer, intent(out) :: state_id
+      logical, intent(out) :: active 
+
+      integer :: s_idx
+      character(len=OPTION_PATH_LEN) :: control_type, material_phase_name, name
+
+      call get_option("/adjoint/controls/control[" // int2str(control_no) //"]/name", control_name)
+      call get_option("/adjoint/controls/control[" // int2str(control_no) //"]/type/name", control_type)
+      call get_option("/adjoint/controls/control[" // int2str(control_no) //"]/type::" // trim(control_type) // "/field_name", name)
+      active = .true.
+      s_idx = scan(trim(name), "::")
+      if (s_idx == 0) then 
+        FLAbort("The control " // trim(control_name) // " uses an invalid field_name. It should be of the form Materialphase::Fieldname")
+      end if
+      material_phase_name = name(1:s_idx - 1)
+      field_name = name(s_idx + 2:len_trim(name))
+      ! Find state associated with the material phase
+      do state_id = 1, size(states) 
+        if (trim(states(state_id)%name) == trim(material_phase_name)) then
+          exit
+        end if
+      end do
+      if (.not. trim(states(state_id)%name) == trim(material_phase_name)) then
+        FLAbort("Could not find state " // trim(material_phase_name) // " as specified in control " // trim(control_name) // ".")
+      end if
+        
+      select case (trim(control_type))
+        case ("initial_condition")
+          if (timestep > 0) then
+            active = .false.
+            field_type = ''
+            field_name = ''
+            return
+          endif
+          assert(timestep == 0)
+          if (has_scalar_field(states(state_id), field_name)) then
+            field_type = "scalar"
+          elseif (has_vector_field(states(state_id), field_name)) then
+            field_type = "vector"
+          elseif (has_tensor_field(states(state_id), field_name)) then
+            field_type = "tensor"
+          else
+            ewrite(0, *) "The control field " // trim(field_name) // " specified in control " // trim(control_name) // " is not a field in the state."
+            ewrite(0, *) "The current state is: "
+            call print_state(states(state_id))
+            FLAbort("Check your control's field settings.")
+          end if
+        case ("source_term")
+          if (has_scalar_field(states(state_id), field_name)) then
+            field_type = "scalar"
+          elseif (has_vector_field(states(state_id), field_name)) then
+            field_type = "vector"
+          elseif (has_tensor_field(states(state_id), field_name)) then
+            field_type = "tensor"
+          else
+            ewrite(0, *) "The control field " // trim(name) // " specified in control " // trim(control_name) // & 
+                       & " is not a field in state " // trim(material_phase_name) // "."
+            ewrite(0, *) "The current state is: "
+            call print_state(states(state_id))
+            FLAbort("Check your control's field settings.")
+          end if
+        case ("boundary_condition")
+          FLAbort("Boundary condition control not implemented yet.")
+      end select
+    end subroutine get_control_details
+
     ! Loop through all controls in the option file and store them in files.
     subroutine adjoint_write_controls(timestep, dt, states)
       integer, intent(in) :: timestep
       real, intent(in) :: dt
       type(state_type), dimension(:), intent(in) :: states
 #ifdef HAVE_ADJOINT
-      integer :: nb_controls, i, state_id, s_idx
+      integer :: nb_controls, i, state_id
+      logical :: active
       character(len=OPTION_PATH_LEN) :: field_name, field_type, control_type, control_name, material_phase_name, name
 
       if (.not. have_option("/adjoint/controls")) then
@@ -58,70 +135,19 @@ module adjoint_controls
       nb_controls = option_count("/adjoint/controls/control")
       ! Now loop over the controls and save them to disk
       do i = 0, nb_controls-1
-        call get_option("/adjoint/controls/control[" // int2str(i) //"]/name", control_name)
-        call get_option("/adjoint/controls/control[" // int2str(i) //"]/type/name", control_type)
-        call get_option("/adjoint/controls/control[" // int2str(i) //"]/type::" // trim(control_type) // "/field_name", name)
-        s_idx = scan(trim(name), "::")
-        if (s_idx == 0) then 
-          FLAbort("The control " // trim(control_name) // " uses an invalid field_name. It should be of the form Materialphase::Fieldname")
+        call get_control_details(states, timestep, i, control_name, state_id, field_name, field_type, active)
+        if (active) then
+          ! Save the control parameter to disk
+          call python_reset()
+          call python_add_state(states(state_id))
+          call python_run_string("import pickle;" // &
+                              &  "fname = 'control_" // trim(control_name) // "_" // int2str(timestep) // ".pkl';" // &
+                              &  "f = open(fname, 'wb');" // &
+                              &  "field = state." // trim(field_type) // "_fields['" // trim(field_name) // "'];" // &
+                              &  "pickle.dump(field.val[:], f);" // &
+                              &  "f.close();")
+          call python_reset()
         end if
-        material_phase_name = name(1:s_idx - 1)
-        field_name = name(s_idx + 2:len_trim(name))
-        ! Find state associated with the material phase
-        do state_id = 1, size(states) 
-          if (trim(states(state_id)%name) == trim(material_phase_name)) then
-            exit
-          end if
-        end do
-        if (.not. trim(states(state_id)%name) == trim(material_phase_name)) then
-          FLAbort("Could not find state " // trim(material_phase_name) // " as specified in control " // trim(control_name) // ".")
-        end if
-          
-        select case (trim(control_type))
-          case ("initial_condition")
-            if (timestep > 0) then
-              cycle
-            end if
-            assert(timestep == 0)
-            if (has_scalar_field(states(state_id), field_name)) then
-              field_type = "scalar"
-            elseif (has_vector_field(states(state_id), field_name)) then
-              field_type = "vector"
-            elseif (has_tensor_field(states(state_id), field_name)) then
-              field_type = "tensor"
-            else
-              ewrite(0, *) "The control field " // trim(field_name) // " specified in control " // trim(control_name) // " is not a field in the state."
-              ewrite(0, *) "The current state is: "
-              call print_state(states(state_id))
-              FLAbort("Check your control's field settings.")
-            end if
-          case ("source_term")
-            if (has_scalar_field(states(state_id), field_name)) then
-              field_type = "scalar"
-            elseif (has_vector_field(states(state_id), field_name)) then
-              field_type = "vector"
-            elseif (has_tensor_field(states(state_id), field_name)) then
-              field_type = "tensor"
-            else
-              ewrite(0, *) "The control field " // trim(name) // " specified in control " // trim(control_name) // & 
-                         & " is not a field in state " // trim(material_phase_name) // "."
-              ewrite(0, *) "The current state is: "
-              call print_state(states(state_id))
-              FLAbort("Check your control's field settings.")
-            end if
-          case ("boundary_condition")
-            FLAbort("Boundary condition control not implemented yet.")
-        end select
-        ! Save the control parameter to disk
-        call python_reset()
-        call python_add_state(states(state_id))
-        call python_run_string("import pickle;" // &
-                            &  "fname = 'control_" // trim(control_name) // "_" // int2str(timestep) // ".pkl';" // &
-                            &  "f = open(fname, 'wb');" // &
-                            &  "field = state." // trim(field_type) // "_fields['" // trim(field_name) // "'];" // &
-                            &  "pickle.dump(field.val[:], f);" // &
-                            &  "f.close();")
-        call python_reset()
       end do
 #endif
     end subroutine adjoint_write_controls
@@ -133,6 +159,7 @@ module adjoint_controls
       type(state_type), dimension(:), intent(in) :: states
 #ifdef HAVE_ADJOINT
       integer :: nb_controls, i, state_id, s_idx
+      logical :: active
       character(len=OPTION_PATH_LEN) :: field_name, field_type, control_type, control_name, material_phase_name, name
 
       ! Do not read anything if we are supposed to write the controls
@@ -142,70 +169,19 @@ module adjoint_controls
       nb_controls = option_count("/adjoint/controls/control")
       ! Now loop over the controls, read their controls files and fill the associated fields
       do i = 0, nb_controls-1
-        call get_option("/adjoint/controls/control[" // int2str(i) //"]/name", control_name)
-        call get_option("/adjoint/controls/control[" // int2str(i) //"]/type/name", control_type)
-        call get_option("/adjoint/controls/control[" // int2str(i) //"]/type::" // trim(control_type) // "/field_name", name)
-        s_idx = scan(trim(name), "::")
-        if (s_idx == 0) then 
-          FLAbort("The control " // trim(control_name) // " uses an invalid field_name. It should be of the form Materialphase::Fieldname")
+        call get_control_details(states, timestep, i, control_name, state_id, field_name, field_type, active)
+        if (active) then
+          ! Read the control parameter from disk
+          call python_reset()
+          call python_add_state(states(state_id))
+          call python_run_string("import pickle;" // &
+                              &  "fname = 'control_" // trim(control_name) // "_" // int2str(timestep) // ".pkl';" // &
+                              &  "f = open(fname, 'rb');" // &
+                              &  "field = state." // trim(field_type) // "_fields['" // trim(field_name) // "'];" // &
+                              &  "field.val[:] = pickle.load(f);" // &
+                              &  "f.close();")
+          call python_reset()
         end if
-        material_phase_name = name(1:s_idx - 1)
-        field_name = name(s_idx + 2:len_trim(name))
-        ! Find state associated with the material phase
-        do state_id = 1, size(states) 
-          if (trim(states(state_id)%name) == trim(material_phase_name)) then
-            exit
-          end if
-        end do
-        if (.not. trim(states(state_id)%name) == trim(material_phase_name)) then
-          FLAbort("Could not find state " // trim(material_phase_name) // " as specified in control " // trim(control_name) // ".")
-        end if
-          
-        select case (trim(control_type))
-          case ("initial_condition")
-            if (timestep > 0) then
-              cycle
-            end if
-            assert(timestep == 0)
-            if (has_scalar_field(states(state_id), field_name)) then
-              field_type = "scalar"
-            elseif (has_vector_field(states(state_id), field_name)) then
-              field_type = "vector"
-            elseif (has_tensor_field(states(state_id), field_name)) then
-              field_type = "tensor"
-            else
-              ewrite(0, *) "The control field " // trim(field_name) // " specified in control " // trim(control_name) // " is not a field in the state."
-              ewrite(0, *) "The current state is: "
-              call print_state(states(state_id))
-              FLAbort("Check your control's field settings.")
-            end if
-          case ("source_term")
-            if (has_scalar_field(states(state_id), field_name)) then
-              field_type = "scalar"
-            elseif (has_vector_field(states(state_id), field_name)) then
-              field_type = "vector"
-            elseif (has_tensor_field(states(state_id), field_name)) then
-              field_type = "tensor"
-            else
-              ewrite(0, *) "The control field " // trim(name) // " specified in control " // trim(control_name) // & 
-                         & " is not a field in state " // trim(material_phase_name) // "."
-              ewrite(0, *) "The current state is: "
-              call print_state(states(state_id))
-              FLAbort("Check your control's field settings.")
-            end if
-          case ("boundary_condition")
-            FLAbort("Boundary condition control not implemented yet.")
-        end select
-        ! Read the control parameter from disk
-        call python_reset()
-        call python_add_state(states(state_id))
-        call python_run_string("import pickle;" // &
-                            &  "fname = 'control_" // trim(control_name) // "_" // int2str(timestep) // ".pkl';" // &
-                            &  "f = open(fname, 'rb');" // &
-                            &  "field = state." // trim(field_type) // "_fields['" // trim(field_name) // "'];" // &
-                            &  "field.val[:] = pickle.load(f);" // &
-                            &  "f.close();")
-        call python_reset()
       end do
 #endif
     end subroutine adjoint_load_controls
