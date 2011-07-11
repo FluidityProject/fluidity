11e56c5dd0d1484c3819ce24bb16ca6475b551c1
Xiaohu Guo <xiaohu.guo@stfc.ac.uk>
2011-07-11 15:00:52 +0100

    adding openmp critial directive to reference counter to avoid racing conditions

--- femtools/Refcount_templates.F90
+++ femtools/Refcount_templates.F90
@@ -1,18 +1,24 @@
   subroutine addref_REFCOUNT_TYPE(object)
+#ifdef _OPENMP
+    use omp_lib
+#endif
     !!< Increment the reference count of object creating a new reference
     !!< counter if needed.
     type(REFCOUNT_TYPE), intent(inout), target :: object
     integer, save :: id = 0
 
     if (associated(object%refcount)) then
+    !$OMP CRITICAL
        ! Reference count already exists, just increment it.
        object%refcount%count=object%refcount%count+1
+    !$OMP END CRITICAL
        
     else
+    !$OMP CRITICAL
        id = id + 1
-       object%refcount=>new_refcount("&
-            &REFCOUNT_TYPE", object%name)
+       object%refcount=>new_refcount("REFCOUNT_TYPE", object%name)
        object%refcount%id = id
+    !$OMP END CRITICAL
     end if
     
   end subroutine addref_REFCOUNT_TYPE
@@ -29,8 +35,10 @@
     end if
        
     ! Reference count already exists, just increment it.
+    !$OMP CRITICAL
     ptr=>object%refcount%count
     ptr=ptr+1
+    !$OMP END CRITICAL
 
   end subroutine incref_REFCOUNT_TYPE  
   
@@ -45,8 +53,11 @@
        return
     end if
 
+    !$OMP CRITICAL
     object%refcount%count=object%refcount%count-1
+    !$OMP END CRITICAL
 
+    !$OMP CRITICAL
     if (object%refcount%count<=0) then
 
        if (object%refcount%count<0) then
@@ -65,6 +76,7 @@
        deallocate(object%refcount)
        
     end if
+    !$OMP END CRITICAL
 
   end subroutine decref_REFCOUNT_TYPE
 
