3d06301987e169fc59da525cb7f365ae8b43a414
Cian Wilson <cwilson@ldeo.columbia.edu>
2011-07-25 12:01:20 -0400

    Realised previous commit wasn't sufficient to prevent uninitialized values being returned.

--- femtools/Diagnostic_variables.F90
+++ femtools/Diagnostic_variables.F90
@@ -199,6 +199,7 @@ contains
     integer :: stat
     character(len = OPTION_PATH_LEN) :: stat_test_path
 
+    stat_mesh = .false.
     stat_test_path=trim(complete_mesh_path(mesh%option_path,stat))
     if(stat==0) then
        stat_mesh = have_option(trim(stat_test_path) // "/stat/include_in_stat") &
