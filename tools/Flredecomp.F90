69b9a0f7869581089533765fc8a3219b9cb76acc
Jon Hill <jon.hill@imperial.ac.uk>
2011-08-03 22:22:14 +0100

    Fix for flredecomp when reducing number of partitions. We *want* an empty partition or two, so do not check this

--- tools/Flredecomp.F90
+++ tools/Flredecomp.F90
@@ -170,7 +170,7 @@ subroutine flredecomp(input_basename, input_basename_len, output_basename, outpu
   no_active_processes = target_nprocs
   
 #ifdef HAVE_ZOLTAN
-  call zoltan_drive(state, .true., global_min_quality, initialise_fields=.true., ignore_extrusion=skip_initial_extrusion)
+  call zoltan_drive(state, .true., global_min_quality, initialise_fields=.true., ignore_extrusion=skip_initial_extrusion, flredecomping=.true.)
 #else
   call strip_level_2_halo(state, initialise_fields=.true.)
   call sam_drive(state, sam_options(target_nprocs))
