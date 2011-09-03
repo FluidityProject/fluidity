ead56df2997257989a20cf86e1065effd616dd84
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2011-09-03 01:59:24 +0100

    Added a new diagnostic algorithm called bulk_density for multi-material and multi-phase simulations. This computes the bulk density (Material/PhaseVolumeFraction * Density) of the material_phase.

--- diagnostics/Multiphase_Diagnostics.F90
+++ diagnostics/Multiphase_Diagnostics.F90
@@ -42,7 +42,7 @@ module multiphase_diagnostics
    
    private
 
-   public :: calculate_particle_reynolds_number
+   public :: calculate_particle_reynolds_number, calculate_bulk_density
   
    contains
 
@@ -109,8 +109,8 @@ module multiphase_diagnostics
          u_continuous => extract_vector_field(states(continuous_state_index), "Velocity")
          u_particle => extract_vector_field(states(state_index), "Velocity")
          x => extract_vector_field(states(state_index), "Coordinate")
-         viscosity => extract_tensor_field(states(continuous_state_index),"Viscosity")
-         density => extract_scalar_field(states(continuous_state_index),"Density")
+         viscosity => extract_tensor_field(states(continuous_state_index), "Viscosity")
+         density => extract_scalar_field(states(continuous_state_index), "Density")
 
          ! Loop through and integrate over each element
          do ele = 1, element_count(s_field)
@@ -147,5 +147,82 @@ module multiphase_diagnostics
          ewrite(1,*) 'Exiting calculate_particle_reynolds_number'
 
       end subroutine calculate_particle_reynolds_number
+
+      subroutine calculate_bulk_density(states, state_index, s_field)
+         !!< Calculates the bulk density (density * vfrac)
+         !!< for the material_phase in states[state_index].
+
+         type(state_type), dimension(:), intent(inout) :: states
+         integer, intent(in) :: state_index
+         type(scalar_field), intent(inout) :: s_field ! Bulk density field
+
+         !! Local variables
+         integer :: i, stat
+
+         ! Velocities of the continuous and particle phases
+         type(vector_field), pointer :: x
+         type(scalar_field), pointer :: density, vfrac
+
+         ! Counters over the elements and Gauss points
+         integer :: ele, gi
+         ! Transformed quadrature weights.
+         real, dimension(ele_ngi(s_field, 1)) :: detwei
+         ! Inverse of the local coordinate change matrix.
+         real, dimension(mesh_dim(s_field), mesh_dim(s_field), ele_ngi(s_field, 1)) :: J
+
+         ! Field values at each quadrature point.
+         real, dimension(ele_ngi(s_field, 1)) :: bulk_density_gi
+
+         ! Current element global node numbers.
+         integer, dimension(:), pointer :: bulk_density_nodes
+         ! Current bulk_density element shape.
+         type(element_type), pointer :: bulk_density_shape
+
+         ! Local bulk_density matrix for the current element.
+         real, dimension(ele_loc(s_field, 1),ele_loc(s_field, 1)) :: bulk_density_mat
+
+
+         ewrite(1,*) 'Entering calculate_bulk_density'
+
+         ! Zero bulk density field
+         call zero(s_field)
+
+         x => extract_vector_field(states(state_index), "Coordinate")
+         density => extract_scalar_field(states(state_index), "Density")
+         vfrac => extract_scalar_field(states(state_index), "PhaseVolumeFraction", stat)
+         if(stat /= 0) then
+            ! PhaseVolumeFraction field not found, so try searching for a 
+            ! MaterialVolumeFraction instead.
+            vfrac => extract_scalar_field(states(state_index), "MaterialVolumeFraction", stat)
+            if(stat /= 0) then
+               ! Volume fraction field not present, so exit with an error.
+               FLExit("A PhaseVolumeFraction or MaterialVolumeFraction field is required to compute the bulk density.")
+            end if
+         end if
+
+         ! Loop through and integrate over each element
+         do ele = 1, element_count(s_field)
+
+            bulk_density_nodes => ele_nodes(s_field, ele)
+            bulk_density_shape => ele_shape(s_field, ele)
+
+            ! Get detwei
+            call transform_to_physical(x, ele, detwei = detwei)
+
+            ! Calculate the bulk density at each quadrature point
+            bulk_density_gi = ele_val_at_quad(density, ele)*ele_val_at_quad(vfrac, ele)
+
+            ! Invert the mass matrix to get the bulk density value at each node
+            bulk_density_mat = matmul(inverse(shape_shape(bulk_density_shape, bulk_density_shape, detwei)), &
+                  shape_shape(bulk_density_shape, bulk_density_shape, detwei*bulk_density_gi))
+
+            ! Assign the field values to the nodes
+            s_field%val(bulk_density_nodes) = max(s_field%val(bulk_density_nodes), sum(bulk_density_mat,2))
+
+         end do
+
+         ewrite(1,*) 'Exiting calculate_bulk_density'
+
+      end subroutine calculate_bulk_density
 
 end module multiphase_diagnostics
