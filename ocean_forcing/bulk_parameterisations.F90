e6ebf2212e47925b6c3dd1dd643f8b8e9a3b95a0
Jon Hill <jon.hill@imperial.ac.uk>
2011-10-26 10:58:09 +0100

    Working options_check now

--- ocean_forcing/bulk_parameterisations.F90
+++ ocean_forcing/bulk_parameterisations.F90
@@ -416,27 +416,29 @@ subroutine bulk_parameterisations_check_options
     end if
    
     ! check if the diagnostics are on same mesh as the velocity
-    if (have_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field::PhotosyntheticRadiationDownward/")) then
-        call get_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field::PhotosyntheticRadiationDownward/diagnostic/mesh",buffer)
+    if (have_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field::PhotosyntheticRadiationDownward")) then
+        call get_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field::PhotosyntheticRadiationDownward/diagnostic/mesh/name",buffer)
         if (trim(buffer) .ne. "VelocityMesh") then
-            FLExit("The bulk_forcing diagnostics must be on the velocity mesh")
+            FLExit("The bulk_forcing diagnostic PhotosyntheticRadiationDownward must be on the velocity mesh")
         end if
     end if
-    if (have_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field:SalinityFlux/")) then
-        call get_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field::SalinityFlux/diagnostic/mesh", buffer)
+    if (have_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field:SalinityFlux")) then
+        call get_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field::SalinityFlux/diagnostic/mesh/name", buffer)
+        print trim(buffer)
     if (trim(buffer) .ne. "VelocityMesh") then
-            FLExit("The bulk_forcing diagnostics must be on the velocity mesh")
+            FLExit("The bulk_forcing diagnostic SalinityFlux must be on the velocity mesh")
         end if
     end if
-    if (have_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field:HeatFlux/")) then
-        call get_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field::HeatFlux/diagnostic/mesh", buffer)
+    if (have_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field:HeatFlux")) then
+        call get_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field::HeatFlux/diagnostic/mesh/name", buffer)
         if (trim(buffer) .ne. "VelocityMesh") then
-            FLExit("The bulk_forcing diagnostics must be on the velocity mesh")
+            FLExit("The bulk_forcing diagnostic HeatFlux must be on the velocity mesh")
         end if
     end if
-    if (have_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field:MomentumFlux/")) then
-        if (.not. have_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/scalar_field::MomentumFlux/diagnostic/mesh::VelocityMesh")) then
-            FLExit("The bulk_forcing diagnostics must be on the velocity mesh")
+    if (have_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/vector_field::MomentumFlux")) then
+        call get_option("/ocean_forcing/bulk_formulae/output_fluxes_diagnostics/vector_field::MomentumFlux/diagnostic/mesh/name", buffer)
+        if (trim(buffer) .ne. "VelocityMesh") then
+            FLExit("The bulk_forcing diagnostic MomentumFlux must be on the velocity mesh")
         end if
     end if
 
