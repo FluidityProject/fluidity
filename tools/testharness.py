5a36073c2cd3d7679c777eba07a0e017d0badcb1
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-11-02 15:57:38 +0000

    Try to use the xml.etree.ElementTree python library, instead of the one we bundle

--- tools/testharness.py
+++ tools/testharness.py
@@ -12,7 +12,10 @@ import xml.parsers.expat
 
 
 sys.path.insert(0, os.path.join(os.getcwd(), os.path.dirname(sys.argv[0]), os.pardir, "python"))
-import elementtree.ElementTree as etree
+try:
+  import xml.etree.ElementTree as etree
+except ImportError:
+  import elementtree.ElementTree as etree
 
 class TestHarness:
     def __init__(self, length="any", parallel=False, exclude_tags=None, tags=None, file="", verbose=True, justtest=False,
