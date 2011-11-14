605b1aac2885f01fa84c41f9b0ddf0e309b187ed
Colin Cotter <colin.cotter@imperial.ac.uk>
2011-11-14 20:29:43 +0000

    Use correct external subroutine name.

--- main/Shallow_Water.F90
+++ main/Shallow_Water.F90
@@ -638,7 +638,7 @@
            &')) then
          advecting_u_prescribed=> &
               extract_vector_field(state, "AdvectingVelocity")
-         call project_cartesian_to_local_generic(X,advecting_u_prescribed,&
+         call project_cartesian_to_local(X,advecting_u_prescribed,&
               advecting_u)
          call set(advecting_u,advecting_u_prescribed)
          call project_cartesian_to_local(state,advecting_u)
