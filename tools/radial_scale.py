98328352c7a7cd7640726a6e9d3d0750fcc0ca3b
Simon Mouradian <simon.mouradian06@imperial.ac.uk>
2011-11-21 13:08:57 +0000

    Remove python path from the script, this obviously shouldn't be there. Thanks ctjacobs for pointing this out.

--- tools/radial_scale.py
+++ tools/radial_scale.py
@@ -2,8 +2,6 @@
 
 from pylab import *
 import argparse
-import sys
-sys.path.append('/home/slm06/fluidity/sea-ice-coupling/python')
 import vtktools
 
 parser = argparse.ArgumentParser(description='Radially scale a .vtu file.')
