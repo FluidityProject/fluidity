bc995e633b148dd87599b88dcea61576f8245e7a
Adam Candy <adam.candy@imperial.ac.uk>
2012-02-11 14:42:10 +0000

    Creation of four netCDF files from a Python script, three of which are invalid for the test problem.

--- tests/netcdf_read_errors/createnetcdf.py
+++ tests/netcdf_read_errors/createnetcdf.py
@@ -4,30 +4,62 @@ from Scientific.IO.NetCDF import NetCDFFile
 from numpy import arange, zeros
 import height
 
-f = NetCDFFile('height.nc', 'w')
-f.description = 'Example free surface height'
+def create(missingdata = False, missingdimension = False, missingvariable = False):
 
-x = arange(-1.2, 1.21, 0.2)
-y = arange(-1.2, 1.21, 0.2)
-h = zeros((len(x),len(y)))
+  if (missingdata):
+    filename = 'missingdata.nc'
+    description = ', with missing data.'
+  elif (missingdimension):
+    filename = 'missingdimension.nc'
+    description = ', with a missing dimension.'
+  elif (missingvariable):
+    filename = 'missingvariable.nc'
+    description = ', with a missing variable.'
+  else:
+    filename = 'valid.nc'
+    description = '.'
 
-for i in range(len(x)):
-  for j in range(len(y)):
-    # Nice ordering netCDF API - y,x !
-    h[j,i] = height.function([x[i], y[j]])
+  f = NetCDFFile(filename, 'w')
+  f.description = 'Example free surface height' + description
+  
+  if (missingdata):
+    offset = -0.4
+  else:
+    offset = 0.0
 
-# dimensions
-f.createDimension('x', len(x))
-f.createDimension('y', len(y))
+  x = arange(-1.2 + offset, 1.21 + offset, 0.2)
+  y = arange(-1.2, 1.21, 0.2)
+  h = zeros((len(x),len(y)))
 
-# variables
-fx = f.createVariable('x', 'd', ('x',))
-fy = f.createVariable('y', 'd', ('y',))
-fh = f.createVariable('z', 'd', ('x', 'y',))
+  for i in range(len(x)):
+    for j in range(len(y)):
+      # Nice ordering netCDF API - y,x !
+      h[j,i] = height.function([x[i], y[j]])
 
-fx[:] = x
-fy[:] = y
-fh[:] = h
+  # dimensions
+  f.createDimension('x', len(x))
+  if (not missingdimension):
+    f.createDimension('y', len(y))
 
-f.close()
+  # variables
+  fx = f.createVariable('x', 'd', ('x',))
+  fx[:] = x
+  if (not missingdimension):
+    fy = f.createVariable('y', 'd', ('y',))
+    fy[:] = y
+    if (not missingvariable):
+      fh = f.createVariable('z', 'd', ('x', 'y',))
+      fh[:] = h
+  else:
+    fh = f.createVariable('z', 'd', ('x',))
+    fh[:] = x
+
+  f.close()
+
+create()
+create(missingdata=True)
+create(missingdimension=True)
+create(missingvariable=True)
+
+
 
