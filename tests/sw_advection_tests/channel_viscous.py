ca5755df5ec070b3d7a4524a0414bd9beae1e1c5
Colin Cotter <colin.cotter@imperial.ac.uk>
2012-03-02 17:04:00 +0000

    Fiddling with test cases.

--- tests/sw_advection_tests/channel_viscous.py
+++ tests/sw_advection_tests/channel_viscous.py
@@ -16,7 +16,8 @@ Plane Surface(6) = {5};
 
 Physical Line(1) = {1};
 Physical Line(2) = {2};
-Physical Line(3) = {4, 3};
+Physical Line(3) = {4};
+Physical Line(4) = {3};
 Physical Surface(1) = {6};
 Mesh.Optimize=1;
 '''
@@ -37,7 +38,8 @@ Plane Surface(6) = {5};
 
 Physical Line(1) = {1};
 Physical Line(2) = {2};
-Physical Line(3) = {4, 3};
+Physical Line(3) = {4};
+Physical Line(4) = {3};
 Physical Surface(1) = {6};
 Mesh.Optimize=1;
 '''
