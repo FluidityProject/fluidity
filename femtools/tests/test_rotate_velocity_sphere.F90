4e8e068c6de79bd67eaba128b0dd66566bea69ed
Alexandros Avdis <a.avdis@imperial.ac.uk>
2012-05-10 18:23:56 +0100

    Aesthetic improvements to test of rotate_velocity_sphere

--- femtools/tests/test_rotate_velocity_sphere.F90
+++ femtools/tests/test_rotate_velocity_sphere.F90
@@ -16,7 +16,7 @@ subroutine test_rotate_velocity_sphere
   type(vector_field), pointer :: UnitRadialVector_inPolar
   type(vector_field), pointer :: UnitPolarVector_inPolar
   type(vector_field), pointer :: UnitAzimuthalVector_inPolar
-  type(vector_field) :: diff_RadialVector_inPolar, diff_PolarVector_inPolar, diff_AzimuthalVector_inPolar
+  type(vector_field) :: difference
   logical :: fail
 
   call vtk_read_state("data/on_sphere_rotations/spherical_shell_withFields.vtu", state)
@@ -31,42 +31,38 @@ subroutine test_rotate_velocity_sphere
 
   !Test the change of basis from Cartesian to spherical-polar.
 
-  call allocate(diff_RadialVector_inPolar, 3 , mesh, 'difference')
-  call allocate(diff_PolarVector_inPolar, 3 , mesh, 'difference')
-  call allocate(diff_AzimuthalVector_inPolar, 3 , mesh, 'difference')
+  call allocate(difference, 3 , mesh, 'difference')
 
   !Set the components difference-vector equal to the unit radial vector, and then apply
   ! transformation to sperical-polar basis. Then compare with vector already in sperical-polar
   ! basis, obtained from vtu.
-  call set(diff_RadialVector_inPolar, UnitRadialVector_inCartesian)
-  call rotate_velocity_sphere(diff_RadialVector_inPolar, state)
-  call addto(diff_RadialVector_inPolar, UnitRadialVector_inPolar, -1.0)
-  fail = any(diff_RadialVector_inPolar%val > 1e-12)
-  call report_test("[vector basis change: cartesian to spherical-polar of unit-radial vector.]", &
+  call set(difference, UnitRadialVector_inCartesian)
+  call rotate_velocity_sphere(difference, state)
+  call addto(difference, UnitRadialVector_inPolar, -1.0)
+  fail = any(difference%val > 1e-12)
+  call report_test("[vector basis change: Cartesian to spherical-polar of unit-radial vector.]", &
                    fail, .false., "Radial unit vector components not transformed correctly.")
   
   !Set the components difference-vector equal to the unit-polar vector, and then apply
   ! transformation to sperical-polar basis. Then compare with vector already in sperical-polar
   ! basis, obtained from vtu.
-  call set(diff_PolarVector_inPolar, UnitPolarVector_inCartesian)
-  call rotate_velocity_sphere(diff_PolarVector_inPolar, state)
-  call addto(diff_PolarVector_inPolar, UnitPolarVector_inPolar, -1.0)
-  fail = any(diff_PolarVector_inPolar%val > 1e-12)
-  call report_test("[vector basis change: cartesian to spherical-polar of unit-polar vector.]", &
+  call set(difference, UnitPolarVector_inCartesian)
+  call rotate_velocity_sphere(difference, state)
+  call addto(difference, UnitPolarVector_inPolar, -1.0)
+  fail = any(difference%val > 1e-12)
+  call report_test("[vector basis change: Cartesian to spherical-polar of unit-polar vector.]", &
                    fail, .false., "Polar unit vector components not transformed correctly.")
 
   !Set the components difference-vector equal to the unit-azimuthal vector, and then apply
   ! transformation to sperical-polar basis. Then compare with vector already in sperical-polar
   ! basis, obtained from vtu.
-  call set(diff_AzimuthalVector_inPolar, UnitAzimuthalVector_inCartesian)
-  call rotate_velocity_sphere(diff_AzimuthalVector_inPolar, state)
-  call addto(diff_AzimuthalVector_inPolar, UnitAzimuthalVector_inPolar, -1.0)
-  fail = any(diff_AzimuthalVector_inPolar%val > 1e-12)
-  call report_test("[vector basis change: cartesian to spherical-polar of unit-azimuthal vector.]", &
+  call set(difference, UnitAzimuthalVector_inCartesian)
+  call rotate_velocity_sphere(difference, state)
+  call addto(difference, UnitAzimuthalVector_inPolar, -1.0)
+  fail = any(difference%val > 1e-12)
+  call report_test("[vector basis change: Cartesian to spherical-polar of unit-azimuthal vector.]", &
                    fail, .false., "Azimuthal unit vector components not transformed correctly.")
 
-  call deallocate(diff_RadialVector_inPolar)
-  call deallocate(diff_PolarVector_inPolar)
-  call deallocate(diff_AzimuthalVector_inPolar)
+  call deallocate(difference)
 
 end subroutine
