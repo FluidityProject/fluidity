bdef7f5606c037f93d3775a6a0c8dc5fc6947470
Jonathan Bull <j.bull08@imperial.ac.uk>
2012-05-29 17:37:29 +0100

    forgot this vital part

---
+++ examples/backward_facing_step_3d/avrl.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+import numpy
+
+def moving_average(name):
+  npy = numpy.load(str(name))
+  av = numpy.zeros([len(npy[:,0])])
+  av[0]=npy[0,0]
+  init=200.
+  for i in range(1,len(av)):
+    t = npy[i,-1]
+    dt = t-npy[i-1,-1]
+    # a = (t-dt)/t, b=dt/t
+    a = (t-init-dt)/(t-init)
+    b = dt/(t-init)
+    # av = a*old_av + b*new_val
+    rl = av[i-1]*a + npy[i,1]*b
+    av[i] = rl
+
+  return npy,av
+
+#########################################################################
+
+def main():
+  print 'Only run inside numpy_data directory'
+  try:
+    name = sys.argv[1]
+  except:
+    print 'argument is name of numpy data file'
+
+  npy, av = moving_average(name)
+  print npy[:,0]
+  print av
+  numpy.save('av_'+str(name), [av,npy[:,-1]])
+
+if __name__ == "__main__":
+  sys.exit(main())
+
