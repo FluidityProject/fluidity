fd1fdc39c1ade4f4b6350376ecb4e4cdef797b21
Sam Parkinson <s.parkinson11@imperial.ac.uk>
2012-06-01 08:23:37 +0100

    modified mms test case to incorporate buoyancy and scalar field diffusion. removed other test cases for these things.
    not working yet!

--- tests/k-epsilon_mms_p1p1_cv/k_epsilon_tools.py
+++ tests/k-epsilon_mms_p1p1_cv/k_epsilon_tools.py
@@ -1,86 +1,94 @@
-from math import sin, cos
+from math import sin, cos, tanh
+
+g = [0,-1.0]
+nu = 1.0
+sigmaT = 1.0
+sigmak = 1.0
+sigmae = 1.3
+ce1 = 1.44 
+ce2 = 1.92
 
 # simulated variables
 
 def velocity(XX):
    '''Velocity'''
    x = XX[0]; y = XX[1]
-   u=sin(x)*cos(y)
-   v=-1.0*cos(x)*sin(y)
+   u= 0.1*sin(x)*cos(y)
+   v= -0.1*cos(x)*sin(y)
    return [u, v]
 
 def pressure(XX):
    x = XX[0]; y = XX[1]
    return cos(x)*cos(y)-1.0
 
-def rho(XX):
-   '''Density'''
+def rho_p(XX):
+   '''Density perturbation'''
    x = XX[0]; y = XX[1]
-   return cos(x)*cos(y)-1.0
-
+   return 0.1*cos(x)*cos(y)
+   
 def tke(XX):
    '''Turbulent kinetic energy'''
    x = XX[0]; y = XX[1]
    return y+1.0
-
+   
 def eps(XX):
    '''Turbulent dissipation'''
    x = XX[0]; y = XX[1]
    return y+1.0
-
+   
 def nut(XX):
    x = XX[0]; y = XX[1]
    return 0.09*(y+1.0)
-
+   
 # gradients
 
 def grad_u(XX):
    '''grad u'''
    x = XX[0]; y = XX[1]
-   u_x = cos(x)*cos(y)
-   u_y = -1.0*sin(x)*sin(y)
+   u_x = 0.1*cos(x)*cos(y)
+   u_y = -0.1*sin(x)*sin(y)
    return [u_x, u_y]
 
 def grad_v(XX):
    '''grad v'''
    x = XX[0]; y = XX[1]
-   v_x = sin(x)*sin(y)
-   v_y = -1.0*cos(x)*cos(y)
+   v_x = 0.1*sin(x)*sin(y)
+   v_y = -0.1*cos(x)*cos(y)
    return [v_x, v_y]
 
 def grad_p(XX):
    '''grad pressure'''
    x = XX[0]; y = XX[1]
-   p_x=-1.0*sin(x)*cos(y)
-   p_y=-1.0*cos(x)*sin(y)
+   p_x= -1.0*sin(x)*cos(y)
+   p_y= -1.0*cos(x)*sin(y)
    return [p_x, p_y]
 
-def grad_rho(XX):
-   '''grad density'''
+def grad_rho_p(XX):
+   '''grad density perturbation'''
    x = XX[0]; y = XX[1]
-   p_x=-1.0*sin(x)*cos(y)
-   p_y=-1.0*cos(x)*sin(y)
+   p_x= -0.1*sin(x)*cos(y)
+   p_y= -0.1*cos(x)*sin(y)
    return [p_x, p_y]
-
+   
 def grad_tke(XX):
    '''grad turbulent kinetic energy'''
    x = XX[0]; y = XX[1]
    k_x=0.0
    k_y=1.0
    return [k_x, k_y]
-
+   
 def grad_eps(XX):
    '''grad turbulent dissipation'''
    x = XX[0]; y = XX[1]
    eps_x=0.0
    eps_y=1.0
-   return [eps_x, eps_y]
+   return  [eps_x, eps_y]
 
 def grad_nut(XX):
    x = XX[0]; y = XX[1]
    nut_x = 0.0
    nut_y = 0.09
-   return [nut_x, nut_y]
+   return  [nut_x, nut_y]
 
 def grad2_u(XX):
    x = XX[0]; y = XX[1]
@@ -95,6 +103,13 @@ def grad2_v(XX):
    v_yy = -1.0*velocity(XX)[1]
    v_xy = velocity(XX)[0]
    return [v_xx, v_yy, v_xy]
+
+def grad2_rho_p(XX):
+   x = XX[0]; y = XX[1]
+   rho_p_xx = -rho_p(XX)
+   rho_p_yy = -rho_p(XX)
+   rho_p_xy = 0.1*sin(x)*sin(y)
+   return  [rho_p_xx, rho_p_yy, rho_p_xy]
 
 def grad2_tke(XX):
    x = XX[0]; y = XX[1]
@@ -111,13 +126,10 @@ def grad2_eps(XX):
 def forcing_mom(XX):
    '''Forcing function: momentum: correct expansion?'''
    x = XX[0]; y = XX[1]
-   adv = 1.0; beta = 0.0; mass = 1.0;
-   nu = 0.7
 
    u = velocity(XX)[0]
    v = velocity(XX)[1]
    p = pressure(XX)
-   rho = rho(XX)
 
    p_x = grad_p(XX)[0]
    p_y = grad_p(XX)[1]
@@ -144,15 +156,48 @@ def forcing_mom(XX):
    #Sv = mass*rho*u_t + adv*rho*(u*v_x+v*v_y) + p_y - (nu+nut(XX))*(v_xx+v_yy) - 2.*nut_y*v_y - nut_x*(u_y+v_x)
 
    # Correct expansion of viscous terms. Eca paper was wrong.
-   Su = mass*rho*u_t + adv*rho*(u*u_x+v*u_y) + p_x - (nu+nut(XX))*(u_xx+u_yy) - nut_x*u_x - nut_y*u_y
-   Sv = mass*rho*u_t + adv*rho*(u*v_x+v*v_y) + p_y - (nu+nut(XX))*(v_xx+v_yy) - nut_x*v_x - nut_y*v_y
+   Su = (u_t 
+         + (u*u_x+v*u_y) 
+         + p_x 
+         - (nu+nut(XX))*(v_xx+v_yy) + nut_x*u_x+nut_y*u_y 
+         - (rho_p(XX))*g[0])
+   Sv = (u_t 
+         + (u*v_x+v*v_y) 
+         + p_y 
+         - (nu+nut(XX))*(v_xx+v_yy) + nut_x*v_x+nut_y*v_y 
+         - (rho_p(XX))*g[1])
    return (Su, Sv)
+
+def forcing_density(XX):
+   '''Forcing function: temperature field'''
+   x = XX[0]; y = XX[1]
+
+   u = velocity(XX)[0]
+   v = velocity(XX)[1]
+
+   p_x = grad_p(XX)[0]
+   p_y = grad_p(XX)[1]
+   u_x = grad_u(XX)[0]
+   u_y = grad_u(XX)[1]
+   v_x = grad_v(XX)[0]
+   v_y = grad_v(XX)[1]
+   rho_p_x = grad_rho_p(XX)[0]
+   rho_p_y = grad_rho_p(XX)[1]
+   rho_p_xx = grad2_rho_p(XX)[0]
+   rho_p_yy = grad2_rho_p(XX)[1]
+   rho_p_t = 0.0
+
+   nut_x = grad_nut(XX)[0]
+   nut_y = grad_nut(XX)[1]
+
+   ST = (rho_p_t 
+         + (u*rho_p_x+v*rho_p_y) 
+         - 1./sigmaT*(nut(XX)*(rho_p_xx+rho_p_yy) + (nut_x*rho_p_x+nut_y*rho_p_y)))
+   return ST
 
 def forcing_k(XX):
    '''Forcing function: k'''
    x = XX[0]; y = XX[1]
-   adv = 1.0; beta = 0.0; mass = 0.0; rho = 1.0
-   sigmak = 1.0
 
    u = velocity(XX)[0]
    v = velocity(XX)[1]
@@ -171,17 +216,23 @@ def forcing_k(XX):
    u_y = grad_u(XX)[1]
    v_x = grad_v(XX)[0]
    v_y = grad_v(XX)[1]
+   rho_p_x = grad_rho_p(XX)[0]
+   rho_p_y = grad_rho_p(XX)[1]
 
    S = 2.*(u_x**2 + v_y**2 + u_y*v_x) + u_y**2 + v_x**2
 
-   Sk = mass*rho*k_t + adv*rho*(u*k_x+v*k_y) - nut(XX)/sigmak*(k_xx+k_yy) - 1./sigmak*(nut_x*k_x+nut_y*k_y) - nut(XX)*S + e
+   Pk = nut(XX)*S
+   Gk = nut(XX)/sigmaT*(rho_p_x*g[0] + rho_p_y*g[1])
+   Ak = e
+   Sk = (k_t 
+         + (u*k_x+v*k_y) 
+         - 1./sigmak*(nut(XX)*(k_xx+k_yy) + (nut_x*k_x+nut_y*k_y)) 
+         - Pk - Gk + Ak)
    return Sk
 
 def forcing_eps(XX):
    '''Forcing function: epsilon'''
    x = XX[0]; y = XX[1]
-   adv = 1.0; beta = 0.0; mass = 0.0; rho = 1.0
-   sigmae = 1.3; ce1 = 1.44; ce2 = 1.92
 
    u = velocity(XX)[0]
    v = velocity(XX)[1]
@@ -203,21 +254,28 @@ def forcing_eps(XX):
    u_y = grad_u(XX)[1]
    v_x = grad_v(XX)[0]
    v_y = grad_v(XX)[1]
-   S = 2.*(u_x**2 + v_y**2 + u_y*v_x) + u_y**2 + v_x**2
+   rho_p_x = grad_rho_p(XX)[0]
+   rho_p_y = grad_rho_p(XX)[1]
 
-   Se = mass*rho*eps_t + adv*rho*(u*eps_x+v*eps_y) - nut(XX)/sigmae*(eps_xx+eps_yy) - 1./sigmae*(nut_x*eps_x+nut_y*eps_y) - ce1*e/k*nut(XX)*S + ce2*e**2/k
+   S = 2.*(u_x**2 + v_y**2 + u_y*v_x) + u_y**2 + v_x**2
+   Pe = e/k*nut(XX)*S
+   Ge = e/k*nut(XX)/sigmaT*(rho_p_x*g[0] + rho_p_y*g[1])
+   Ae = e**2/k
+   ce3 = tanh(u_y/u_x)
+
+   Se = (eps_t 
+         + (u*eps_x+v*eps_y) 
+         - 1./sigmae*(nut(XX)*(eps_xx+eps_yy) + (nut_x*eps_x+nut_y*eps_y))
+         - ce1*Pe - ce1*ce3*Ge + ce2*Ae)
    return Se
 
 def forcing_mom_noturb(XX):
    '''Forcing function: momentum'''
    x = XX[0]; y = XX[1]
-   adv = 1.0; beta = 0.0; mass = 1.0
-   nu = 0.7
 
    u = velocity(XX)[0]
    v = velocity(XX)[1]
    p = pressure(XX)
-   rho = rho(XX)
 
    p_x = grad_p(XX)[0]
    p_y = grad_p(XX)[1]
@@ -233,6 +291,6 @@ def forcing_mom_noturb(XX):
    v_xy = grad2_v(XX)[2]
    u_t = 0.0
 
-   Su = mass*rho*u_t + adv*rho*(u*u_x+v*u_y) + p_x - nu*(u_xx+u_yy)
-   Sv = mass*rho*u_t + adv*rho*(u*v_x+v*v_y) + p_y - nu*(v_xx+v_yy)
+   Su = u_t + (u*u_x+v*u_y) + p_x - nu*(u_xx+u_yy) - rho_p(XX)*g[0]
+   Sv = u_t + (u*v_x+v*v_y) + p_y - nu*(v_xx+v_yy) - rho_p(XX)*g[1]
    return (Su, Sv)
