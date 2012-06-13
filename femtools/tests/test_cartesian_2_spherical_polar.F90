0683f7d9828ec593a38480cdfe50c2b1b5f4bdff
Alexandros Avdis <a.avdis@imperial.ac.uk>
2012-06-13 20:31:23 +0100

    Added LGPL notice to file and some cosmetic improvements to comments.

--- femtools/tests/test_cartesian_2_spherical_polar.F90
+++ femtools/tests/test_cartesian_2_spherical_polar.F90
@@ -1,5 +1,31 @@
+!    Copyright (C) 2012 Imperial College London and others.
+!    
+!    Please see the AUTHORS file in the main source directory for a full list
+!    of copyright holders.
+!
+!    Prof. C Pain
+!    Applied Modelling and Computation Group
+!    Department of Earth Science and Engineering
+!    Imperial College London
+!
+!    amcgsoftware@imperial.ac.uk
+!    
+!    This library is free software; you can redistribute it and/or
+!    modify it under the terms of the GNU Lesser General Public
+!    License as published by the Free Software Foundation,
+!    version 2.1 of the License.
+!
+!    This library is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!    Lesser General Public License for more details.
+!
+!    You should have received a copy of the GNU Lesser General Public
+!    License along with this library; if not, write to the Free Software
+!    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+!    USA
 subroutine test_cartesian_2_spherical_polar
-  !Test for ensuring the position vector at a point is correctly converted from a cartesian
+  !Subroutine for testing conversion of the position vector components from a Cartesian
   ! basis to a spherical-polar basis.
   use fields
   use vtk_interfaces
@@ -14,8 +40,8 @@ subroutine test_cartesian_2_spherical_polar
   type(vector_field), pointer :: PolarCoordinate
   type(vector_field) :: difference
   integer :: node
-  real, dimension(3) :: X, RTP !Arrays containing a signel node's position vector
-                               ! components in certesian & spherical-polar bases.
+  real, dimension(3) :: X, RTP !Arrays containing a single node's position vector
+                               ! components in Cartesian & spherical-polar bases.
   logical :: fail
 
   call vtk_read_state("data/on_sphere_rotations/spherical_shell_withFields.vtu", state)
@@ -23,8 +49,9 @@ subroutine test_cartesian_2_spherical_polar
   CartesianCoordinate => extract_vector_field(state, "CartesianCoordinate")
   PolarCoordinate => extract_vector_field(state, "PolarCoordinate")
 
-  !Extract the components of points in vtu file in cartesian coordinates, apply transformation and 
-  ! compare with position-vector is sperical-polar coordinates.
+  !Extract the components of points in vtu file in Cartesian coordinates,
+  ! apply transformation and compare with position-vector is spherical-polar
+  ! coordinates.
   call allocate(difference, 3 , mesh, 'difference')
   do node=1,node_count(CartesianCoordinate)
     X = node_val(CartesianCoordinate, node)
