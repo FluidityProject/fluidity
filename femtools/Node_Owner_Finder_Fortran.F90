71f4396e14edefefc77c3ba19bd0129d4ab53c04
Mark Filipiak <mjf@staffmail.ed.ac.uk>
2012-08-02 13:14:41 +0100

    Backed out change to find_serial() in node_owner_finder_find_multiple_positions(Node_Owner_Finder_Fortran.F90. Using this code with global=.false. needs to be checked for each case. I think that some of the existing cases will not work in general in parallel.

--- femtools/Node_Owner_Finder_Fortran.F90
+++ femtools/Node_Owner_Finder_Fortran.F90
@@ -228,15 +228,8 @@ contains
         closest_ele_id = -1
         ! We don't tolerate very large ownership failures
         closest_miss = out_of_bounds_tolerance
-        possible_elements_loop: do j = 1, nele_ids
+        do j = 1, nele_ids
           call cnode_owner_finder_get_output(id, possible_ele_id, j)
-          ! If this process does not own this possible_ele_id element then
-          ! don't consider it.  This filter is needed to make this subroutine work in
-          ! parallel even if you just want to look at the local-to-this-process elements.
-          if(.not.element_owned(positions_a,possible_ele_id)) then
-             assert(isparallel())
-             cycle possible_elements_loop
-          end if
           ! Zero tolerance - we're not using an "epsilon-ball" approach here
           if(ownership_predicate(positions_a, possible_ele_id, positions(:, i), 0.0, miss = miss)) then
             ele_ids(i) = possible_ele_id
@@ -247,7 +240,7 @@ contains
             closest_ele_id = possible_ele_id
             closest_miss = miss
           end if
-       end do possible_elements_loop
+       end do
 
         ! We didn't find an owner, so choose the element with the closest miss
         ele_ids(i) = closest_ele_id
@@ -325,6 +318,9 @@ contains
           ! Another processes has a smaller miss for this coordinate
           ele_ids(i) = -1
         end if
+        ! if no process has closest_misses(i) < out_of_bounds_tolerance
+        ! then ele_ids(i) is already set to -1 in positions_loop above
+        ! on all processes.  This matches the find_serial(0) behaviour.
       end do
 #endif
             
