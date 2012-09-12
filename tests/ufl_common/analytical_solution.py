d17d180a8290a5b06a45f1c4c5ca5e54fefc04de
Florian Rathgeber <f.rathgeber10@imperial.ac.uk>
2012-09-12 19:14:25 +0100

    Add Advection-Diffusion MMS test

--- tests/ufl_common/analytical_solution.py
+++ tests/ufl_common/analytical_solution.py
@@ -1,4 +1,11 @@
-from numpy import cos, pi
+from numpy import cos, exp, matrix, pi, sqrt
+
+def advection_diffusion(x, t):
+    dx= (matrix(x)-matrix((0.45 + t,0.5)))
+    r=sqrt(dx*dx.T)
+    D = 0.1 # Diffusivity
+    A = 0.1 # Normalisation
+    return A*(exp((-r**2)/(4*D*t))/(4*pi*D*t))
 
 def helmholtz(X,t):
     n = 8
