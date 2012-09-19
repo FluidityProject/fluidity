dcfb261556bb8c04cd6fa9cef3ce139e4d05d1ff
Frank Milthaler <f.milthaler10@imperial.ac.ic.uk>
2012-09-19 13:51:55 +0100

    adding the option "fsi" to the schema, and checking for required options in the preprocessor

--- preprocessor/Populate_State.F90
+++ preprocessor/Populate_State.F90
@@ -3236,6 +3236,8 @@ contains
        call check_foams_options
     case ("multiphase")
        call check_multiphase_options
+    case ("fsi")
+       call check_fsi_model_options
     case default
        ewrite(0,*) "Problem type:", trim(problem_type)
        FLAbort("Error unknown problem_type")
@@ -4089,5 +4091,47 @@ if (.not.have_option("/material_phase[0]/vector_field::Velocity/prognostic/vecto
     end if
 
   end subroutine check_multiphase_options
+
+  subroutine check_fsi_model_options
+    integer :: i, ndim, nfmat
+    ! Get dimension:
+    call get_option('/geometry/dimension', ndim)
+    nfmat = option_count('/material_phase')
+    ! Check options for FSI modelling:
+    if (have_option('/embedded_models/fsi_model/one_way_coupling') .and. ndim==1) then 
+      ewrite(-1,*) 'Error: The 1-way Fluid-Structure Interactions are not supported for 1D simulations via the FSI Model'
+      FLExit('Use a 2D or 3D set-up when using a 1-way coupled simulation')
+    end if
+
+    ! Loop for material phases (fluid):
+    do i = 0, nfmat-1
+      if (.not. have_option('/material_phase['//int2str(i)//']/vector_field::Velocity/prognostic/vector_field::Absorption/diagnostic')) then
+        ewrite(-1,*) 'This models relies on an internal algorithm to compute the absorption. Please enable the vector_field "Absorption"'
+        ewrite(-1,*) 'under vector_field "Veclotiy" in your options file'
+      end if
+
+      if (.not. have_option('/material_phase['//int2str(i)//']/scalar_field::SolidConcentration/diagnostic')) then
+        ewrite(-1,*) 'This models relies on an internal algorithm to compute the solid volume fraction on the fluid mesh.'
+        ewrite(-1,*) 'Please enable the scalar_field "SolidConcentration" under "material_phase" in your options file'
+      end if
+
+      if (.not. have_option('/material_phase['//int2str(i)//']/vector_field::SolidForce')) then
+        ewrite(-1,*) 'This models relies on an internal algorithm to compute the force acting on the solid body.'
+        ewrite(-1,*) 'Please enable the vector_field "SolidForce" under "material_phase" in your options file'
+      end if
+
+      if (.not. have_option('/material_phase['//int2str(i)//']/vector_field::SolidVelocity')) then
+        ewrite(-1,*) 'This models relies on an internal algorithm to compute the force acting on the solid body.'
+        ewrite(-1,*) 'Please enable the vector_field "SolidForce" under "material_phase" in your options file'
+      end if
+
+    end do
+
+    if (have_option('/embedded_models/fsi_model/two_way_coupled')) then
+      ewrite(-1,*) 'Error: The FSI Model does not support 2-way Fluid-Solid coupling'
+      ewrite(-1,*) 'Use the Fluidity/FEMDEM approach for 2-way coupling instead'
+      FLExit('2-way coupling of Fluids and Solids is not supported here. Use the Fluidity/FEMDEM approach for 2-way coupling instead (implicit_solids in the schema)')
+    end if
+  end subroutine check_fsi_model_options
 
 end module populate_state_module
