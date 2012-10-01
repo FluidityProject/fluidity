d0989a39fa0e6b21222044669c32a3d30a8248ca
Jeff Gomes <j.gomes@imperial.ac.uk>
2012-10-01 11:43:34 +0100

    Initially set up for adding adaptivity functionality into the multiphase flow model.

---
+++ legacy_reservoir_prototype/src/OutOfState_2_Adapt.F90
@@ -0,0 +1,49 @@
+!    Copyright (C) 2006 Imperial College London and others.
+!
+!    Please see the AUTHORS file in the main source directory for a full list
+!    of copyright holders.
+!
+!    Prof. C Pain
+!    Applied Modelling and Computation Group
+!    Department of Earth Science and Engineering
+!    Imperial College London
+!
+!    amcgsoftware@imperial.ac.uk
+!
+!    This library is free software; you can redistribute it and/or
+!    modify it under the terms of the GNU Lesser General Public
+!    License as published by the Free Software Foundation,
+!    version 2.1 of the License.
+!
+!    This library is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without seven the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!    Lesser General Public License for more details.
+!
+!    You should have received a copy of the GNU Lesser General Public
+!    License along with this library; if not, write to the Free Software
+!    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+!    USA
+
+#include "fdebug.h"
+
+  module Exchange_4_Adapt
+
+
+  contains
+
+    subroutine UpdateFieldsBeforeAdapting( state, &
+         totele, &
+         cv_ndgln, u_ndgln, p_ndgln, x_ndgln, xu_ndgln, mat_ndgln, &
+         cv_sndgln, p_sndgln, u_sndgln, &
+         
+         )
+      implicit none
+
+
+      return
+
+    end subroutine UpdateFieldsBeforeAdapting
+
+
+  end module Exchange_4_Adapt
