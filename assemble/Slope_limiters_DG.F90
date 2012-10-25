8eec2f1e5b5cd33fe29aaa8b1056518f8ec741ed
Sam Parkinson <s.parkinson11@imperial.ac.uk>
2012-10-25 16:52:16 +0100

    - changed equality condition so that alpha is never greater than 1.0 - added overall condition that alpha is not assessed for node if abs(T_val(node) - Tbar) < 1e-14 to try to stop solver failures on cx1

--- assemble/Slope_limiters_DG.F90
+++ assemble/Slope_limiters_DG.F90
@@ -1428,10 +1428,12 @@ contains
 
        !loop over nodes, adjust alpha
        do node = 1, size(T_val)
-          if ( T_val(node) > Tbar + (T_val_max(node) - Tbar)*1.0e-1 ) then
-             alpha = min(alpha,(T_val_max(node)-Tbar)/(T_val(node)-Tbar))
-          else if ( T_val(node) < Tbar + (T_val_min(node) - Tbar)*1.0e-1 ) then
-             alpha = min(alpha,(T_val_min(node)-Tbar)/(T_val(node)-Tbar))
+          if (abs(T_val(node) - Tbar) > 1e-14) then
+             if ( T_val(node) > Tbar + (T_val_max(node) - Tbar) ) then
+                alpha = (T_val_max(node)-Tbar)/(T_val(node)-Tbar)
+             else if ( T_val(node) < Tbar + (T_val_min(node) - Tbar) ) then
+                alpha = (T_val_min(node)-Tbar)/(T_val(node)-Tbar)
+             end if
           end if
        end do
 
