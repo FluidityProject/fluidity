44aabc47a609b58f7eccaa15919d86b80260b3b8
Stephan Kramer <s.kramer@imperial.ac.uk>
2012-10-25 18:00:48 +0100

    Temporarily switching off baij format as it doesn't actually seem to work with gamg. Still keeping the new velocity ordering though.

--- femtools/Sparse_Tools_Petsc.F90
+++ femtools/Sparse_Tools_Petsc.F90
@@ -217,7 +217,7 @@ contains
     
     if (.not. IsParallel()) then
 
-      if (product(lgroup_size)==1) then
+      if (.true.) then !product(lgroup_size)==1) then
         ! Create serial matrix:
         matrix%M=csr2petsc_CreateSeqAIJ(sparsity, matrix%row_numbering, &
           matrix%column_numbering, ldiagonal, use_inodes=use_inodes)
