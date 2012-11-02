d6ecdd45d9d6b5dd35b56a01be29c7d56648085c
Tim Greaves <tim.greaves@imperial.ac.uk>
2012-11-02 12:02:33 +0000

    Formatting error and warning messages for an 80-column terminal, and making the 'no binaries found' error message clearer, following Lawrence's comment.

--- examples/tides_in_the_Mediterranean_Sea/envcheck.mk
+++ examples/tides_in_the_Mediterranean_Sea/envcheck.mk
@@ -19,30 +19,30 @@ envcheck:
 ifneq (${TESTBIN},00)
  # Do we only have fluidity (and not fldecomp) on the existing path?
  ifeq (${TESTBIN},01)
-	@echo "The 'fluidity' binary is available on your system, but the 'fldecomp' binary isn't. This indicates that you have a partial install; perhaps you copied the 'fluidity' binary into a system directory by hand? If you installed from source, please rebuild Fluidity using the '--prefix' argument to specify an install directory, and run 'make install'. If Fluidity was installed for you on the system, please pass this message on to your system administrator and ask them to resolve the problem."
+	@echo "*** ERROR ***\nThe 'fluidity' binary is available on your system, but the 'fldecomp' binary\nisn't. This indicates that you have a partial install; perhaps you copied the\n'fluidity' binary into a system directory by hand? If you installed from\nsource, please rebuild Fluidity using the '--prefix' argument to specify an\ninstall directory, and run 'make install'. If Fluidity was installed for youon\nthe system, please pass this message on to your system administrator and ask\nthem to resolve the problem."
 	@exit 1
  else
   # fluidity isn't on the path; do we have both fluidity and fldecomp locally?
   ifneq (${TESTBINLOCAL},00)
    # Do we only have fluidity (and not fldecomp) locally?
    ifeq (${TESTBINLOCAL},01)
-	@echo "You appear to be running within a partially-built Fluidity source tree. The 'fluidity' binary is present, but the 'fldecomp' binary was not found. Please ensure that 'make fltools' has been run in this source tree before you proceed to run examples."
+	@echo "*** ERROR ***\nYou appear to be running within a partially-built Fluidity source tree. The\n'fluidity' binary is present, but the 'fldecomp' binary was not found. Please\nensure that 'make fltools' has been run in this source tree before you proceed\nto run examples."
 	@exit 1
    else
     # No fluidity binary on the system path or locally
-	@echo "The Fluidity binaries needed to run this example can't be found.  To fix this, either (a) install the Fluidity binary package if you are on Ubuntu, (b) compile Fluidity from source and install it in a system-accessable location, or (c) set your PATH environment variable to point to a built version of Fluidity. Refer to the Fluidity manual for more instructions on any of the above, or contact the fluidity@imperial.ac.uk mailing list."
+	@echo "*** ERROR ***\nThe Fluidity binaries needed to run this example can't be found.\nTo fix this either:\n\n   (a) install the Fluidity binary package if you are on Ubuntu\n   (b) compile Fluidity from source and install it in a system-accessable\n         location\n   (c) set your PATH environment variable to point to a built version of\n         Fluidity.\n\nRefer to the Fluidity manual for more instructions on any of the above,\nor contact the fluidity@imperial.ac.uk mailing list.\n"
 	@exit 1
    endif 
   endif
  endif
  # Do we have both system AND local builds available?
  ifeq (${TESTBINLOCAL},00)
-	@echo "CAUTION: You are running in a built Fluidity source tree, with the fluidity binary also on your default path. This example will run with the version as built in the local source tree.\n\n"
+	@echo "*** WARNING ***\nYou are running in a built Fluidity source tree, with the fluidity binary also\non your default path. This example will run with the version as built in the\nlocal source tree.\n\n"
  endif
 endif
 # Did we entirely fail to find fluidity python?
 ifeq (${TESTPYTHON},11)
-	@echo "Fluidity python support, required by this example, was not found on your system. Please either (a) install the Fluidity binary package if you are on Ubuntu, or (b) get hold of a Fluidity source tree and set your PYTHONPATH to refer to this, as described in the Fluidity manual."
+	@echo "*** ERROR ***\nFluidity python support, required by this example, was not found on your\nsystem. Please either (a) install the Fluidity binary package if you are on\nUbuntu, or (b) get hold of a Fluidity source tree and set your PYTHONPATH to\nrefer to this, as described in the Fluidity manual."
 	@exit 1
 endif
 
