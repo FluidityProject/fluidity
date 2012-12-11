0239aa380fe72aa54ffc10cc9883247a85bfd3e0
Stephan Kramer <s.kramer@imperial.ac.uk>
2012-12-11 23:45:22 +0000

    Hopefully fixing the intel debug build.
    For some bizarre reason intel started to choke on a bit of debugging code, code that is perfectly legal and hasn't changed for a long time. However on reading the code I realized the debugging check was actually incorrect. So hopefully fixing that will also placate the intel compiler.

--- assemble/Free_Surface.F90
+++ assemble/Free_Surface.F90
@@ -2289,8 +2289,8 @@ contains
         call insert(owned_surface_nodes, face_global_nodes(mesh, face))
       end do
       ewrite(2,*) "Number of owned surface nodes:", key_count(owned_surface_nodes)
-      ewrite(2,*) "Number of columns in vertical prolongator:", size(vertical_prolongator,2)
-      if (size(vertical_prolongator,2)>key_count(owned_surface_nodes)) then
+      ewrite(2,*) "Number of columns in vertical prolongator:", size(csr_vertical_prolongator,2)
+      if (size(csr_vertical_prolongator,2)>key_count(owned_surface_nodes)) then
         ewrite(-1,*) "Vertical prolongator seems to be using more surface nodes than the number"
         ewrite(-1,*) "of surface nodes within completely owned surface elements. This indicates"
         ewrite(-1,*) "the parallel decomposition is not done along columns. You shouldn't be using"
