1ef55ea2d8f626973f84e0d070c33185823099c8
Jemma Shipton <jshipton@ic.ac.uk>
2013-01-16 16:47:59 +0000

    work in progress moving scripts and swml templates to the sw_common directory

--- tests/sw_common/common.mk
+++ tests/sw_common/common.mk
@@ -1,11 +1,21 @@
-mesh:
+sphere_mesh:
 	tar -xzvf sw_common/sphere.tgz
 
-swml:
+fsphere_swml:
 	sed -e 's/{SIMULATION}/$(SIMULATION)3/g' -e 's/{MESH}/sphere_ico3/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)3.swml
 	sed -e 's/{SIMULATION}/$(SIMULATION)4/g' -e 's/{MESH}/sphere_ico4/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)4.swml
 	sed -e 's/{SIMULATION}/$(SIMULATION)5/g' -e 's/{MESH}/sphere_ico5/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)5.swml
 	sed -e 's/{SIMULATION}/$(SIMULATION)6/g' -e 's/{MESH}/sphere_ico6/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)6.swml
 
+channel_mesh_convergence:
+	sw_common/generate_mesh_convergence
+
+rossby_swml:
+	sed -e 's/{SIMULATION}/$(SIMULATION)5/g' -e 's/{MESH}/channel5/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)5.swml
+	sed -e 's/{SIMULATION}/$(SIMULATION)10/g' -e 's/{MESH}/channel10/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)10.swml
+	sed -e 's/{SIMULATION}/$(SIMULATION)20/g' -e 's/{MESH}/channel20/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)20.swml
+	sed -e 's/{SIMULATION}/$(SIMULATION)50/g' -e 's/{MESH}/channel50/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)50.swml
+	sed -e 's/{SIMULATION}/$(SIMULATION)100/g' -e 's/{MESH}/channel100/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)100.swml
+
 clean:
-	rm -f *.vtu *.stat *.log sphere*.ele sphere*.node *.pyc *.swml
+	rm -f *.vtu *.stat *.log *.geo *.msh *.ele *.edge *.node *.pyc *.swml
