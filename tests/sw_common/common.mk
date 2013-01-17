55364f26436e58be7d2e025000eafcff6d6bece6
Jemma Shipton <jshipton@ic.ac.uk>
2013-01-17 12:34:52 +0000

    changes to swml as RT1 currently needs to output on the pressure mesh as velocity mesh is quadratic and not supported by vtk

--- tests/sw_common/common.mk
+++ tests/sw_common/common.mk
@@ -1,3 +1,7 @@
+ifndef OUTPUTMESH
+OUTPUTMESH=VelocityMesh
+endif
+
 sphere_mesh:
 	tar -xzvf sw_common/sphere.tgz
 
@@ -14,11 +18,11 @@ channel_mesh_convergence_quads:
 	sw_common/generate_mesh_convergence --quads
 
 rossby_swml:
-	sed -e 's/{SIMULATION}/$(SIMULATION)5/g' -e 's/{MESH}/channel5/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)5.swml
-	sed -e 's/{SIMULATION}/$(SIMULATION)10/g' -e 's/{MESH}/channel10/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)10.swml
-	sed -e 's/{SIMULATION}/$(SIMULATION)20/g' -e 's/{MESH}/channel20/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)20.swml
-	sed -e 's/{SIMULATION}/$(SIMULATION)50/g' -e 's/{MESH}/channel50/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)50.swml
-	sed -e 's/{SIMULATION}/$(SIMULATION)100/g' -e 's/{MESH}/channel100/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)100.swml
+	sed -e 's/{SIMULATION}/$(SIMULATION)5/g' -e 's/{MESH}/channel5/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' -e 's/{OUTPUTMESH}/$(OUTPUTMESH)/g' sw_common/$(SWML) > $(SIMULATION)5.swml
+	sed -e 's/{SIMULATION}/$(SIMULATION)10/g' -e 's/{MESH}/channel10/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' -e 's/{OUTPUTMESH}/$(OUTPUTMESH)/g' sw_common/$(SWML) > $(SIMULATION)10.swml
+	sed -e 's/{SIMULATION}/$(SIMULATION)20/g' -e 's/{MESH}/channel20/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' -e 's/{OUTPUTMESH}/$(OUTPUTMESH)/g' sw_common/$(SWML) > $(SIMULATION)20.swml
+	sed -e 's/{SIMULATION}/$(SIMULATION)50/g' -e 's/{MESH}/channel50/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' -e 's/{OUTPUTMESH}/$(OUTPUTMESH)/g' sw_common/$(SWML) > $(SIMULATION)50.swml
+	sed -e 's/{SIMULATION}/$(SIMULATION)100/g' -e 's/{MESH}/channel100/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' -e 's/{OUTPUTMESH}/$(OUTPUTMESH)/g' sw_common/$(SWML) > $(SIMULATION)100.swml
 
 clean:
 	rm -f *.vtu *.stat *.log *.geo *.msh *.ele *.edge *.node *.pyc *.swml
