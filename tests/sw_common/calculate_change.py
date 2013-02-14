1570bbc507efda36317eee04d3c902de2557db18
Jemma Shipton <jshipton@ic.ac.uk>
2013-02-14 16:05:44 +0000

    work on fsphere and cubed sphere tests

--- tests/sw_common/calculate_change.py
+++
@@ -1,9 +0,0 @@
-from vtktools import VtuDiff
-
-def change(vtu1, vtu2):
-    diff=VtuDiff(vtu1,vtu2)
-    dh=diff.GetField('LayerThickness')
-    maxdh=max(abs(dh.min()),abs(dh.max()))
-    du=diff.GetField('Velocity')
-    maxdu=max(abs(du.min()),abs(du.max()))
-    return [maxdh, maxdu]
--- tests/sw_common/common.mk
+++ tests/sw_common/common.mk
@@ -6,10 +6,7 @@ sphere_mesh:
 	tar -xzvf sw_common/sphere.tgz
 
 fsphere_swml:
-	sed -e 's/{SIMULATION}/$(SIMULATION)3/g' -e 's/{MESH}/sphere_ico3/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)3.swml
-	sed -e 's/{SIMULATION}/$(SIMULATION)4/g' -e 's/{MESH}/sphere_ico4/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)4.swml
-	sed -e 's/{SIMULATION}/$(SIMULATION)5/g' -e 's/{MESH}/sphere_ico5/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)5.swml
-	sed -e 's/{SIMULATION}/$(SIMULATION)6/g' -e 's/{MESH}/sphere_ico6/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > $(SIMULATION)6.swml
+	sed -e 's/{MESH}/sphere_ico3/g' -e 's/{PDEGREE}/$(PDEGREE)/g' -e 's/{UDEGREE}/$(UDEGREE)/g' -e 's/{VDEGREE}/$(VDEGREE)/g' -e 's/{CONSTRAINT}/$(CONSTRAINT)/g' sw_common/$(SWML) > fsphere.swml
 
 channel_mesh_convergence:
 	sw_common/generate_mesh_convergence
