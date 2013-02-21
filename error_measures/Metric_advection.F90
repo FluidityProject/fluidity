e265a12def04c59b106e3ce81782dbd8236a01b7
Jonathan Bull <j.bull08@imperial.ac.uk>
2013-02-21 15:25:41 +0000

    reverting unintended changes.

--- error_measures/Metric_advection.F90
+++ error_measures/Metric_advection.F90
@@ -761,7 +761,7 @@ contains
                     notvisited(ggi)=.false.
 
                     ! correct the orientation of the normal so it points away from iloc
-                    call orientate_cvsurf_normgi(normgi, node_val(x_tfield, x_nodes(iloc)),x_f(:,ggi),normal(:,ggi))
+                    normgi=orientate_cvsurf_normgi(node_val(x_tfield, x_nodes(iloc)),x_f(:,ggi),normal(:,ggi))
 
                     ! calculate u.n
                     udotn=dot_product(u_f(:,ggi), normgi(:))
