3d5c6289f9c66e7df710400d9d120699d62af92c
Frank Milthaler <f.milthaler10@imperial.ac.ic.uk>
2013-04-11 20:08:05 +0100

    the meshconverter now also runs in parallel (except for the exodusii mesh format). Tests and documentation have been changed accordingly.

--- tools/Meshconv.F90
+++ tools/Meshconv.F90
@@ -34,6 +34,9 @@ subroutine Meshconv(c_input_basename, input_basename_len, c_input_mesh_format, i
 
   use global_parameters, only: OPTION_PATH_LEN, is_active_process, no_active_processes, topology_mesh_name
   use fields
+  use parallel_tools, only: isparallel, parallel_filename
+  use halos_registration, only: read_halos, write_halos
+  use fields_halos, only: verify_consistent_local_element_numbering
   use mesh_files
   use state_module
   use iso_c_binding
@@ -62,8 +65,8 @@ subroutine Meshconv(c_input_basename, input_basename_len, c_input_mesh_format, i
   ewrite(1, *) "In Meshconv"
 
   nprocs = getnprocs()
-  if (nprocs > 1) then
-    FLExit("Meshconv must be run in serial!")
+  if (nprocs > 1 .and. input_mesh_format == 'exodusii') then
+    FLExit("Meshconv must be run in serial when reading in an ExodusII mesh!")
   end if
   ! now turn into proper fortran strings (is there an easier way to do this?)
   do i=1, input_basename_len
@@ -83,27 +86,48 @@ subroutine Meshconv(c_input_basename, input_basename_len, c_input_mesh_format, i
   ! this doesn't matter for the conversion of the mesh file,
   ! but is required for the subroutine below
   quad_degree = 5
-  
+
   ! Read in the mesh file:
   position=read_mesh_files(trim(input_basename), &
                       quad_degree=quad_degree, &
                       format=input_mesh_format)
+  ! If reading in a decomposed mesh, read in halos as well:
+  if(isparallel()) then
+    call read_halos(trim(input_basename), position)
+    ! Local element ordering needs to be consistent between processes, otherwise
+    ! code in Halos_Repair (used in halo construction of derived meshes) will fail
+    if (.not. verify_consistent_local_element_numbering(position%mesh)) then
+      ewrite(-1,*) "The local element ordering is not the same between processes"
+      ewrite(-1,*) "that see the same element. This is a necessary condition on the"
+      ewrite(-1,*) "decomposed input meshes for fluidity. The fact that you've"
+      ewrite(-1,*) "obtained such meshes is likely a bug in fldecomp or the"
+      ewrite(-1,*) "checkpointing code. Please report to the fluidity mailing"
+      ewrite(-1,*) "list and state exactly how you've obtained your input files."
+      FLAbort("Inconsistent local element ordering")
+    end if
+  end if
   mesh=position%mesh
   ! Insert mesh and position field into state and
   call insert(state, mesh, mesh%name)
   call insert(state, position, position%name)
   call deallocate(position)
-  ewrite(2,*) "Mesh file was successfully read in"
+  ewrite(1,*) "Mesh file was successfully read in"
 
   ! Writing mesh file:
   assert(associated(state))
   ewrite(1,*) "**********************************"
   ewrite(1,*) "Writing mesh file of format: "//trim(output_mesh_format)
-  call write_mesh_files(trim(input_basename), output_mesh_format, state, mesh)
+  ! If this runs in parallel, write mesh and halos:
+  if(isparallel()) then
+    call write_mesh_files(parallel_filename(trim(input_basename)), output_mesh_format, state, mesh)
+    call write_halos(trim(input_basename), mesh)
+  else
+    call write_mesh_files(trim(input_basename), output_mesh_format, state, mesh)
+  end if
 
   ! We are done here, deallocating state:
   call deallocate(state)
 
-  ewrite(2, *) "Exiting Meshconv"
+  ewrite(1, *) "Exiting Meshconv"
 
 end subroutine Meshconv
