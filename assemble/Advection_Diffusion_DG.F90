20b5ec1c2a82785ea0a52ab28a4e4b7c9f876ebe
Sam Parkinson <s.parkinson11@imperial.ac.uk>
2013-11-05 09:50:43 +0000

    fix addto diffusivity bug

--- assemble/Advection_Diffusion_DG.F90
+++ assemble/Advection_Diffusion_DG.F90
@@ -843,7 +843,7 @@ contains
          eddy_visc => extract_scalar_field(state, "DGLESScalarEddyViscosity", stat=stat)
          call get_option(trim(T%option_path)//"/prognostic"//&
               &"/subgridscale_parameterisation::LES/PrandtlNumber", prandtl)
-         do i = 1, mesh_dim(U)
+         do i = 1, mesh_dim(X)
            call addto(Diffusivity, i, i, eddy_visc, scale=1./prandtl)
          end do
        end if
