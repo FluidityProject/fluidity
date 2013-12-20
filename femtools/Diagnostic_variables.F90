f0f7461a5a50997f4dd73dee64def9b6b20506e1
Michael Lange <michael.lange@imperial.ac.uk>
2013-12-20 10:54:40 +0000

    Fixing bug #1262890: Individual lagrangian detectors are initialised with wrong names, causing detector I/O to fail in parallel.

--- femtools/Diagnostic_variables.F90
+++ femtools/Diagnostic_variables.F90
@@ -1533,7 +1533,7 @@ contains
           default_stat%detector_list%detector_names(static_dete+i)=detector_name
 
           call create_single_detector(default_stat%detector_list, xfield, &
-                detector_location, static_dete+1, LAGRANGIAN_DETECTOR, trim(detector_name))
+                detector_location, static_dete+i, LAGRANGIAN_DETECTOR, trim(detector_name))
        end do
 
        k=static_dete+lagrangian_dete+1
