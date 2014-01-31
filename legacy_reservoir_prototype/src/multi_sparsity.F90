d1b6cb00840d6a8f6d485296450d6a7d1ea6ca86
James Robert Percival <j.percival@imperial.ac.uk>
2014-01-31 14:45:25 +0000

    Fix to my fix, for the case when a coordinate mesh has more elements than nodes.

--- legacy_reservoir_prototype/src/multi_sparsity.F90
+++ legacy_reservoir_prototype/src/multi_sparsity.F90
@@ -302,7 +302,7 @@
 
       allocate( fintran( nonods + 1 ))
       allocate( coltran( max( totele, nonods ) * mx_nface_p1 ))
-      allocate( icount( nonods ))
+      allocate( icount( max( nonods, totele) ))
 
       icount = 0
       do ele = 1, totele
