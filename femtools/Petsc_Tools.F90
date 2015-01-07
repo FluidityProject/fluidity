deb8f744cd0dca230b8a755a57b4954aa419ba02
Stephan Kramer <s.kramer@imperial.ac.uk>
2015-01-07 15:36:39 +0000

    Bug-fix:  work around for a bug in petsc that pops up with intel 15.
    VecGetValues fails a check when passing it a zero-length array. This is fixed in petsc master 18ae1927.

--- femtools/Petsc_Tools.F90
+++ femtools/Petsc_Tools.F90
@@ -608,9 +608,12 @@ contains
 #ifdef DOUBLEP
     do b=1, nfields
       
-      call VecGetValues(vec, nnodp, &
-        petsc_numbering%gnn2unn( 1:nnodp, b ), &
-        array( start+1:start+nnodp ), ierr)
+      ! this check should be unnecessary but is a work around for a bug in petsc, fixed in 18ae1927 (pops up with intel 15)
+      if (nnodp>0) then
+        call VecGetValues(vec, nnodp, &
+          petsc_numbering%gnn2unn( 1:nnodp, b ), &
+          array( start+1:start+nnodp ), ierr)
+      end if
         
       ! go to next field:
       start=start+nnodes
@@ -626,10 +629,12 @@ contains
     allocate(vals(nnodp))
     do b=1, nfields
       
-      call VecGetValues(vec, nnodp, &
-        petsc_numbering%gnn2unn( 1:nnodp, b ), &
-        vals, ierr)
-      array( start+1:start+nnodp ) = vals
+      if (nnodp>0) then
+        call VecGetValues(vec, nnodp, &
+          petsc_numbering%gnn2unn( 1:nnodp, b ), &
+          vals, ierr)
+        array( start+1:start+nnodp ) = vals
+      end if
         
       ! go to next field:
       start=start+nnodes
