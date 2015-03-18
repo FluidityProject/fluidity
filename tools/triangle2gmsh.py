323aff2d32fe331e33f2d2d93e79c39f2dc17437
Frank Milthaler <f.milthaler10@imperial.ac.uk>
2015-03-18 20:22:40 +0000

    Tools: Changes to triangle2gmsh to account for periodic boundary IDs
    Identifying periodic boundaries in the triangle mesh file and
    modifying the mesh object such that the gmsh writer outputs
    four tags/ids for each surface element/facet. Four tags/ids are
    needed by the gmsh reader in Fluidity, whereas the physical id
    is the first tag, and the element owner is the fourth tag.
    Otherwise the element owner from the triangle mesh is stored as
    the second tag, which by default is the geometrical id in a gmsh
    mesh.

--- tools/triangle2gmsh.py
+++ tools/triangle2gmsh.py
@@ -4,6 +4,13 @@ from optparse import OptionParser
 import fluidity.diagnostics.triangletools as triangletools
 import fluidity.diagnostics.gmshtools as gmshtools
 
+def addAdditionalIds(mesh):
+  # only modify the tags of the surface elements:
+  for ele in mesh.GetSurfaceElements():
+    ids = ele.GetIds()
+    # squeezing in two zeros, first is tag is the physical ID, last one is the ElementOwner (ID):
+    ele.SetIds([ids[0], 0, 0, ids[1]])
+
 optparser=OptionParser(usage='usage: %prog <filename>',
                        add_help_option=True,
                        description="""Converts Fluidity-generated Triangle """ + 
@@ -17,4 +24,9 @@ optparser.add_option("--ascii", "-a",
 filename = argv[0]
 
 mesh = triangletools.ReadTriangle(filename)
+hasInternalBoundaries = triangletools.hasPeriodicBoundary(filename)
+if (hasInternalBoundaries):
+  # squeeze in two additional tags before we start writing out the gmsh file:
+  addAdditionalIds(mesh)
+# now we can write the gmsh file:
 gmshtools.WriteMsh(mesh, "%s.msh" % (filename), binary=not options.ascii)
