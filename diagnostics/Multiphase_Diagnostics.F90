10866808cf02a14a4885c030238fe3baafaff246
gbhutani <gbhutani@users.noreply.github.com>
2015-06-18 15:16:00 +0100

    To ensure that molecular viscosity is used for calculating particle Reynolds number when k-epsilon model is activated.

--- diagnostics/Multiphase_Diagnostics.F90
+++ diagnostics/Multiphase_Diagnostics.F90
@@ -109,7 +109,12 @@ module multiphase_diagnostics
          u_continuous => extract_vector_field(states(continuous_state_index), "Velocity")
          u_particle => extract_vector_field(states(state_index), "Velocity")
          x => extract_vector_field(states(state_index), "Coordinate")
-         viscosity => extract_tensor_field(states(continuous_state_index), "Viscosity")
+         if(have_option(trim(states(continuous_state_index)%option_path)//'/subgridscale_parameterisations/k-epsilon')) then
++           viscosity => extract_tensor_field(states(continuous_state_index),"BackgroundViscosity")
++        else
++           viscosity => extract_tensor_field(states(continuous_state_index), "Viscosity")
++         end if
+         ! viscosity => extract_tensor_field(states(continuous_state_index), "Viscosity")
          density => extract_scalar_field(states(continuous_state_index), "Density")
          vfrac => extract_scalar_field(states(continuous_state_index), "PhaseVolumeFraction")
 
