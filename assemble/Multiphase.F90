5a9e4b006d4599baf3940dfe4a3514a11202c17b
gbhutani <gbhutani@users.noreply.github.com>
2015-06-18 15:27:17 +0100

    Using molecular viscosity when calculating the multiphase drag term. BackgroudViscosity tensor field used for viscosity_fluid when k-epsilon model is activated.

--- assemble/Multiphase.F90
+++ assemble/Multiphase.F90
@@ -375,7 +375,13 @@
                   vfrac_particle => extract_scalar_field(state(istate_particle), "PhaseVolumeFraction")
                   density_fluid => extract_scalar_field(state(istate_fluid), "Density")
                   density_particle => extract_scalar_field(state(istate_particle), "Density")
-                  viscosity_fluid => extract_tensor_field(state(istate_fluid), "Viscosity")
+                  ! Make sure that the molecular viscosity us used. Not the effective viscosity
++                 if(have_option(trim(state(istate_fluid)%option_path)//'/subgridscale_parameterisations/k-epsilon')) then
++                    viscosity_fluid => extract_tensor_field(state(istate_fluid),"BackgroundViscosity")
++                 else
++                    viscosity_fluid => extract_tensor_field(state(istate_fluid), "Viscosity")
++                 end if
+                  ! viscosity_fluid => extract_tensor_field(state(istate_fluid), "Viscosity")
          
                   call get_option(trim(state(istate_particle)%option_path)//"/multiphase_properties/particle_diameter", d)
 
