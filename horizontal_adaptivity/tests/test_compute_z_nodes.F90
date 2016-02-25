99faef278d10bf62627f5c0a2adb3a2d8aea7717
Stephan Kramer <s.kramer@imperial.ac.uk>
2016-02-25 18:45:49 +1100

    Unittest fixes after api changes in horiz. adaptivity.

--- horizontal_adaptivity/tests/test_compute_z_nodes.F90
+++ horizontal_adaptivity/tests/test_compute_z_nodes.F90
@@ -7,6 +7,7 @@ subroutine test_compute_z_nodes
   implicit none
 
   type(vector_field) :: z_mesh
+  real :: top_depth
   integer :: stat
   logical :: fail
 
@@ -14,12 +15,15 @@ subroutine test_compute_z_nodes
   call set_option("/geometry/mesh::ExtrudedMesh/from_mesh/extrude/regions[0]/bottom_depth", 1.0, stat=stat)
   call set_option("/geometry/mesh::ExtrudedMesh/from_mesh/extrude/regions[0]/sizing_function/constant", 0.1, stat=stat)
   
-  call compute_z_nodes(z_mesh, 1.0, (/ 0.0 /), min_bottom_layer_frac=1e-3, radial_extrusion=.false., sizing=0.1)
+  top_depth = 0.0
+  call compute_z_nodes(z_mesh, 1.0, (/ 0.0 /), top_depth, min_bottom_layer_frac=1e-3, radial_extrusion=.false., sizing=0.1)
  
   fail = ele_count(z_mesh) /= 10
   call report_test("[compute_z_mesh: ele_count]", fail, .false., "Should be 10")
   fail = node_count(z_mesh) /= 11
   call report_test("[compute_z_mesh: node_count]", fail, .false., "Should be 11")
+  fail = abs(top_depth-1.0)>1e-9
+  call report_test("[compute_z_mesh: top_depth]", fail, .false., "Should be 1.0")
 
   call vtk_write_fields("data/z_mesh", 0, z_mesh, z_mesh%mesh, vfields=(/z_mesh/))
   
