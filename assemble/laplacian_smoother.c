c7e4731d1995a23214fc8781baa8c7696bfd51b3
Tim McManus <t.mcmanus@imperial.ac.uk>
2016-07-19 11:08:58 +0100

    Adding 3D K_{e}

--- assemble/laplacian_smoother.c
+++ assemble/laplacian_smoother.c
@@ -15,7 +15,7 @@ void lap_smoother(int dimension, int num_nodes, int num_elements, int num_surf_e
 		 Istart,Iend,Ii,its,num_nodes_col[num_nodes],j_other=0,len_new=0,j, local_surf_connectivity[dimension*num_surf_elements], global_surf_connectivity[dimension*num_surf_elements];
   PetscMPIInt    size;
   PetscInt       *holder_new;
-  PetscScalar    p_value[4],A_holder[16],grad_holder[6],det,Fe,x_bound_points[num_surf_elements],Vol,
+  PetscScalar    p_value[4],A_holder[16],grad_holder[12],det,Fe,x_bound_points[num_surf_elements],Vol,
                  y_bound_points[num_surf_elements],Area,Ke[4][4],smoothed_x[num_nodes],smoothed_y[num_nodes],value[5];
   PetscViewer    viewer;
   PetscScalar   A_inv_holder[4][4];
@@ -167,9 +167,36 @@ void lap_smoother(int dimension, int num_nodes, int num_elements, int num_surf_e
       - A_holder[3]*(A_holder[4]*(A_holder[9]*A_holder[14]-A_holder[10]*A_holder[13]) -A_holder[5]*(A_holder[8]*A_holder[14]-A_holder[10]*A_holder[12]) + A_holder[6]*(A_holder[8]*A_holder[13]- A_holder[9]*A_holder[12]));
     */
 
-      Vol = fabs(det)/6.0;
+     Vol = fabs(det)/6.0;
+     
+     grad_holder[0]=A_inv_holder[1][0];grad_holder[1]=A_inv_holder[1][1];grad_holder[2]=A_inv_holder[1][2];grad_holder[3]=A_inv_holder[1][3];
+     grad_holder[4]=A_inv_holder[2][0];grad_holder[5]=A_inv_holder[2][1];grad_holder[6]=A_inv_holder[2][2];grad_holder[7]=A_inv_holder[2][3];
+     grad_holder[8]=A_inv_holder[3][0];grad_holder[9]=A_inv_holder[3][1];grad_holder[10]=A_inv_holder[3][2];grad_holder[11]=A_inv_holder[3][3];
+
+     //Ke = Vol*grad_holder'*grad_holder;
+     
+     Ke[0][0]=Vol*(pow(grad_holder[0],2)+pow(grad_holder[4],2)+pow(grad_holder[8],2));
+     Ke[0][1]=Vol*(grad_holder[0]*grad_holder[1]+grad_holder[4]*grad_holder[5]+grad_holder[8]*grad_holder[9]);
+     Ke[0][2]=Vol*(grad_holder[0]*grad_holder[2]+grad_holder[4]*grad_holder[6]+grad_holder[8]*grad_holder[10]);
+     Ke[0][3]=Vol*(grad_holder[0]*grad_holder[3]+grad_holder[4]*grad_holder[7]+grad_holder[8]*grad_holder[11]);
+
+     Ke[1][0]=Vol*(grad_holder[1]*grad_holder[0]+grad_holder[5]*grad_holder[4]+grad_holder[9]*grad_holder[8]);
+     Ke[1][1]=Vol*(pow(grad_holder[1],2)+pow(grad_holder[5],2)+pow(grad_holder[9],2));
+     Ke[1][2]=Vol*(grad_holder[1]*grad_holder[2]+grad_holder[5]*grad_holder[6]+grad_holder[9]*grad_holder[10]);
+     Ke[1][3]=Vol*(grad_holder[1]*grad_holder[3]+grad_holder[5]*grad_holder[7]+grad_holder[9]*grad_holder[11]);
+
+     Ke[2][0]=Vol*(grad_holder[2]*grad_holder[0]+grad_holder[6]*grad_holder[4]+grad_holder[10]*grad_holder[8]);
+     Ke[2][1]=Vol*(grad_holder[2]*grad_holder[1]+grad_holder[6]*grad_holder[5]+grad_holder[10]*grad_holder[9]);
+     Ke[2][2]=Vol*(pow(grad_holder[2],2)+pow(grad_holder[6],2)+pow(grad_holder[10],2));
+     Ke[2][3]=Vol*(grad_holder[2]*grad_holder[3]+grad_holder[6]*grad_holder[7]+grad_holder[10]*grad_holder[11]);
+
+     Ke[3][0]=Vol*(grad_holder[3]*grad_holder[0]+grad_holder[7]*grad_holder[4]+grad_holder[11]*grad_holder[8]);
+     Ke[3][1]=Vol*(grad_holder[3]*grad_holder[1]+grad_holder[7]*grad_holder[5]+grad_holder[11]*grad_holder[9]);
+     Ke[3][2]=Vol*(grad_holder[3]*grad_holder[2]+grad_holder[7]*grad_holder[6]+grad_holder[11]*grad_holder[10]);
+     Ke[3][3]=Vol*(pow(grad_holder[3],2)+pow(grad_holder[7],2)+pow(grad_holder[11],2));
 
 
+     
      if (holder_new[0]<=num_owned_nodes) {
        MatSetValue(K,mapping[convert[0]],mapping[convert[0]],Ke[0][0],ADD_VALUES);
        MatSetValue(K,mapping[convert[0]],mapping[convert[1]],Ke[0][1],ADD_VALUES);
