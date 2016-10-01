c4b1beea1b88549b35f1b85b0a031fb234489612
gdeskos-ic <gd1414@ic.ac.uk>
2016-10-01 11:17:14 +0100

    added the write_output.f90 file

---
+++ turbine_models/Actuator_Line_Write_Output.F90
@@ -0,0 +1,31 @@
+#include "fdebug.h"
+
+module actuator_line_write_output
+    
+    use fldebug
+    use spud
+    use global_parameters, only:FIELD_NAME_LEN,OPTION_PATH_LEN, PYTHON_FUNC_LEN, pi
+    use futils
+
+    use actuator_line_turbine
+    use actuator_line_element
+
+contains
+
+    subroutine actuator_line_element_write_output(act_line,dumb_num)
+
+        implicit none
+        type(ActuatorLineType),intent(in) :: act_line
+        integer,intent(in) :: dumb_num
+        integer :: ielem
+
+        open(2017,File=trim(act_line%name)//'_'//int2str(dumb_num)//'.dat')
+        write(2017,*) 'ielem, AOA, CD, CDL,CM'
+        do ielem=1,act_line%NElem
+        write(2017,*) ielem,',',act_line%EAOA_Last(ielem),',',act_line%ECD(ielem),',',act_line%ECL(ielem),',',act_line%ECM(ielem)
+        end do
+        close(2017)
+ 
+    end subroutine actuator_line_element_write_output
+
+end module actuator_line_write_output
