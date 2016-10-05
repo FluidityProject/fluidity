0c83d08c24ec50e21760b3bede17c8ba5385c200
gdeskos-ic <gd1414@ic.ac.uk>
2016-10-05 18:55:00 +0100

    changed actuatorline output format

--- turbine_models/Actuator_Line_Write_Output.F90
+++ turbine_models/Actuator_Line_Write_Output.F90
@@ -17,14 +17,19 @@ contains
         implicit none
         type(ActuatorLineType),intent(in) :: act_line
         integer,intent(in) :: dump_num
+        character(LEN=40) :: Format
         integer :: ielem
+
 
         open(2017,File=trim(act_line%name)//'_'//int2str(dump_num)//'.dat')
         write(2017,*) 'ielem, AOA, CD, CDL,CM'
+        Format="(I5,A,F10.5,A,F10.5,A,F10.5,A,F10.5)"
         do ielem=1,act_line%NElem
-        write(2017,*) ielem,',',act_line%EAOA_Last(ielem),',',act_line%ECD(ielem),',',act_line%ECL(ielem),',',act_line%ECM(ielem)
+        write(2017,Format) ielem,',',act_line%EAOA_Last(ielem),',',act_line%ECD(ielem),',',act_line%ECL(ielem),',',act_line%ECM(ielem)
         end do
         close(2017)
+
+
  
     end subroutine actuator_line_element_write_output
 
