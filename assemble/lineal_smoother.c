d35f97bf82dc1db86c729f3e7ae63ec118838963
Tim McManus <t.mcmanus@imperial.ac.uk>
2016-11-03 17:32:39 +0000

    3D parallel lineal spring analogy with flml example.

--- assemble/lineal_smoother.c
+++ assemble/lineal_smoother.c
@@ -1,4 +1,4 @@
-static char help[] = "2D Lineal Spring Analogy Smoother in serial and parallel.\n\n";
+static char help[] = "2D/3D Lineal Spring Analogy Smoother in serial and parallel.\n\n";
 #include <petscksp.h>
 #include <stdio.h>
 #include <string.h>
@@ -13,27 +13,28 @@ void lin_smoother(int dimension, int num_nodes, int num_elements, int num_surf_e
   PC             pc;
   PetscErrorCode ierr;
   PetscInt       i,n,m,j,its,nodei,Ii,len_new=0,local_surf_connectivity[dimension*num_surf_elements], global_surf_connectivity[dimension*num_surf_elements],
-                 num_nodes_col_x[num_nodes],num_nodes_col_y[num_nodes];
+    num_nodes_col_x[num_nodes],num_nodes_col_y[num_nodes],num_nodes_col_z[num_nodes];
   PetscScalar    length,x_disp[num_surf_elements],y_disp[num_surf_elements],z_disp[num_surf_elements],smoothed_x[num_nodes],smoothed_y[num_nodes],smoothed_z[num_nodes];
 
-  double lij(int dimension, int nodei, int nodej){
+  double lij(int nodei, int nodej){
+    double xi,yi,zi,xj,yj,zj,length;
     if (nodei == nodej){
     PetscPrintf(PETSC_COMM_WORLD,"Failure: nodei is the same as nodej.\n");
     length = 0.0;
     }
     else if (dimension == 1) {
-      double xi = comp_mesh[dimension*nodei-1];
-      double xj = comp_mesh[dimension*nodej-1];
+      xi = comp_mesh[dimension*nodei-1];
+      xj = comp_mesh[dimension*nodej-1];
       length = labs(xi-xj);
     }
     else if (dimension == 2) {
-      double xi = comp_mesh[dimension*nodei]; double yi = comp_mesh[dimension*nodei+1];
-      double xj = comp_mesh[dimension*nodej]; double yj = comp_mesh[dimension*nodej+1];
+      xi = comp_mesh[dimension*nodei]; yi = comp_mesh[dimension*nodei+1];
+      xj = comp_mesh[dimension*nodej]; yj = comp_mesh[dimension*nodej+1];
       length = sqrt(pow(xi-xj,2)+pow(yi-yj,2));
     }
     else {
-      double xi = comp_mesh[dimension*nodei-3]; double yi = comp_mesh[dimension*nodei-2]; double zi = comp_mesh[dimension*nodei-1];
-      double xj = comp_mesh[dimension*nodej-3]; double yj = comp_mesh[dimension*nodej-2]; double zj = comp_mesh[dimension*nodej-1];
+      xi = comp_mesh[dimension*nodei];yi = comp_mesh[dimension*nodei+1];zi = comp_mesh[dimension*nodei+2];
+      xj = comp_mesh[dimension*nodej];yj = comp_mesh[dimension*nodej+1];zj = comp_mesh[dimension*nodej+2];
       length = sqrt(pow(xi-xj,2)+pow(yi-yj,2)+pow(zi-zj,2));
     }
     
@@ -42,54 +43,153 @@ void lin_smoother(int dimension, int num_nodes, int num_elements, int num_surf_e
 
   /*Lineal Stiffness*/
   double * K_lin(int nodei, int nodej){
-    nodej = nodej-1;
-    static double K_lin_temp[16];
-    double xi = comp_mesh[dimension*nodei]; double yi = comp_mesh[dimension*nodei+1];
-    double xj = comp_mesh[dimension*nodej]; double yj = comp_mesh[dimension*nodej+1];
-    double x_edge = xj - xi; double y_edge = yj - yi;
-    double alpha = atan2(y_edge,x_edge);
-    double inv_length = 1.0/lij(2,nodei,nodej);
     
-    K_lin_temp[0]=inv_length*cos(alpha)*cos(alpha);K_lin_temp[1]=inv_length*sin(alpha)*cos(alpha);K_lin_temp[2]=-1.0*inv_length*cos(alpha)*cos(alpha);K_lin_temp[3]=-1.0*inv_length*cos(alpha)*sin(alpha);
-    K_lin_temp[4]=inv_length*sin(alpha)*cos(alpha);K_lin_temp[5]=inv_length*sin(alpha)*sin(alpha);K_lin_temp[6]=-1.0*inv_length*sin(alpha)*cos(alpha);K_lin_temp[7]=-1.0*inv_length*sin(alpha)*sin(alpha);
-    K_lin_temp[8]=-1.0*inv_length*cos(alpha)*cos(alpha);K_lin_temp[9]=-1.0*inv_length*sin(alpha)*cos(alpha);K_lin_temp[10]=inv_length*cos(alpha)*cos(alpha);K_lin_temp[11]=inv_length*sin(alpha)*cos(alpha);
-    K_lin_temp[12]=-1.0*inv_length*sin(alpha)*cos(alpha);K_lin_temp[13]=-1.0*inv_length*sin(alpha)*sin(alpha);K_lin_temp[14]=inv_length*sin(alpha)*cos(alpha);K_lin_temp[15]=inv_length*sin(alpha)*sin(alpha);
+    nodej = nodej-1;
+    int n;
+    double xi,yi,xj,yj,x_edge,y_edge,alpha,inv_length;
 
-    return K_lin_temp;
+    xi = comp_mesh[dimension*nodei];yi = comp_mesh[dimension*nodei+1];
+    xj = comp_mesh[dimension*nodej];yj = comp_mesh[dimension*nodej+1];
+    x_edge = xj - xi;y_edge = yj - yi;
+    alpha = atan2(y_edge,x_edge);inv_length = 1.0/lij(nodei,nodej);
+    
+    if(dimension == 2){
+      static double K_lin_temp[16];      
+      K_lin_temp[0]=inv_length*cos(alpha)*cos(alpha);
+      K_lin_temp[1]=inv_length*sin(alpha)*cos(alpha);
+      K_lin_temp[2]=-1.0*inv_length*cos(alpha)*cos(alpha);
+      K_lin_temp[3]=-1.0*inv_length*cos(alpha)*sin(alpha);
+
+      K_lin_temp[4]=inv_length*sin(alpha)*cos(alpha);
+      K_lin_temp[5]=inv_length*sin(alpha)*sin(alpha);
+      K_lin_temp[6]=-1.0*inv_length*sin(alpha)*cos(alpha);
+      K_lin_temp[7]=-1.0*inv_length*sin(alpha)*sin(alpha);
+
+      for(n=8;n<16;++n){
+	K_lin_temp[n]=-1.0*K_lin_temp[n-8];
+      }
+
+      return K_lin_temp;
+    }
+    else{
+      double zi,zj,z_edge,phi;
+      static double K_lin_temp_3d[36];
+      zi=comp_mesh[dimension*nodei+2];zj=comp_mesh[dimension*nodei+2];z_edge=zj-zi;phi=acos(z_edge/lij(nodei,nodej));
+      
+      K_lin_temp_3d[0]=inv_length*cos(alpha)*cos(alpha)*sin(phi);
+      K_lin_temp_3d[1]=inv_length*sin(alpha)*cos(alpha)*sin(phi);
+      K_lin_temp_3d[2]=inv_length*cos(alpha)*cos(phi); 
+      K_lin_temp_3d[3]=-1.0*inv_length*cos(alpha)*cos(alpha)*sin(phi);
+      K_lin_temp_3d[4]=-1.0*inv_length*sin(alpha)*cos(alpha)*sin(phi);
+      K_lin_temp_3d[5]=-1.0*inv_length*cos(alpha)*cos(phi); 
+      
+      K_lin_temp_3d[6]=inv_length*sin(alpha)*cos(alpha)*sin(phi);
+      K_lin_temp_3d[7]=inv_length*sin(alpha)*sin(alpha)*sin(phi);
+      K_lin_temp_3d[8]=inv_length*sin(alpha)*cos(phi);
+      K_lin_temp_3d[9]=-1.0*inv_length*sin(alpha)*cos(alpha)*sin(phi);
+      K_lin_temp_3d[10]=-1.0*inv_length*sin(alpha)*sin(alpha)*sin(phi);
+      K_lin_temp_3d[11]=-1.0*inv_length*sin(alpha)*cos(phi);
+      
+      K_lin_temp_3d[12]=inv_length*cos(alpha)*sin(phi)*cos(phi);
+      K_lin_temp_3d[13]=inv_length*sin(alpha)*sin(phi)*cos(phi);
+      K_lin_temp_3d[14]=inv_length*cos(phi)*cos(phi);
+      K_lin_temp_3d[15]=-1.0*inv_length*cos(alpha)*sin(phi)*cos(phi);
+      K_lin_temp_3d[16]=-1.0*inv_length*sin(alpha)*sin(phi)*cos(phi);
+      K_lin_temp_3d[17]=-1.0*inv_length*cos(phi)*cos(phi);
+
+      for(n=18;n<36;++n){
+	K_lin_temp_3d[n]=-1.0*K_lin_temp_3d[n-18];
+      }
+
+      return K_lin_temp_3d;
+
+    }
   }
 
-
-  
   MatCreate(PETSC_COMM_WORLD,&K);
   PetscObjectSetName((PetscObject) K, "Stiffness Matrix");
-  MatSetSizes(K,2*num_owned_nodes,2*num_owned_nodes,PETSC_DETERMINE,PETSC_DETERMINE);
+  MatSetSizes(K,dimension*num_owned_nodes,dimension*num_owned_nodes,PETSC_DETERMINE,PETSC_DETERMINE);
   MatSetUp(K);
 
   for(Ii=0;Ii<num_owned_nodes;Ii++){
+    
+    if(dimension==2){
+  
        for(n=findrm[Ii];n<findrm[Ii+1];++n){
-     	int neb_hold = colm[n-1];
-	    double * tmp = K_lin(Ii,neb_hold);
-	    MatSetValue(K,2*mapping[Ii],2*mapping[Ii],*(tmp+0),ADD_VALUES);
-	    MatSetValue(K,2*mapping[Ii],2*mapping[Ii]+1,*(tmp+1),ADD_VALUES);
-	    MatSetValue(K,2*mapping[Ii],2*mapping[neb_hold-1],*(tmp+2),ADD_VALUES);
-	    MatSetValue(K,2*mapping[Ii],2*mapping[neb_hold-1]+1,*(tmp+3),ADD_VALUES);
-	    
-	    MatSetValue(K,2*mapping[Ii]+1,2*mapping[Ii],*(tmp+4),ADD_VALUES);
-	    MatSetValue(K,2*mapping[Ii]+1,2*mapping[Ii]+1,*(tmp+5),ADD_VALUES);
-	    MatSetValue(K,2*mapping[Ii]+1,2*mapping[neb_hold-1],*(tmp+6),ADD_VALUES);
-	    MatSetValue(K,2*mapping[Ii]+1,2*mapping[neb_hold-1]+1,*(tmp+7),ADD_VALUES);
+	 
+	 int neb_hold = colm[n-1];
+	 double * tmp = K_lin(Ii,neb_hold);
+	 
+	 MatSetValue(K,2*mapping[Ii],2*mapping[Ii],*(tmp+0),ADD_VALUES);
+	 MatSetValue(K,2*mapping[Ii],2*mapping[Ii]+1,*(tmp+1),ADD_VALUES);
+	 MatSetValue(K,2*mapping[Ii],2*mapping[neb_hold-1],*(tmp+2),ADD_VALUES);
+	 MatSetValue(K,2*mapping[Ii],2*mapping[neb_hold-1]+1,*(tmp+3),ADD_VALUES);
 
-	    
-	    MatSetValue(K,2*mapping[neb_hold-1],2*mapping[Ii],*(tmp+8),ADD_VALUES);
-	    MatSetValue(K,2*mapping[neb_hold-1],2*mapping[Ii]+1,*(tmp+9),ADD_VALUES);
-	    MatSetValue(K,2*mapping[neb_hold-1],2*mapping[neb_hold-1],*(tmp+10),ADD_VALUES);
-	    MatSetValue(K,2*mapping[neb_hold-1],2*mapping[neb_hold-1]+1,*(tmp+11),ADD_VALUES);
-	    
-	    MatSetValue(K,2*mapping[neb_hold-1]+1,2*mapping[Ii],*(tmp+12),ADD_VALUES);
-	    MatSetValue(K,2*mapping[neb_hold-1]+1,2*mapping[Ii]+1,*(tmp+13),ADD_VALUES);
-	    MatSetValue(K,2*mapping[neb_hold-1]+1,2*mapping[neb_hold-1],*(tmp+14),ADD_VALUES);
-	    MatSetValue(K,2*mapping[neb_hold-1]+1,2*mapping[neb_hold-1]+1,*(tmp+15),ADD_VALUES);	    
+	 MatSetValue(K,2*mapping[Ii]+1,2*mapping[Ii],*(tmp+4),ADD_VALUES);
+	 MatSetValue(K,2*mapping[Ii]+1,2*mapping[Ii]+1,*(tmp+5),ADD_VALUES);
+	 MatSetValue(K,2*mapping[Ii]+1,2*mapping[neb_hold-1],*(tmp+6),ADD_VALUES);
+	 MatSetValue(K,2*mapping[Ii]+1,2*mapping[neb_hold-1]+1,*(tmp+7),ADD_VALUES);
+
+	 MatSetValue(K,2*mapping[neb_hold-1],2*mapping[Ii],*(tmp+8),ADD_VALUES);
+	 MatSetValue(K,2*mapping[neb_hold-1],2*mapping[Ii]+1,*(tmp+9),ADD_VALUES);
+	 MatSetValue(K,2*mapping[neb_hold-1],2*mapping[neb_hold-1],*(tmp+10),ADD_VALUES);
+	 MatSetValue(K,2*mapping[neb_hold-1],2*mapping[neb_hold-1]+1,*(tmp+11),ADD_VALUES);
+
+	 MatSetValue(K,2*mapping[neb_hold-1]+1,2*mapping[Ii],*(tmp+12),ADD_VALUES);
+	 MatSetValue(K,2*mapping[neb_hold-1]+1,2*mapping[Ii]+1,*(tmp+13),ADD_VALUES);
+	 MatSetValue(K,2*mapping[neb_hold-1]+1,2*mapping[neb_hold-1],*(tmp+14),ADD_VALUES);
+	 MatSetValue(K,2*mapping[neb_hold-1]+1,2*mapping[neb_hold-1]+1,*(tmp+15),ADD_VALUES);	    
        }
+    }
+    else{
+       for(n=findrm[Ii];n<findrm[Ii+1];++n){
+	 
+	 int neb_hold = colm[n-1];
+	 double * tmp = K_lin(Ii,neb_hold);
+	 
+	 MatSetValue(K,3*mapping[Ii],3*mapping[Ii],*(tmp+0),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii],3*mapping[Ii]+1,*(tmp+1),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii],3*mapping[Ii]+2,*(tmp+2),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii],3*mapping[neb_hold-1],*(tmp+3),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii],3*mapping[neb_hold-1]+1,*(tmp+4),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii],3*mapping[neb_hold-1]+2,*(tmp+5),ADD_VALUES);
+
+	 MatSetValue(K,3*mapping[Ii]+1,3*mapping[Ii],*(tmp+6),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+1,3*mapping[Ii]+1,*(tmp+7),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+1,3*mapping[Ii]+2,*(tmp+8),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+1,3*mapping[neb_hold-1],*(tmp+9),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+1,3*mapping[neb_hold-1]+1,*(tmp+10),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+1,3*mapping[neb_hold-1]+2,*(tmp+11),ADD_VALUES);
+
+	 MatSetValue(K,3*mapping[Ii]+2,3*mapping[Ii],*(tmp+12),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+2,3*mapping[Ii]+1,*(tmp+13),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+2,3*mapping[Ii]+2,*(tmp+14),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+2,3*mapping[neb_hold-1],*(tmp+15),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+2,3*mapping[neb_hold-1]+1,*(tmp+16),ADD_VALUES);
+	 MatSetValue(K,3*mapping[Ii]+2,3*mapping[neb_hold-1]+2,*(tmp+17),ADD_VALUES);
+
+	 MatSetValue(K,3*mapping[neb_hold-1],3*mapping[Ii],*(tmp+18),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1],3*mapping[Ii]+1,*(tmp+19),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1],3*mapping[Ii]+2,*(tmp+20),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1],3*mapping[neb_hold-1],*(tmp+21),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1],3*mapping[neb_hold-1]+1,*(tmp+22),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1],3*mapping[neb_hold-1]+2,*(tmp+23),ADD_VALUES);
+
+	 MatSetValue(K,3*mapping[neb_hold-1]+1,3*mapping[Ii],*(tmp+24),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+1,3*mapping[Ii]+1,*(tmp+25),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+1,3*mapping[Ii]+2,*(tmp+26),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+1,3*mapping[neb_hold-1],*(tmp+27),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+1,3*mapping[neb_hold-1]+1,*(tmp+28),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+1,3*mapping[neb_hold-1]+2,*(tmp+29),ADD_VALUES);
+
+	 MatSetValue(K,3*mapping[neb_hold-1]+2,3*mapping[Ii],*(tmp+30),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+2,3*mapping[Ii]+1,*(tmp+31),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+2,3*mapping[Ii]+2,*(tmp+32),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+2,3*mapping[neb_hold-1],*(tmp+33),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+2,3*mapping[neb_hold-1]+1,*(tmp+34),ADD_VALUES);
+	 MatSetValue(K,3*mapping[neb_hold-1]+2,3*mapping[neb_hold-1]+2,*(tmp+35),ADD_VALUES);
+       }
+    }
   }
     
   MatAssemblyBegin(K,MAT_FINAL_ASSEMBLY);
@@ -97,7 +197,7 @@ void lin_smoother(int dimension, int num_nodes, int num_elements, int num_surf_e
 
   VecCreate(PETSC_COMM_WORLD,&F);
   PetscObjectSetName((PetscObject) F, "Load Vector");
-  VecSetSizes(F,2*num_owned_nodes,PETSC_DECIDE);
+  VecSetSizes(F,dimension*num_owned_nodes,PETSC_DECIDE);
   VecSetFromOptions(F);
 
   //Removing duplicate points in surf_connectivity
@@ -115,15 +215,23 @@ void lin_smoother(int dimension, int num_nodes, int num_elements, int num_surf_e
     }
   }
 
-  
-  for(i=0;i<len_new;i++){
-    global_surf_connectivity[2*i]=2*mapping[local_surf_connectivity[i]];
-    global_surf_connectivity[2*i+1]=2*mapping[local_surf_connectivity[i]]+1;
+  if(dimension==2){
+    for(i=0;i<len_new;i++){
+      global_surf_connectivity[dimension*i]=dimension*mapping[local_surf_connectivity[i]];
+      global_surf_connectivity[dimension*i+1]=dimension*mapping[local_surf_connectivity[i]]+1;
+    }
   }
-  
+  else{
+    for(i=0;i<len_new;i++){
+      global_surf_connectivity[dimension*i]=dimension*mapping[local_surf_connectivity[i]];
+      global_surf_connectivity[dimension*i+1]=dimension*mapping[local_surf_connectivity[i]]+1;
+      global_surf_connectivity[dimension*i+2]=dimension*mapping[local_surf_connectivity[i]]+2;
+    }
+  }
 
-  MatZeroRows(K,2*len_new,global_surf_connectivity,1.0,NULL,NULL);
+  MatZeroRows(K,dimension*len_new,global_surf_connectivity,1.0,NULL,NULL);
 
+ if(dimension==2){
  for(n=0;n<len_new;n++){
    x_disp[n]=phys_mesh[dimension*(local_surf_connectivity[n])] - comp_mesh[dimension*(local_surf_connectivity[n])];
    y_disp[n]=phys_mesh[dimension*(local_surf_connectivity[n])+1] - comp_mesh[dimension*(local_surf_connectivity[n])+1];
@@ -131,11 +239,25 @@ void lin_smoother(int dimension, int num_nodes, int num_elements, int num_surf_e
  
  for(Ii=0;Ii<len_new;Ii++){
    if (local_surf_connectivity[Ii]<num_owned_nodes){
-     VecSetValue(F,global_surf_connectivity[2*Ii],x_disp[Ii],INSERT_VALUES);
-     VecSetValue(F,global_surf_connectivity[2*Ii+1],y_disp[Ii],INSERT_VALUES);
+     VecSetValue(F,global_surf_connectivity[dimension*Ii],x_disp[Ii],INSERT_VALUES);
+     VecSetValue(F,global_surf_connectivity[dimension*Ii+1],y_disp[Ii],INSERT_VALUES);
    }
  }
-   
+  }
+  else{
+    for(n=0;n<len_new;n++){
+      x_disp[n]=phys_mesh[dimension*(local_surf_connectivity[n])] - comp_mesh[dimension*(local_surf_connectivity[n])];
+      y_disp[n]=phys_mesh[dimension*(local_surf_connectivity[n])+1] - comp_mesh[dimension*(local_surf_connectivity[n])+1];
+      z_disp[n]=phys_mesh[dimension*(local_surf_connectivity[n])+2] - comp_mesh[dimension*(local_surf_connectivity[n])+2];
+    }
+    for(Ii=0;Ii<len_new;Ii++){
+      if (local_surf_connectivity[Ii]<num_owned_nodes){
+	VecSetValue(F,global_surf_connectivity[dimension*Ii],x_disp[Ii],INSERT_VALUES);
+	VecSetValue(F,global_surf_connectivity[dimension*Ii+1],y_disp[Ii],INSERT_VALUES);
+	VecSetValue(F,global_surf_connectivity[dimension*Ii+2],z_disp[Ii],INSERT_VALUES);
+      }
+    }
+  }
   VecAssemblyBegin(F);
   VecAssemblyEnd(F);
 
@@ -156,7 +278,7 @@ void lin_smoother(int dimension, int num_nodes, int num_elements, int num_surf_e
 
   VecCreate(PETSC_COMM_WORLD,&U_h);
   PetscObjectSetName((PetscObject) U_h, "U_h");
-  VecSetSizes(U_h,2*num_owned_nodes,PETSC_DECIDE);
+  VecSetSizes(U_h,dimension*num_owned_nodes,PETSC_DECIDE);
   VecSetFromOptions(U_h);
 
   VecAssemblyBegin(U_h);
@@ -169,18 +291,36 @@ void lin_smoother(int dimension, int num_nodes, int num_elements, int num_surf_e
   KSPGetConvergedReason(ksp,&reason);
   PetscPrintf(PETSC_COMM_WORLD,"KSPConvergedReason: %D\n", reason);
 
-  for(n=0;n<num_owned_nodes;n++){
-     num_nodes_col_x[n]=2*mapping[n];
-     num_nodes_col_y[n]=2*mapping[n]+1;
-   }
-  
-  VecGetValues(U_h,num_owned_nodes,num_nodes_col_x,smoothed_x);
-  VecGetValues(U_h,num_owned_nodes,num_nodes_col_y,smoothed_y);
+  if(dimension==2){
+    for(n=0;n<num_owned_nodes;n++){
+      num_nodes_col_x[n]=dimension*mapping[n];
+      num_nodes_col_y[n]=dimension*mapping[n]+1;
+    }
+    VecGetValues(U_h,num_owned_nodes,num_nodes_col_x,smoothed_x);
+    VecGetValues(U_h,num_owned_nodes,num_nodes_col_y,smoothed_y);
 
-  for(n=0;n<num_owned_nodes; n++){
-    smooth_mesh[dimension*n+0] = smoothed_x[n]+comp_mesh[dimension*n+0];
-    smooth_mesh[dimension*n+1] = smoothed_y[n]+comp_mesh[dimension*n+1];
-   }
+    for(n=0;n<num_owned_nodes; n++){
+      smooth_mesh[dimension*n+0] = smoothed_x[n]+comp_mesh[dimension*n+0];
+      smooth_mesh[dimension*n+1] = smoothed_y[n]+comp_mesh[dimension*n+1];
+    }
+  }
+  else{
+    for(n=0;n<num_owned_nodes;n++){
+      num_nodes_col_x[n]=dimension*mapping[n];
+      num_nodes_col_y[n]=dimension*mapping[n]+1;
+      num_nodes_col_z[n]=dimension*mapping[n]+2;
+    }
+    VecGetValues(U_h,num_owned_nodes,num_nodes_col_x,smoothed_x);
+    VecGetValues(U_h,num_owned_nodes,num_nodes_col_y,smoothed_y);
+    VecGetValues(U_h,num_owned_nodes,num_nodes_col_z,smoothed_z);
+
+    for(n=0;n<num_owned_nodes;n++){
+      smooth_mesh[dimension*n+0] = smoothed_x[n]+comp_mesh[dimension*n+0];
+      smooth_mesh[dimension*n+1] = smoothed_y[n]+comp_mesh[dimension*n+1];
+      smooth_mesh[dimension*n+2] = smoothed_z[n]+comp_mesh[dimension*n+2];
+    }
+
+  }
 
   
   VecDestroy(&F);VecDestroy(&U_h);MatDestroy(&K);KSPDestroy(&ksp);
