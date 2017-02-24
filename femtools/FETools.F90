6a951b8d7f3fa42ecd1e3fb5258ee0d1af6b5319
James Percival <j.percival@imperial.ac.uk>
2017-02-24 14:11:44 +0000

    Adding in the CDG partial stress code from the templated DG assembly branch.
    This appears to be no_normal_flow condition safe now.

--- femtools/FETools.F90
+++ femtools/FETools.F90
@@ -693,6 +693,74 @@ contains
 
   end function dshape_tensor_dshape
 
+function dshape_outer_tensor_dshape(dshape1, tensor, dshape2, detwei) result (R)
+    !!<
+    !!< Evaluate: (Grad N1)'_k T_ki (Grad N2)_j For shape N and tensor T.
+    !!<          
+    real, dimension(:,:,:), intent(in) :: dshape1, dshape2
+    real, dimension(size(dshape1,3),size(dshape1,3),size(dshape1,2)), intent(in) :: tensor
+    real, dimension(size(dshape1,2)) :: detwei
+
+    real, dimension(size(dshape1,3),size(dshape2,3),size(dshape1,1),size(dshape2,1)) :: R
+
+    real, dimension(size(dshape1,1),size(dshape2,2)) :: temp
+
+    integer :: iloc,jloc, gi, idim
+    integer :: loc1, loc2, ngi, dim
+    
+    loc1=size(dshape1,1)
+    loc2=size(dshape2,1)
+    ngi=size(dshape1,2)
+    dim=size(dshape1,3)
+    
+    assert(loc1==loc2)
+    
+    R=0.0
+    
+
+    do gi=1,ngi
+       temp = matmul(dshape1(:,gi,:), tensor(:,:,gi)) * detwei(gi)
+       forall(iloc=1:loc1,jloc=1:loc2,idim=1:dim)
+             r(:,idim,iloc,jloc)=r(:,idim,iloc,jloc) &
+                  +dshape2(jloc,gi,:)*temp(iloc,idim)
+       end forall
+    end do
+
+  end function dshape_outer_tensor_dshape
+
+function dshape_tensor_outer_dshape(dshape1, tensor, dshape2, detwei) result (R)
+    !!<
+    !!< Evaluate: (Grad N1)'_j T_ik (Grad N2)_k For shape N and tensor T.
+    !!<          
+    real, dimension(:,:,:), intent(in) :: dshape1, dshape2
+    real, dimension(size(dshape1,3),size(dshape1,3),size(dshape1,2)), intent(in) :: tensor
+    real, dimension(size(dshape1,2)) :: detwei
+
+    real, dimension(size(dshape1,3),size(dshape1,3),size(dshape1,1),size(dshape2,1)) :: R
+    real, dimension(size(dshape1,3),size(dshape2,1)) :: temp
+
+    integer :: iloc,jloc, gi, idim
+    integer :: loc1, loc2, ngi, dim
+    
+    loc1=size(dshape1,1)
+    loc2=size(dshape2,1)
+    ngi=size(dshape1,2)
+    dim=size(dshape2,3)
+    
+    assert(loc1==loc2)
+    
+    R=0.0
+    
+    do gi=1,ngi
+       temp = matmul(tensor(:,:,gi),transpose(dshape2(:,gi,:)))*detwei(gi)
+       forall(iloc=1:loc1,jloc=1:loc2,idim=1:dim)
+          r(:,idim,iloc,jloc)=r(:,idim,iloc,jloc) &
+               +temp(:,jloc)*dshape1(iloc,gi,idim)
+       end forall
+    end do
+
+  end function dshape_tensor_outer_dshape
+
   function dshape_dot_vector_shape(dshape, vector, shape, detwei) result (R)
     !!< 
     !!< Evaluate (Grad N1 dot vector) (N2)
