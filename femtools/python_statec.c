11e4cab0786aad71a0b0272223fb38ebe532b5a0
James Percival <j.percival@imperial.ac.uk>
2017-06-14 11:42:08 +0100

    Add cmake package routine for the fluidity library.

--- femtools/python_statec.c
+++ femtools/python_statec.c
@@ -162,7 +162,8 @@ void python_add_statec_(char *name,int *len, void *ptr){
   PyObject* State = PyObject_CallObject(PyObject_GetAttrString(m, "State"), args);
   Py_DECREF(args);
 
-  PyObject_SetAttrString(State, "c_ptr", PyCapsule_New(ptr, NULL, NULL));
+  PyObject_SetAttrString(State, "c_ptr",
+			 PyCapsule_New(ptr, NULL, PyObject_GetAttrString(State,"name")));
 
   PyDict_SetItemString(PyObject_GetAttrString(m, "states"), n, State);
   PyObject_SetAttrString(m, "state", State);
