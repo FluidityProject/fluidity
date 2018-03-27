26ebd43f33db28f28c4e1077cf85692883e88f5a
James Percival <j.percival@imperial.ac.uk>
2018-03-27 19:17:20 +0100

    Updates to deal with skramer's comments.

--- tests/python_validation/python_validate.py
+++ tests/python_validation/python_validate.py
@@ -9,7 +9,7 @@ or
 python3 python_validate.py [-p] [-f] [file1 file2 file3]
 
 -f  (--fixup)  Automatically attempt to fix obvious python3 errors using reindent and 2to3.
--p  (--pep8)   Run flake8 to test adherence to pep8 coding stanaards.
+-p  (--pep8)   Run flake8 to test adherence to pep8 coding standards.
 
 Script to attempt to validate python files and code snippets in the Fluidity code base against python2 and python3 syntax.
 
@@ -20,9 +20,12 @@ import sys
 import os
 import ast
 import subprocess
+import pickle
+import io
+import tokenize
 import tempfile
 
-from lxml import etree
+from xml.etree.ElementTree import ElementTree
 
 import argparse
 
@@ -44,21 +47,33 @@ def print_as_function(filename, add_future_import=False):
     print(blah)"""
     lines = []
 
-    def sanitize(line, sep):
-        if len(sep)==0:
-            if 'print ' in line:
-                return line.replace('print ', 'print(')+')'
-            else:
-                return line
+    def isplit(line, indices):
+        return [line[i+1:j] for i, j in zip(indices, indices[1:]+[None])]
+
+    def split_comments(line):
+        tokens = tokenize.generate_tokens(io.StringIO(line).readline)
+        indices = [-1]
+        for _ in tokens:
+            if _[0] == tokenize.COMMENT:
+                return isplit(line.replace(_[1],''), indices), _[1]
+            if _[0] == tokenize.OP and _[1] == ';':
+                indices.append(_[2][1])
+                continue
+        return isplit(line, indices), ''
+
+    def sanitize(line):
+        if 'print ' in line:
+            return line.replace('print ', 'print(')+')'
         else:
-            return sep[0].join([sanitize(_, sep[1:]) for _ in line.split(sep[0])])
+            return line
 
     with open(filename, 'r') as temp:
         for line in temp.readlines():
             if len(line.strip())==0 or line.strip()[0]=='#':
                 pass
             if 'print ' in line:
-                line = sanitize(line.rstrip('\n'), [';', '#'])+'\n'
+                line, comment = split_comments(line)
+                line = ';'.join([sanitize(_.strip('\n')) for _ in line])+comment+'\n'
             lines.append(line)
     with open(filename, 'w') as temp:
         if add_future_import:
@@ -94,95 +109,110 @@ def process_snippet(name, snippet):
 
     On failure, also print the string with line numbers.
     """ 
-    fail=False
+    fail = False
     snippet = snippet.strip()
     try:
         ast.parse(snippet)
     except SyntaxError as err:
-        print(str(err))
-        fail = True
+        fail = "%s\n%s\n"%(name, err)
         lines = snippet.split('\n')
         nchar = len(str(len(lines)))
         for k, line in enumerate(lines):
-            print(("%{0}d:".format(nchar))%(k+1), repr(line)[1:-1])
+            fail += ("%{0}d: %s\n".format(nchar))%((k+1), repr(line)[1:-1])
+        print(fail)
     return fail
 
 def validate_file(filename, fix):
-    """Run the validotor on a whole file."""
+    """Run the validator on a whole file."""
     with open(filename, 'r') as testfile:
         failed = process_snippet(filename, testfile.read())
         if failed:
             if fix:
                 print_as_function(filename)
-            return True
-    return False
-
+    return failed
 
 def validate_xml_snippets(filename, test_pep8, fix):
-    """Run the validotor on elements with attribute 'language=python' in an xml file."""
+    """Run the validator on elements with attribute 'language=python' in an xml file."""
 
     snippets=[]
 
     with open(filename, 'r') as xmlfile:
-        tree = etree.ElementTree(file=filename)
+        tree = ElementTree(file=filename)
 
+        #build iterator by hand, since not using lxml
+        exp_tree = {}
 
-        for ele in tree.iter():
+        def expand_tree(ele, path):
+            epath = path+ele.tag+'/'
+            exp_tree[epath] = ele
+            for _ in ele.getchildren():
+                expand_tree(_, epath)
+
+        expand_tree(tree.getroot(), '/')
+
+        for name, ele in exp_tree.items():
             if ele.attrib.get('language',None)=='python':
-                snippets.append((get_name(ele), ele.text, ele))
+                snippets.append((name, ele.text, ele))
 
-        failed = False
+        failed = ''
         modified = False
         for name, snippet, ele in snippets:
+            name = name+ele.attrib.get('name','')
             if(test_pep8):
                 flake8_snippet(name, snippet)
-            if process_snippet(name, snippet):
-                failed = True
+            test = process_snippet(name, snippet)
+            if test:
+                failed += test
                 if fix:
                     fixed = fix_snippet(name, snippet)
                     if fixed:
                         modified = True
                         ele.text = fixed
                 else:
-                    return True
+                    return failed
         if failed and modified:
-            tree.write(filename, xml_declaration=True, encoding='utf-8',
-                   with_tail=False, pretty_print=True)
+            tree.write(filename, xml_declaration=True, encoding='utf-8')
 
-    return failed
+    return failed or False
 
 def main():
     """Main python validation routine."""
 
-    parser = argparse.ArgumentParser(usage="<python_executable> python_validate.py [-p] [-f] [file1 file2 file3]")
+    parser = argparse.ArgumentParser(usage="<python_executable> python_validate.py [-f] [-p] [-w output] [file1 file2 file3]")
     parser.add_argument("-f", "--fixup",  action="store_true", dest="fix", default=False,
                   help="Automatically attempt to fix obvious python3 errors using reindent and 2to3")
+    parser.add_argument("-w", "--write-failures",  action="store", dest="write", default=None,
+                  help="Write failures to disk.")
     parser.add_argument("-p", "--pep8", action="store_true", dest="test_pep8", default=False,
                   help="Run flake8 to test adherence to pep8 coding stanaards.")
     parser.add_argument('files', nargs='*',
                     help='files to process')
     options = parser.parse_args()
-    failures = []
+    failures = {}
 
-    failed = False
-    print(options.files)
+#    print(options.files)
     for filename in options.files:
+        print(filename)
         if filename.rsplit('.',1)[-1]=='py':
-            if validate_file(filename, options.fix):
-                failed =True
-                failures.append(filename)
+            test = validate_file(filename, options.fix)
+            if test:
+                failures[filename] = test
         else:
-            if validate_xml_snippets(filename, 
+            test =  validate_xml_snippets(filename, 
                                      test_pep8=options.test_pep8,
-                                     fix=options.fix):
-                failed =True
-                failures.append(filename)
+                                     fix=options.fix)
+            if test:
+                failures[filename] = test
 
-    if failed:
+    if options.write:
+        with open(options.write.strip(), 'wb') as output:
+            pickle.dump(failures, output, protocol=2)
+
+    if failures:
         print("Failed:")
         for filename in failures:
             print(filename)
-        exit(1)
+        exit(100)
     else:
         print("Passed!")
         exit(0)
@@ -191,7 +221,7 @@ def check_versions():
     versions = []
     for ver in ("python2", "python3"):
         try:
-            subprocess.call([ver+' --version'], shell=True)
+            subprocess.check_output([ver+' --version'], shell=True)
             versions.append(ver)
         except subprocess.CalledProcessError:
             pass
