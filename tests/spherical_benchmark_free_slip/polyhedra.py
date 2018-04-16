637faaf0bb4a6014a0f38fdd9326ae14a7927736
Stephan Kramer <s.kramer@imperial.ac.uk>
2018-04-16 11:36:05 +1000

    Get rid of dodgy version of polyhedra.py
    The older version had hard-coded vertex locations rounded off to 3
    digits only.

--- tests/spherical_benchmark_free_slip/polyhedra.py
+++ tests/spherical_benchmark_free_slip/polyhedra.py
@@ -1,50 +1,55 @@
 ﻿from numpy import array
 from numpy.linalg import norm
+from math import sqrt
 
 # 8-3-2017 s.kramer@imperial.ac.uk:
-# the icosahedron() code is copied from http://prideout.net/blog/?p=44 - "I consider this code to be on the public domain" (sic.)
-# the subdivide() routine is based on it, but completely rewritten to actually produce a valid mesh
+# the icosahedron() code is partly based on http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
+# the subdivide() routine is based on http://prideout.net/blog/?p=44 , but completely rewritten to actually produce a valid mesh
 # with unique vertices and not depend on euclid
 
 def icosahedron():
-
     """Construct a 20-sided polyhedron"""
-    faces = [ \
-        (0,1,2),
-        (0,2,3),
-        (0,3,4),
-        (0,4,5),
-        (0,5,1),
-        (11,6,7),
-        (11,7,8),
-        (11,8,9),
-        (11,9,10),
-        (11,10,6),
-        (1,2,6),
-        (2,3,7),
-        (3,4,8),
-        (4,5,9),
-        (5,1,10),
-        (6,7,2),
-        (7,8,3),
-        (8,9,4),
-        (9,10,5),
-        (10,6,1) ]
-    verts = [ \
-        ( 0.000,  0.000,  1.000 ),
-        ( 0.894,  0.000,  0.447 ),
-        ( 0.276,  0.851,  0.447 ),
-        (-0.724,  0.526,  0.447 ),
-        (-0.724, -0.526,  0.447 ),
-        ( 0.276, -0.851,  0.447 ),
-        ( 0.724,  0.526, -0.447 ),
-        (-0.276,  0.851, -0.447 ),
-        (-0.894,  0.000, -0.447 ),
-        (-0.276, -0.851, -0.447 ),
-        ( 0.724, -0.526, -0.447 ),
-        ( 0.000,  0.000, -1.000 ) ]
+    t = (1.0 + sqrt(5.0)) / 2.0;
+    verts = [(-1,  t,  0),
+             ( 1,  t,  0),
+             (-1, -t,  0),
+             ( 1, -t,  0),
 
+             ( 0, -1,  t),
+             ( 0,  1,  t),
+             ( 0, -1, -t),
+             ( 0,  1, -t),
+
+             ( t,  0, -1),
+             ( t,  0,  1),
+             (-t,  0, -1),
+             (-t,  0,  1)]
+    # now normalize them:
+    s = sqrt(1 + t**2)
+    verts = [tuple(x/s for x in vertex) for vertex in verts]
+
+    faces = [(0, 11, 5),
+             (0, 5, 1),
+             (0, 1, 7),
+             (0, 7, 10),
+             (0, 10, 11),
+             (1, 5, 9),
+             (5, 11, 4),
+             (11, 10, 2),
+             (10, 7, 6),
+             (7, 1, 8),
+             (3, 9, 4),
+             (3, 4, 2),
+             (3, 2, 6),
+             (3, 6, 8),
+             (3, 8, 9),
+             (4, 9, 5),
+             (2, 4, 11),
+             (6, 2, 10),
+             (8, 6, 7),
+             (9, 8, 1)]
     return verts, faces
+
 
 def create_new_node(face, i, j, verts, new_nodes):
     a, b = face[i], face[j]
