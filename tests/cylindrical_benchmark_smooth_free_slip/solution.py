1f9b0a1b0730ac6c3d8d50ddb4110511db330e19
Stephan Kramer <s.kramer@imperial.ac.uk>
2018-04-18 12:24:07 +1000

    Use sage-generated solution.py in cyl. benchmarks
    Also rescales density perturbation with R2^-k

--- tests/cylindrical_benchmark_smooth_free_slip/solution.py
+++ tests/cylindrical_benchmark_smooth_free_slip/solution.py
@@ -1,60 +1,61 @@
 import numpy
 from math import sqrt, atan2, cos, sin
 
-eta = 1.
+nu = 1.
 g = 1.
-Ra = g/eta
 R1, R2 = 1.22, 2.22
+alpha = R1/R2
 n = 2
 k = 2
 
-if n>1:
-    A5 = R1**2*R2**(4*n + 2) + R1**(4*n + 2)*R2**2 - (R1**(2*n)*R2**4 + R1**(2*n + 4))*R2**(2*n)
-    # velocity: coefficients for n, -n, n+2, -n+2 and k+3 powers off r
-    C = -Ra * (R1**(k + n + 1) - R2**(k + n + 1))*R1**2*R2**2/((R1**n*R2 + R1*R2**n)*(R1*R2**n - R1**n*R2)*(k + n + 1)*(k - n + 3))/4.
-    D = -Ra * (R1**n*R2**(k + 1) - R1**(k + 1)*R2**n)*R1**(n + 2)*R2**(n + 2)/((R1**(n + 1) + R2**(n + 1))*(R1**(n + 1) - R2**(n + 1))*(k + n + 3)*(k - n + 1))/4.
-    E = -Ra * (R1**(k + n + 3) - R2**(k + n + 3))*g/((R1**(n + 1) + R2**(n + 1))*(R1**(n + 1) - R2**(n + 1))*(k + n + 3)*(k - n + 1))/4.
-    F = -Ra * (R1**n*R2**(k + 3) - R1**(k + 3)*R2**n)*R1**n*R2**n*g/((R1**n*R2 + R1*R2**n)*(R1*R2**n - R1**n*R2)*(k + n + 1)*(k - n + 3))/4.
-    A0 = Ra * n / (((k+3)**2-n**2)*((k+1)**2-n**2))
-
-    # pressure: coefficients for n, -n, and k+1
-    G = -4*eta*E*(n+1)
-    H = -4*eta*F*(n-1)
-    K = -g*(k + 1)/((k+1)**2-n**2)
-else:
+if n<=1:
     raise NotImplemented()
+# velocity solution: coefficients for n, -n, n+2, -n+2 and k+3 powers off r
+A = g*R2**(-n + 3)/nu*-0.25*(alpha**2 - alpha**(k + n + 3))/((alpha + alpha**n)*(alpha**n - alpha)*(k + n + 1)*(k - n + 3))
+B = g*R2**(n + 3)/nu*0.25*(alpha**(k + n + 3) - alpha**(2*n + 2))/((alpha**(n + 1) + 1)*(alpha**(n + 1) - 1)*(k + n + 3)*(k - n + 1))
+C = g*R2**(-n + 1)/nu*-0.25*(alpha**(k + n + 3) - 1)/((alpha**(n + 1) + 1)*(alpha**(n + 1) - 1)*(k + n + 3)*(k - n + 1))
+D = g*R2**(n + 1)/nu*-0.25*(alpha**(k + n + 3) - alpha**(2*n))/((alpha + alpha**n)*(alpha**n - alpha)*(k + n + 1)*(k - n + 3))
+E = R2**(-k)*g*n/(((k + 3)**2 - n**2)*((k + 1)**2 - n**2)*nu)
 
 
-def u_r(r, theta):
-    dpsi_dtheta = n*cos(n*theta)*(C*r**n+D*r**(-n)+E*r**(n+2)+F*r**(-n+2)+A0*r**(k+3))
-    return -dpsi_dtheta/r
+# pressure: coefficients for n, -n, and k+1
+G = -4*nu*C*(n+1)
+H = -4*nu*D*(n-1)
+F = -g*(k + 1)*R2**(-k)/((k+1)**2-n**2)
+
+
+def u_r(r, phi):
+    dpsi_dphi = n*cos(n*phi)*(A*r**n+B*r**(-n)+C*r**(n+2)+D*r**(-n+2)+E*r**(k+3))
+    return -dpsi_dphi/r
 
 numpy.testing.assert_almost_equal(u_r(R1, 0.), 0.)
 numpy.testing.assert_almost_equal(u_r(R2, 0.), 0.)
 
-def u_theta(r, theta):
-    dpsi_dr = sin(n*theta)*(C*n*r**(n-1) + D*-n*r**(-n-1) + E*(n+2)*r**(n+1) + F*(-n+2)*r**(-n+1)+A0*(k+3)*r**(k+2))
+def u_phi(r, phi):
+    dpsi_dr = sin(n*phi)*(A*n*r**(n-1) + B*-n*r**(-n-1) + C*(n+2)*r**(n+1) + D*(-n+2)*r**(-n+1)+E*(k+3)*r**(k+2))
     return dpsi_dr
 
-def p(r, theta):
-    return (G*r**n + H*r**(-n) + K*r**(k+1))*cos(n*theta)
 
-def delta_rho(r, theta):
-    return r**k * cos(n*theta)
+
+def p(r, phi):
+    return (G*r**n + H*r**(-n) + F*r**(k+1))*cos(n*phi)
+
+def delta_rho(r, phi):
+    return r**k * cos(n*phi) / R2**k
 
 def get_cartesian_solution(X):
     r = sqrt(X[0]**2+X[1]**2)
-    theta = atan2(X[1], X[0])
-    ur = u_r(r,theta)
-    ut = u_theta(r,theta)
+    phi = atan2(X[1], X[0])
+    ur = u_r(r,phi)
+    ut = u_phi(r,phi)
     return [ur*X[0]/r - ut*X[1]/r, ur*X[1]/r + ut*X[0]/r]
 
 def get_cartesian_pressure_solution(X):
     r = sqrt(X[0]**2+X[1]**2)
-    theta = atan2(X[1], X[0])
-    return p(r, theta)
+    phi = atan2(X[1], X[0])
+    return p(r, phi)
 
 def delta_rho_cartesian(X):
     r = sqrt(X[0]**2+X[1]**2)
-    theta = atan2(X[1], X[0])
-    return delta_rho(r, theta)
+    phi = atan2(X[1], X[0])
+    return delta_rho(r, phi)
