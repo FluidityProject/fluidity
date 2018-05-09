c0cb1d097dc1068c814c4b2fd144ba7bcc04813b
Cmath <Christopher.Mathews@anu.edu.au>
2018-05-09 16:38:00 +1000

    buildbot testing

--- assemble/Zoltan_integration.F90
+++ assemble/Zoltan_integration.F90
@@ -1889,12 +1889,8 @@ module zoltan_integration
 
     ! Tell Zoltan which callback functions to use for the migration
     ierr = Zoltan_Set_Fn(zz, ZOLTAN_OBJ_SIZE_MULTI_FN_TYPE, zoltan_cb_pack_field_sizes); assert(ierr == ZOLTAN_OK)
-    ierr = Zoltan_Set_Fn(zz, ZOLTAN_PACK_OBJ_MULTI_FN_TYPE, zoltan_cb_pack_fields);
-    if (assert(ierr == ZOLTAN_OK)) then
-       ewrite(1,*) "GAN460"
-    else
-       ewrite(1,*) "Cubicle"
-    end if
+    ierr = Zoltan_Set_Fn(zz, ZOLTAN_PACK_OBJ_MULTI_FN_TYPE, zoltan_cb_pack_fields); assert(ierr == ZOLTAN_OK)
+    ewrite(1,*) "GAN460"
     ierr = Zoltan_Set_Fn(zz, ZOLTAN_UNPACK_OBJ_MULTI_FN_TYPE, zoltan_cb_unpack_fields); assert(ierr == ZOLTAN_OK)
     
     ewrite(1,*) 'exiting initialise_transfer'
