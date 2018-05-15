d416abcf00bc8b51f43fbbe404f2c59ff5b20264
Cmath <Christopher.Mathews@anu.edu.au>
2018-05-15 12:10:41 +1000

    .

--- femtools/Detector_Tools.F90
+++ femtools/Detector_Tools.F90
@@ -361,11 +361,18 @@ contains
        assert(size(buff)>=ndims+3+attributes_buffer(1)+attributes_buffer(2)+attributes_buffer(3))
        ewrite(1,*) "Flag 2"
        ! Basic fields: ndims+3
+       ewrite(1,*) "buff_size:", size(buff), "position:", size(detector%position)
        buff(1:ndims) = detector%position
        buff(ndims+1) = detector%element
        buff(ndims+2) = detector%id_number
        buff(ndims+3) = detector%type
+       ewrite(1,*) "data:", size(detector%position)+size(detector%element)+size(detector%id_number)+size(detector%type)
+       ewrite(1,*) "attributes_buffer_1:", size(attributes_buffer(1))
+       ewrite(1,*) "attributes_buffer_2:", size(attributes_buffer(2))
+       ewrite(1,*) "attributes_buffer_3:", size(attributes_buffer(3))
+       ewrite(1,*) "attributes:", size(detector%attributes)
        if (attributes_buffer(1).ne.0) then
+          ewrite(1,*) "Flag 2.5"
           buff(ndims+4:ndims+3+attributes_buffer(1)) = detector%attributes
        end if
        ewrite(1,*) "Flag 3"
