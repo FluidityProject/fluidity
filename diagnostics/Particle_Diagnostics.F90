e5013629891ca109c037c76fe63f254f1227c79a
Stephan Kramer <s.kramer@imperial.ac.uk>
2018-07-16 11:18:22 +0100

    Add halo_accumulate routine used in active particles.
    Halo_accumulate adds contributions locally written into the entries of
    an array corresponding to recv nodes, adds them into the contribution in
    the corresponding send node entry on the owning process.

--- diagnostics/Particle_Diagnostics.F90
+++ diagnostics/Particle_Diagnostics.F90
@@ -34,8 +34,8 @@ module particle_diagnostics
   use particles, only : get_particles, get_particle_arrays
   use spud
   use state_module
+  use halos
   use fields
-  use fields_base
   use detector_data_types
   use field_options
   
@@ -81,13 +81,14 @@ module particle_diagnostics
     type(state_type), dimension(:), target, intent(inout) :: states
     integer, intent(in) :: state_index
     type(scalar_field), intent(inout) :: s_field
+    type(halo_type), pointer :: halo
 
     character(len=OPTION_PATH_LEN) :: lgroup, lattribute
     type(detector_linked_list), allocatable, dimension(:) :: p_array
     type(detector_type), pointer :: particle
     integer :: attribute_number, i, p_allocated
     real, allocatable, dimension(:) :: node_values
-    integer, allocatable, dimension(:) :: node_part_count
+    real, allocatable, dimension(:) :: node_part_count ! real instead of integer, so we can use halo_accumulate
     integer :: element, node_number, dim
     real, allocatable, dimension(:) :: local_crds
     integer, dimension(:), pointer :: nodes
@@ -106,6 +107,12 @@ module particle_diagnostics
 
     !Call subroutine to check if particles are allocated
     call get_particles(p_array, p_allocated)
+
+    ! contribution of local particles to non-owned nodes are summed
+    ! into the owner in the halo_accumulate calls below
+    ! we might be safe to assume we only need to add into halo 1 nodes (as these
+    ! are the only ones that make up owned elements), but let's include halo 2 to be sure
+    halo => s_field%mesh%halos(2)
 
     if (p_allocated.eq.0) then
        !Particles not yet setup, Initial field value will be 0
@@ -143,7 +150,7 @@ module particle_diagnostics
              !Store particle attribute value on closest node
              node_values(node_number) = node_values(node_number) + att_value
              !Increase particle count for this node by 1
-             node_part_count(node_number) = node_part_count(node_number) + 1
+             node_part_count(node_number) = node_part_count(node_number) + 1.0
              particle => particle%next
              
           end do
@@ -151,6 +158,9 @@ module particle_diagnostics
              deallocate(local_crds)
           end if
        end do
+
+       call halo_accumulate(halo, node_part_count)
+       call halo_accumulate(halo, node_values)
 
        !Loop over all nodes
        do i = 1,node_count(s_field)
@@ -165,6 +175,10 @@ module particle_diagnostics
        end do
        deallocate(node_values)
        deallocate(node_part_count)
+
+       ! all values in owned nodes should now be correct
+       ! now we need to make sure the halo is updated accordingly
+       call halo_update(s_field)
     end if
 
   end subroutine calculate_ratio_from_particles
