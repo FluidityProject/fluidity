db3ccdcc1004066b37c430f403fc2e557cf376e1
Stephan Kramer <s.kramer@imperial.ac.uk>
2019-11-15 19:40:05 +0000

    Fix wrong Jacobian dimensions in transform_cvsurf_to_physical.
    The second dimension of J should be the surface dimension (i.e. dim-1),
    otherwise the matmul in line 2326 does not match. This is copying a
    smaller array into a larger, so should be safe - and I can't actually
    get my compiler to complain (even with debugging) - but this should
    hopefully fix #206.

--- femtools/Transform_elements.F90
+++ femtools/Transform_elements.F90
@@ -2285,7 +2285,7 @@ contains
     type(cv_faces_type), intent(in) :: cvfaces
 
     ! Jacobian matrix
-    real, dimension(size(X,1),x_shape%numbering%dimension) :: J
+    real, dimension(size(X,1), size(x_shape%dn, 3)) :: J
 
     ! Determinant of J
     real :: detJ
