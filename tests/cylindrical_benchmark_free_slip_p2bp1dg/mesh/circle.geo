d569649569c10f989f0eb3bdabb8cb32fd070649
Cian Wilson <cwilson@carnegiescience.edu>
2020-01-31 19:45:13 -0500

    Add cylindrical and spherical tests to the tests.
    Cylindrical tests work, spherical tests are untested.

---
+++ tests/cylindrical_benchmark_free_slip_p2bp1dg/mesh/circle.geo
@@ -0,0 +1,35 @@
+lti = 64*N; // number of cells per Pi/4 section
+Point(1) = {2.22, 0, 0, 1.0};
+Extrude {{0, 0, 1}, {0, 0, 0}, Pi/4} {
+  Point{1};
+}
+Extrude {{0, 0, 1}, {0, 0, 0}, Pi/4} {
+  Point{2};
+}
+Extrude {{0, 0, 1}, {0, 0, 0}, Pi/4} {
+  Point{4};
+}
+Extrude {{0, 0, 1}, {0, 0, 0}, Pi/4} {
+  Point{5};
+}
+Extrude {{0, 0, 1}, {0, 0, 0}, Pi/4} {
+  Point{6};
+}
+Extrude {{0, 0, 1}, {0, 0, 0}, Pi/4} {
+  Point{7};
+}
+Extrude {{0, 0, 1}, {0, 0, 0}, Pi/4} {
+  Point{8};
+}
+Extrude {{0, 0, 1}, {0, 0, 0}, Pi/4} {
+  Point{9};
+}
+Transfinite Line {1} = lti + 1;
+Transfinite Line {2} = lti + 1;
+Transfinite Line {3} = lti + 1;
+Transfinite Line {4} = lti + 1;
+Transfinite Line {5} = lti + 1;
+Transfinite Line {6} = lti + 1;
+Transfinite Line {7} = lti + 1;
+Transfinite Line {8} = lti + 1;
+Physical Line(9) = {8, 7, 6, 5, 4, 3, 2, 1};
---
+++ tests/cylindrical_benchmark_no_slip_p2bp1dg/solution.py
@@ -0,0 +1,92 @@
+from __future__ import division
+import numpy
+from math import sqrt, atan2, cos, sin, pi
+
+nu = 1.
+g = 1.
+# outer and inner radius R_+ and R_-
+Rp, Rm = 2.22, 1.22
+# radial height of anomaly: r'
+rp = Rm + 0.5
+n = 2
+
+if n <= 1:
+    raise NotImplementedError("Only implemented solutions for n>1")
+
+alpha_pm = numpy.array([Rp/rp, Rm/rp])
+alpha_mp = numpy.array([Rm/rp, Rp/rp])
+alpha, beta = alpha_pm
+pm = numpy.array([1, -1])
+mp = -pm
+
+# velocity solution: coefficients for n, -n, n+2, and -n+2 power of r
+# since the alpha's are length-2 arrays (each entry corresponding to one halve
+# of the domain) so will the coefficients and thus the solution
+A_pm = 0.125*(((alpha**2 - beta**2)*n + (n + 1)*pm + 1/alpha**(2*n) - 1/beta**(2*n))*(n - 1) + (alpha**2/beta**(2*n) - beta**2/alpha**(2*n))*n - (n**2*(beta/alpha)**(2*mp) - (beta/alpha)**(2*n*pm))*pm)*g*rp**(-n + 2)/((n**2*(alpha/beta - beta/alpha)**2 - ((beta/alpha)**n - 1/(beta/alpha)**n)**2)*(n - 1)*nu)
+B_pm = 0.125*(((alpha**2 - beta**2)*n + (n - 1)*pm - alpha**(2*n) + beta**(2*n))*(n + 1) - (alpha**2*beta**(2*n) - alpha**(2*n)*beta**2)*n - (n**2*(beta/alpha)**(2*mp) - (beta/alpha)**(2*mp*n))*pm)*g*rp**(n + 2)/((n**2*(alpha/beta - beta/alpha)**2 - ((beta/alpha)**n - 1/(beta/alpha)**n)**2)*(n + 1)*nu)
+C_pm = -0.125*((n**2*(beta/alpha)**(2*pm) - (beta/alpha)**(2*n*pm))*mp - (mp*(n - 1) - n*(1/alpha**2 - 1/beta**2) + 1/alpha**(2*n) - 1/beta**(2*n))*(n + 1) + n*(1/(alpha**(2*n)*beta**2) - 1/(alpha**2*beta**(2*n))))*g/((n**2*(alpha/beta - beta/alpha)**2 - ((beta/alpha)**n - 1/(beta/alpha)**n)**2)*(n + 1)*nu*rp**n)
+D_pm = -0.125*((n**2*(beta/alpha)**(2*pm) - (beta/alpha)**(2*mp*n))*mp - (mp*(n + 1) - n*(1/alpha**2 - 1/beta**2) - alpha**(2*n) + beta**(2*n))*(n - 1) - n*(alpha**(2*n)/beta**2 - beta**(2*n)/alpha**2))*g*rp**n/((n**2*(alpha/beta - beta/alpha)**2 - ((beta/alpha)**n - 1/(beta/alpha)**n)**2)*(n - 1)*nu)
+
+
+# pressure solution: coefficients for n and -n
+G_pm = -4*nu*C_pm*(n+1)
+H_pm = -4*nu*D_pm*(n-1)
+
+
+def u_r(r, phi):
+    dpsi_dphi = n*cos(n*phi)*(A_pm*r**n+B_pm*r**(-n)+C_pm*r**(n+2)+D_pm*r**(-n+2))
+    return -dpsi_dphi/r
+
+
+# some sanity checks:
+# no-normal flow
+numpy.testing.assert_almost_equal(u_r(Rm, 0.)[1], 0.)
+numpy.testing.assert_almost_equal(u_r(Rp, 0.)[0], 0.)
+# coninuity of u_r
+numpy.testing.assert_almost_equal(u_r(rp, 0.)[0], u_r(rp, 0.)[1])
+
+
+def u_phi(r, phi):
+    dpsi_dr = sin(n*phi)*(A_pm*n*r**(n-1) + B_pm*-n*r**(-n-1) + C_pm*(n+2)*r**(n+1) + D_pm*(-n+2)*r**(-n+1))
+    return dpsi_dr
+
+
+# coninuity of u_phi
+numpy.testing.assert_almost_equal(u_phi(rp, pi/(2*n))[0], u_phi(rp, pi/(2*n))[1])
+
+numpy.testing.assert_almost_equal(u_phi(Rp, 0.), 0.)
+numpy.testing.assert_almost_equal(u_phi(Rm, 0.), 0.)
+
+
+def p(r, phi):
+    return (G_pm*r**n + H_pm*r**(-n))*cos(n*phi)
+
+
+def normal_stress(r, phi):
+    dpsi_dphi = n*cos(n*phi)*(A_pm*r**n + B_pm*r**(-n) + C_pm*r**(n+2) + D_pm*r**(-n+2))
+    dpsi_drdphi = n*cos(n*phi)*(A_pm*n*r**(n-1) + B_pm*-n*r**(-n-1) + C_pm*(n+2)*r**(n+1) + D_pm*(-n+2)*r**(-n+1))
+    tau_rr = 2*nu*(dpsi_dphi/r**2 - dpsi_drdphi/r)
+    return tau_rr - p(r, phi)
+
+
+def normal_stress_cartesian(X, i):
+    # i==0: upper mantle, i==1: lower mantle
+    r = sqrt(X[0]**2+X[1]**2)
+    phi = atan2(X[1], X[0])
+    return normal_stress(r, phi)[i]
+
+
+def velocity_cartesian(X, i):
+    # i==0: upper mantle, i==1: lower mantle
+    r = sqrt(X[0]**2+X[1]**2)
+    phi = atan2(X[1], X[0])
+    ur = u_r(r, phi)[i]
+    ut = u_phi(r, phi)[i]
+    return [ur*X[0]/r - ut*X[1]/r, ur*X[1]/r + ut*X[0]/r]
+
+
+def pressure_cartesian(X, i):
+    # i==0: upper mantle, i==1: lower mantle
+    r = sqrt(X[0]**2+X[1]**2)
+    phi = atan2(X[1], X[0])
+    return p(r, phi)[i]
