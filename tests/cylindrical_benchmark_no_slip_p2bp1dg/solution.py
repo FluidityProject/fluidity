6aa1fbcb98ea52f778eb28378f25b13cd4118121
Stephan Kramer <s.kramer@imperial.ac.uk>
2020-06-01 17:47:56 +0100

    Use right version of bc type.

--- tests/cylindrical_benchmark_no_slip_p2bp1dg/solution.py
+++ tests/cylindrical_benchmark_no_slip_p2bp1dg/solution.py
@@ -2,5 +2,5 @@ import assess
 
 nu = 1.0
 n = 2
-solution_upper = assess.CylindricalStokesSolutionDeltaFreeSlip(n, +1, nu=nu)
-solution_lower = assess.CylindricalStokesSolutionDeltaFreeSlip(n, -1, nu=nu)
+solution_upper = assess.CylindricalStokesSolutionDeltaZeroSlip(n, +1, nu=nu)
+solution_lower = assess.CylindricalStokesSolutionDeltaZeroSlip(n, -1, nu=nu)
