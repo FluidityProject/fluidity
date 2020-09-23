4bba385d301613aeda57e3b3e63d4bf4c5d168f4
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2020-09-23 01:35:29 +0100

    Updated unittestharness to generate junit reports. Fixes: Jenkins unit test step not producing fail report #285
    ## Detailed Changes:
    ------------------------------------------------------------------------
    - Unittestharness now works with our CI on Jenkins.
    - The junit report is based on scanning stdout from report_test
      output, which is then sorted into passed, warned and failed outputs
      and their respective error messages (if any)
    - Added a minimal options parser, cleaning, xml output
    - Fixed the PYTHONPATH and library linking (did not test electric)
    - Fixed some issues with inconsistent indentantion in the file
    - Updated Makefile.in to generate unittest junit report
    - Updated Makefile.in to have a separate unittest clean target
    
    ## Comments on Changes:
    ------------------------------------------------------------------------
    - The timings reported in the junit report are not representative of
      the time taken for each individual report_test call to execute, given
      that that would require adding Fortran timing hooks, in across all
      the unittest code blocs, which is a fair bit more intrusive and not
      that important to know. However, the entire unittest runtime is
      accurate.
    - The only stdout captured and placed in the junit report are lines
      starting with either Pass:, Warn: or Fail: anything else is ignored
      given that capturing normal stdout in the correct order for individual
      report_test calls, was too much trouble and once again I am not sure
      if there is any substantial added benefit having that functionality.
    - It would be great to get the base folder of the unittest, instead of
      simply just the name, however given that all the unit tests are
      stripped away from that info when placed into `bin/tests`, I could
      not think of a scalable in runtime or highly intrusive solution.

--- tools/unittestharness.py
+++ tools/unittestharness.py
@@ -3,6 +3,21 @@
 import os
 import os.path
 import glob
+import time
+
+try:
+  from junit_xml import TestSuite, TestCase
+except ImportError:
+    class TestSuite(object):
+        def __init__(self, name, test_cases):
+            self.test_cases=test_cases
+        def to_file(self,*args):
+            print("cannot generate xml report without junit_xml module.")
+    class TestCase(object):
+        def __init__(self,*args,**kwargs):
+            pass
+        def add_failure_info(self,*args,**kwargs):
+            pass
 
 class UnitTest:
     def __init__(self, exe):
@@ -10,6 +25,9 @@ class UnitTest:
         self.verbose = True
         self.dir = os.sep.join((os.curdir + os.sep + exe).split(os.sep)[:-1])
         self.cwd = os.getcwd()
+        self.fail_msgs = []
+        self.pass_msgs = []
+        self.warn_msgs = []
 
     def log(self, msg):
         if self.verbose and msg != '':
@@ -17,7 +35,7 @@ class UnitTest:
 
     def run(self):
         os.chdir(self.dir)
-#        self.log("chdir " + self.dir)
+        # self.log("chdir " + self.dir)
         self.log("Running")
         f = os.popen(self.exe)
         self.output = f.read()
@@ -37,22 +55,45 @@ class UnitTest:
         for line in self.output.split('\n'):
             line = line.lstrip()
             self.log(line)
-            if line.startswith("Pass"): passcount = passcount + 1
-            if line.startswith("Warn"): warncount = warncount + 1
-            if line.startswith("Fail"): failcount = failcount + 1
+
+            # The junit integration is closely dependent to the output from
+            # report_test in the Fortran Unitest_tools.F90 module, so if logging
+            # in the Fortran module is ever altered remember to update these
+            # splits as well accordingly.
+            # Get the output after Pass, Warn, Fail
+            if line.startswith("Pass"):
+                self.pass_msgs.append(line.split("Pass: ", 1)[1])
+                passcount = passcount + 1
+
+            elif line.startswith("Warn"):
+                self.warn_msgs.append(line.split("Warn: ", 1)[1])
+                warncount = warncount + 1
+
+            elif line.startswith("Fail"):
+                self.fail_msgs.append(line.split("Fail: ", 1)[1])
+                failcount = failcount + 1
 
         return (passcount, warncount, failcount)
 
 class UnitTestHarness:
-    def __init__(self, dir):
+    def __init__(self, dir, xml_outfile=''):
         self.tests = []
+        self.xml_parser = TestSuite('TestHarness',[])
+        self.xml_outfile = xml_outfile
+        self.xml_reports = []
+        self.cwd = os.getcwd()
+
         if dir[-1] == '/': dir = dir + "*"
         else: dir = dir + "/*"
 
-        files = glob.glob(dir)
+        # Ignore all .vtu files in the test directory
+        files = glob.glob(f'{dir}[!.vtu]')
         for file in files:
             if not os.path.isdir(file):
                 self.tests.append(UnitTest(file))
+
+    def clean(self):
+        os.system(f'rm -rf {self.cwd}/bin/tests')        
 
     def run(self):
         passcount = 0
@@ -61,28 +102,62 @@ class UnitTestHarness:
 
         warntests = []
         failtests = []
+        passtests = []
 
         for test in self.tests:
+            wall_time = time.time()
             exitStatus = test.run()
             
             (P, W, F) = test.parse()
 
+            # Discard relative path characters
+            test_name = test.exe[2:]
+            classname = f'unittest.{test_name}'
+
             if (P, W, F) == (0, 0, 0):
-              print("    WARNING: no output from test")
-              warncount += 1
-              warntests.append(test.exe)
+                msg = "    WARNING: no output from test"
+                print(msg)
+                test.warn_msgs.append(msg)
+                warncount += 1
+                warntests.append(test_name)
+
+            if P > 0:
+                passtests.append(test_name)
+
+                for t in test.pass_msgs:
+                    tc = TestCase(t, classname, time.time() - wall_time)
+                    self.xml_reports.append(tc)
+                    self.xml_parser.test_cases += [tc]
 
             if W > 0:
-              warntests.append(test.exe)
+                warntests.append(test_name)
+
+                for t in test.warn_msgs:
+                    # Extract the test name from the error message
+                    name = t.split('; ', 1)[0]
+                    msg = t.split('error: ', 1)[1]
+                    tc = TestCase(name, classname, time.time() - wall_time)
+                    tc.add_failure_info('Failure', msg)
+                    self.xml_reports.append(tc)
+                    self.xml_parser.test_cases += [tc]
 
             if F > 0:
-              failtests.append(test.exe)
+                failtests.append(test_name)
+
+                for t in test.fail_msgs:
+                    # Extract the test name from the error message
+                    name = t.split('; ', 1)[0]
+                    msg = t.split('error: ', 1)[1]
+                    tc = TestCase(name, classname, time.time() - wall_time)
+                    tc.add_failure_info('Failure', msg)
+                    self.xml_reports.append(tc)
+                    self.xml_parser.test_cases += [tc]
               
             if not exitStatus == 0:
-              print("    ERROR: non-zero exit code from test")
-              failcount += 1
-              if not test.exe in failtests:
-                  failtests.append(test.exe)
+                print('    ERROR: non-zero exit code from test')
+                failcount += 1
+                if not test_name in failtests:
+                    failtests.append(test_name)
 
             passcount += P
             warncount += W
@@ -99,22 +174,40 @@ class UnitTestHarness:
         else:
             print("    Failures: %d; tests = %s" % (failcount, failtests))
 
+        if self.xml_outfile != '':
+            fd = open(f'{self.cwd}/{self.xml_outfile}', 'w')
+            self.xml_parser.to_file(fd, [self.xml_parser])
+            fd.close()
+
 if __name__ == "__main__":
     import sys
+    import optparse
+
+    parser = optparse.OptionParser()
+    parser.add_option('-d', '--dir', dest='dir', default='bin/tests', help='nittest binary location default bin/tests')
+    parser.add_option('-x', '--xml-output', dest='xml_outfile', default='', help='filename for xml output')
+    parser.add_option('-c', '--clean', action='store_true', dest='clean', default = False)
+    parser.add_option('--electricfence', action='store_true', dest='electricfence', default = False, help='link against electric fence lib')
+    (options, args) = parser.parse_args()
 
     try:
-        os.environ["PYTHONPATH"] = os.path.abspath(os.path.join(os.getcwd(), "python")) + ":" + os.environ["PYTHONPATH"]
+        os.environ["PYTHONPATH"] = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), "..", "python")) + ":" + os.environ["PYTHONPATH"]
     except KeyError:
-        os.putenv("PYTHONPATH", os.path.abspath(os.path.join(os.getcwd(), "python")))
+        os.putenv("PYTHONPATH", os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), "..", "python")))
 
     try:
-        os.environ["LD_LIBRARY_PATH"] = os.getcwd() + os.sep + sys.argv[1] + os.sep + "lib:" + os.environ["LD_LIBRARY_PATH"]
+        os.environ["LD_LIBRARY_PATH"] = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), "..", "lib")) + ":" + os.environ["LD_LIBRARY_PATH"]
     except KeyError:
-        os.putenv("LD_LIBRARY_PATH", os.getcwd() + os.sep + sys.argv[1] + os.sep + "lib")
+        os.putenv("LD_LIBRARY_PATH", os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), "..", "lib")))
 
-    if "--electricfence" in sys.argv:
-      os.putenv("LD_PRELOAD", "/usr/lib/libefence.so.0.0")
-      #os.putenv("EF_DISABLE_BANNER", "1")
+    if options.electricfence:
+        os.putenv("LD_PRELOAD", "/usr/lib/libefence.so.0.0")
+        #os.putenv("EF_DISABLE_BANNER", "1")
 
-    TestHarness = UnitTestHarness(sys.argv[-1])
-    TestHarness.run()
+    unittestharness = UnitTestHarness(dir=options.dir, 
+                                      xml_outfile=options.xml_outfile)
+
+    if options.clean:
+        unittestharness.clean()
+    else:
+        unittestharness.run()
