FluidityProject/fluidity

commit bd623421d0168e481a76b9a5a5b335bc913f5d04

Always create %region_ids on extruded mesh,
even if the horizontal mesh doesn't have a %region_ids. This fixes the
case where extruded_region_ids are specified in one or more layers, but
the horizontal input mesh does not have any. To keep things simple the
extruded mesh now always allocates a region_ids array which if
extruded_region_ids are not specified, and there are no horizontal
region_ids to copy from, default to 0.

horizontal_adaptivity/Advancing_front.F90
@@ -59,7 +59,7 @@ module hadapt_advancing_front
     
     logical:: adjacent_to_owned_element, adjacent_to_owned_column, shared_face
     logical :: top_element, bottom_element
-    logical :: apply_region_ids, propagate_region_ids
+    logical :: multiple_regions
     integer, dimension(:), allocatable :: old_element_data
     
     integer, dimension(2) :: shape_option
@@ -113,9 +113,7 @@ module hadapt_advancing_front
     assert(associated(mesh%mesh%columns))
     assert(associated(mesh%mesh%element_columns))
 
-    ! if the horizontal mesh has region ids associated with it, the elements below
-    ! can inherit these ids (not necessarily true in unittests)
-    propagate_region_ids = associated(mesh%mesh%region_ids)
+    mesh%mesh%region_ids = 0
 
     faces_seen = 0
     if (has_faces(h_mesh%mesh)) then
@@ -176,9 +174,9 @@ module hadapt_advancing_front
       ! get the region id information used for extrusion (if any) plus the top and bottom surface ids
       n_regions = option_count(trim(mesh%mesh%option_path)//'/from_mesh/extrude/layer[' &
         // int2str(layer-1) // ']/regions')
-      apply_region_ids = (n_regions>1)
+      multiple_regions = (n_regions>1)
 
-      if (apply_region_ids .and. .not. propagate_region_ids) then
+      if (multiple_regions .and. .not. associated(h_mesh%mesh%region_ids)) then
         FLAbort("Multiple extrude regions in options tree but no region ids in mesh")
       end if
 
@@ -193,7 +191,7 @@ module hadapt_advancing_front
         region_option_path = trim(mesh%mesh%option_path) // &
             "/from_mesh/extrude/layer[" // int2str(layer-1) // &
             "]/regions[" // int2str(r) // "]"
-        if(apply_region_ids) then
+        if(multiple_regions) then
           shape_option=option_shape(trim(region_option_path)// "/region_ids")
           allocate(region_ids(1:shape_option(1)))
           call get_option(trim(region_option_path) // "/region_ids", region_ids)
@@ -207,7 +205,7 @@ module hadapt_advancing_front
         call get_option(trim(region_option_path) // '/extruded_region_id', extruded_region_id, stat=extruded_region_id_stat)
 
         do h_ele = 1, size(top_surface_ids)
-          if(apply_region_ids) then
+          if(multiple_regions) then
             if(.not. any(h_mesh%mesh%region_ids(h_ele)==region_ids)) cycle
           end if
 
@@ -217,12 +215,12 @@ module hadapt_advancing_front
           bottom_surface_ids(h_ele) = bottom_surface_id
           if (extruded_region_id_stat==0) then
             extruded_region_ids(h_ele) = extruded_region_id
-          else if (propagate_region_ids) then
+          else if (associated(h_mesh%mesh%region_ids)) then
             extruded_region_ids(h_ele) = h_mesh%mesh%region_ids(h_ele)
           end if
         end do
 
-        if(apply_region_ids) deallocate(region_ids)
+        if(multiple_regions) deallocate(region_ids)
 
       end do
 
@@ -291,10 +289,7 @@ module hadapt_advancing_front
             ndglno_ptr(2) = l
           end if
 
-          ! if the horizontal mesh has region_ids these may as well be preserved here
-          if(propagate_region_ids) then
-            mesh%mesh%region_ids(ele) = extruded_region_ids(h_ele)
-          end if
+          mesh%mesh%region_ids(ele) = extruded_region_ids(h_ele)
 
           ! we now know the relationship between the mesh element and the h_mesh surface element
           ! save this for later...
@@ -430,15 +425,12 @@ module hadapt_advancing_front
       end do
       deallocate(old_element_data)
       
-      ! renumber the region_ids
-      if(propagate_region_ids) then
-        allocate(old_element_data(size(mesh%mesh%region_ids)))
-        old_element_data = mesh%mesh%region_ids
-        do i = 1, size(mesh%mesh%region_ids)
-          mesh%mesh%region_ids(fetch(old2new_ele, i)) = old_element_data(i)
-        end do
-        deallocate(old_element_data)
-      end if
+      allocate(old_element_data(size(mesh%mesh%region_ids)))
+      old_element_data = mesh%mesh%region_ids
+      do i = 1, size(mesh%mesh%region_ids)
+        mesh%mesh%region_ids(fetch(old2new_ele, i)) = old_element_data(i)
+      end do
+      deallocate(old_element_data)
       
       call deallocate(old2new_ele)
       

horizontal_adaptivity/Combine_Meshes.F90
@@ -91,11 +91,8 @@ module hadapt_combine_meshes
     ! it lies under
     allocate(mesh%element_columns(total_out_elements))
     mesh%element_columns = 0
-    ! if the horizontal mesh has region ids these can be propagated down into the full
-    ! mesh.  allocate space for this...
-    if(associated(h_mesh%mesh%region_ids)) then
-      allocate(mesh%region_ids(total_out_elements))
-    end if
+    ! to keep things simple, always allocate region_ids for the extruded mesh, regardless of whether h_mesh has them
+    allocate(mesh%region_ids(total_out_elements))
     if (mesh_name=="CoordinateMesh") then
       call allocate(out_mesh, mesh_dim(h_mesh)+1, mesh, "Coordinate")
     else

commit 7e7bd691ba3a785d2ee547b16edcd084b2d24008

Fix extrusion with unspecified region ids
This is for the case where there are region ids specified in the options
and the input mesh also does not have region ids.

Bug introduced in bd623421d0168e481a76b9a5a5b335bc913f5d04:
although the new mesh%regions was initialised to 0, it then gets
clobbered by an uninitalised extruded_region_ids array that determines
the region id in each column.

This should actually be backported to master.

horizontal_adaptivity/Advancing_front.F90
@@ -83,6 +83,7 @@ module hadapt_advancing_front
     allocate(bottom_surface_ids(ele_count(h_mesh)))
     allocate(top_surface_ids(ele_count(h_mesh)))
     allocate(extruded_region_ids(ele_count(h_mesh)))
+    extruded_region_ids = 0
 
     allocate(hanging_node(node_count(h_mesh)))
     allocate(column_size(node_count(h_mesh)))