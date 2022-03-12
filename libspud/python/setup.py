71b893e424c5ba3efa5e4d86609dd6fc68156d31
Patol75 <td75013@hotmail.fr>
2022-03-12 11:02:59 +1100

    ENH: Incorporate python; further progress in incorporating spud sub-directories; misc improvements

--- libspud/python/setup.py
+++ libspud/python/setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup, Extension
-import os.path
+from setuptools import Extension, setup
 
-module1 = Extension('libspud', sources = ['libspud.c'], libraries=["spud"], library_dirs=[os.path.abspath("..")], include_dirs=[os.path.abspath("../include")])
-
-setup (name = 'libspud',
-       version = '1.1.3',
-       description = 'Python bindings for libspud',
-       ext_modules = [module1])
+setup(
+    ext_modules=[
+        Extension(
+            name='libspud', sources=['libspud.c'], include_dirs=['../include'],
+            library_dirs=['..'], libraries=['spud'],
+        )
+    ]
+)
